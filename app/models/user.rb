1907deac4c1f2202eb8f6cc2d86705d91fe9b71d
Geoffrey Clapp <geoff.clapp@gmail.com>
2013-01-16 08:33:58 -0800

    Change query for Heroku failure

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,38 +7,38 @@ class User < ActiveRecord::Base
   			:through => :user_readings
 
 def readContent
-	contentList = User.find_by_sql("SELECT DISTINCT contents.*, user_readings.completed_date AS completedDate from contents INNER JOIN user_readings ON content_id = user_readings.content_id WHERE content_id = user_readings.content_id AND user_readings.user_id = " + self.id.to_s)
-	contentList.uniq!
+	readingList = UserReading.find_by_sql("SELECT DISTINCT contents.*, user_readings.completed_date AS completedDate from contents INNER JOIN user_readings ON content_id = user_readings.content_id WHERE content_id = user_readings.content_id AND user_readings.user_id = " + self.id.to_s)
+	readingList.uniq!
 	
-	contentList.each do | content |
+	readingList.each do | readingItem |
+		puts readingItem
+		puts readingItem.headline + readingItem.completedDate
 
-		#puts content.headline + content.completedDate
+		 if !readingItem.completedDate.nil?
+		 	readingItem.completedDate = ActiveSupport::TimeWithZone.new(nil, Time.zone, DateTime.parse(readingItem.completedDate))
+		 end
 
-		# if !content.completedDate.nil?
-		# 	content.completedDate = ActiveSupport::TimeWithZone.new(nil, Time.zone, DateTime.parse(content.completedDate))
-		# end
-
-		if !content.text.nil?
+		if !readingItem.text.nil?
 
 		item = "<html><head></head><body>"
 
-  		if !content.author_id.nil?
-  			author = Author.find(content.author_id)
+  		if !readingItem.author_id.nil?
+  			author = Author.find(readingItem.author_id)
   			if !author.imageURL.empty?
   				item += "<div class=""authorPicture"" style=""float:left""><img src=""/assets/" + author.imageURL + "/></div>"
   			end
   			item +="<div class=""articleAuthor"">From " + author.name + "</div>" 
   		end
 
-  		item += "<div class=""articleBody"" style=""font-family:""Arial"", Arial, sans-serif;"">" + content.text + "</div>"
+  		item += "<div class=""articleBody"" style=""font-family:""Arial"", Arial, sans-serif;"">" + readingItem.text + "</div>"
   		item += "</body></html>"
 
-  		content.text = item
+  		readingItem.text = item
 
   		end
 	end
 	
-	contentList
+	readingList
 end
 
 end
