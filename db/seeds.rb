352618ef6c1782e983b6387eaa2ccee3cbcf70c9
Geoffrey Clapp <geoff.clapp@gmail.com>
2013-02-11 21:47:36 -0800

    Clean up new install situations (e.g., new database, rake db:reset, etc)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,47 +3,57 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 
-# Create some default Users
 
-nancy 	= User.create(firstName: "Nancy", 	lastName: "Smith", 	gender:"F", birthDate:"06/18/1950")
-bob 	= User.create(firstName: "Bob", 	lastName: "Jones", 	gender:"M", birthDate:"01/10/1973")
-limburg = User.create(firstName: "Paul", 	lastName: "Limburg",gender:"M",  )
-shelly  = User.create(firstName: "Shelly", 	lastName: "Norman", gender:"F", imageURL: "shelly.png")
-sheldon = User.create(firstName: "Sheldon",	lastName: "Sheps", 	gender:"M", imageURL: "drsheps.png")
-varkey  = User.create(firstName: "Prathibha", lastName: "Varkey", gender:"F" )
+
+# Content Keywords Default Set
+ContentKeywords.create!(name:"Flu Shot", 	default:true)
+ContentKeywords.create!(name:"Medications", 	default:true)
+ContentKeywords.create!(name:"Weight Loss", 	default:true)
+ContentKeywords.create!(name:"Diabetes", 	default:true)
+ContentKeywords.create!(name:"Healthy Eating", default:true)
+
 
 #some default content
-installed = Content.create(
+installed = Content.create!(
 	contentsType: 'message',
 	headline: "Installed RHS",
 	body:"Installed RHS")
 
-ContentAuthor.create(user:shelly, content:installed)
 
-welcome = Content.create( 
+welcome = Content.create!( 
 	contentsType: 'message',
 	headline: 'Welcome',
 	body:'<p>I''m Sandra, one of your RHS Health Advocates here to support you in your quest for a healthier, fuller, life. The more you use RHS, the smarter we will get in providing you a personalized health expierence and better care</p>'
 	)
-ContentAuthor.create(user:shelly, content:welcome)
 
-bpWeather = Content.create(
+bpWeather = Content.create!(
 	contentsType: 'article',
 	headline: "Weather alert: Low Temps and your Blood Pressure.",
 	body:"<p>Blood pressure generally is higher in the winter because low temperatures cause your blood vessels to narrow, which increases blood pressure because more pressure is needed to force blood through your narrowed veins and arteries.</p>"
 	)
-ContentAuthor.create(user:sheldon, content:bpWeather)
 
 
-nancyReadInstalled = UserReading.create(read_date:Time.zone.now.iso8601, user:nancy, content:installed)
-nancyReadWelcome = UserReading.create(read_date:Time.zone.now.iso8601, user:nancy, content:welcome)
-nancyUnreadBPWeather = UserReading.create(user:nancy, content:bpWeather)
+# Create some default Users
+nancy 	= User.create!(firstName: "Nancy", 	lastName: "Smith", 	gender:"F", birthDate:"06/18/1950", install_id: "123345")
+bob 	= User.create!(firstName: "Bob", 	lastName: "Jones", 	gender:"M", birthDate:"01/10/1973", install_id: "122233")
+limburg = User.create!(firstName: "Paul", 	lastName: "Limburg",gender:"M", install_id: "144444") 
+shelly  = User.create!(firstName: "Shelly", 	lastName: "Norman", gender:"F", imageURL: "shelly.png", install_id: "555555")
+sheldon = User.create!(firstName: "Sheldon",	lastName: "Sheps", 	gender:"M", imageURL: "drsheps.png", install_id: "666666")
+varkey  = User.create!(firstName: "Prathibha", lastName: "Varkey", gender:"F", install_id: "77777")
 
-bobReadInstalled = UserReading.create(read_date:Time.now, user:bob, content:installed)
-bobWelcome   = UserReading.create(user:bob, content:welcome)
-bobBPWeather = UserReading.create(user:bob, content:bpWeather)
+ContentAuthor.create!(user:shelly, content:installed)
+ContentAuthor.create!(user:shelly, content:welcome)
+ContentAuthor.create!(user:sheldon, content:bpWeather)
+
+#nancyReadInstalled = UserReading.create!(read_date:Time.zone.now.iso8601, user:nancy, content:installed)
+#nancyReadWelcome = UserReading.create!(read_date:Time.zone.now.iso8601, user:nancy, content:welcome)
+nancyUnreadBPWeather = UserReading.create!(user:nancy, content:bpWeather)
+
+#bobReadInstalled = UserReading.create!(read_date:Time.now, user:bob, content:installed)
+#bobWelcome   = UserReading.create!(user:bob, content:welcome)
+bobBPWeather = UserReading.create!(user:bob, content:bpWeather)
 
 
