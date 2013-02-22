5054acb791a01edfdf6026291a408322a5b57758
Geoffrey Clapp <geoff.clapp@gmail.com>
2013-02-22 10:25:34 -0800

    More fixes

--- app/models/content.rb
+++ app/models/content.rb
@@ -34,13 +34,13 @@ end
 def asFullArticle
 	item = "<html><head><link href=""/assets/application.css"" rel=""stylesheet"" type=""text/css""></head><body>"
 
-	if !content_authors.first.nil?
-		if !content_authors.first.user.imageURL.empty?
-			item += "<div class=""authorPicture"" style=""float:left""><img src=""/assets/" + content_authors.first.user.imageURL + "/></div>"
-		end
-		item +="<div class=""content_subtitle""> " + created_at.to_s + "&nbsp;|&nbsp;By&nbsp;" 
-		+ content_authors.first.user.firstName + "&nbsp;" + content_authors.first.user.lastName + "</div>" 
-	end
+	# if !content_authors.first.nil?
+	# 	if !content_authors.first.user.imageURL.empty?
+	# 		item += "<div class=""authorPicture"" style=""float:left""><img src=""/assets/" + content_authors.first.user.imageURL + "/></div>"
+	# 	end
+	# 	item +="<div class=""content_subtitle""> " + created_at.to_s + "&nbsp;|&nbsp;By&nbsp;" 
+	# 	+ content_authors.first.user.firstName + "&nbsp;" + content_authors.first.user.lastName + "</div>" 
+	# end
 
 	item += "<div class=""content_text"">" + body + "</div>"
 	item += "</body></html>"
