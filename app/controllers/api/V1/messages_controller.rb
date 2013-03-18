655fb2739ef7f77e20e45c9faf02df438a42d8b0
Cody Veal <cveal05@gmail.com>
2013-03-18 11:06:46 -0400

    AndreiD - [#46201651] creating a new message

--- app/controllers/api/V1/messages_controller.rb
+++ app/controllers/api/V1/messages_controller.rb
@@ -1,18 +1,55 @@
 class Api::V1::MessagesController < Api::V1::ABaseController
   def create
-    #create encounter
-    encounter = Encounter.create :status=>"open", :priority=> "medium"
-    #create encounter_member entry
-    encounter_user = EncountersUser.create :role=>"participator", :encounter_id=>encounter.id, :user_id=>current_user.id
-    if params[:subject_user_id] && params[:subject_user_id]!=current_user.id
-      encounter_subject_user = EncountersUser.create :role=>"subject", :encounter_id=>encounter.id, :user_id=>subject_user_id
+
+    if params[:encounter_id].present?
+      encounter = Encounter.find_by_id params[:encounter_id]
+      return render_failure(reason:"Encounter with id #{params[:encounter_id]} is not found", 404) unless encounter
+    else
+      #create encounter
+      encounter = Encounter.create :status=>"open", :priority=> "medium"
     end
 
-     #create message
-     message = Message.create :body=>params[:body]
-        #attach mayo_keywords
-        #attach attachments
-        #create and set location
-     #create message_status
+    #create encounter_member entry
+    encounter_user = EncountersUser.find_or_create_by_encounter_id_and_user_id encounter.id, current_user.id
+    encounter_user.update_attribute :role, "participator"
+    if params[:subject_user_id] && params[:subject_user_id]!=current_user.id
+      encounter_subject_user = EncountersUser.find_or_createby_encounter_id_and_user_id encounter.id, ubject_user_id
+    end
+
+
+    #create message
+    message = Message.create :body=>params[:body]
+
+    #attach mayo_keywords
+    if params[:keywords].present?
+      params[:keywords].each do |keyword_name|
+        keyword = MayoVocabulary.find_by_name keyword_name
+        message.mayo_vocabulary << keyword
+      end
+    end
+
+    #attach attachments
+    if params[:attachments].present?
+      params[:attachments].each do |attachment_url|
+        attachment = Attachment.create :url => attachment_url
+        message.attachments << attachment
+      end
+    end
+
+    #create and set location
+    location_json = params[:location]
+    if location_json
+      location = UserLocation.create :longitude=>location_json[:longitude], :latitude=>location_json[:latitude]
+      message.user_location = location
+    end
+
+    #create message_status
+
+
+  end
+
+
+  def list
+    return render_successful encounters:current_user.encounters
   end
 end
--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,7 +1,16 @@
 class Encounter < ActiveRecord::Base
   attr_accessible :checked, :priority, :status
-  
+
   has_many :encounters_users
   has_many :messages
   has_many :users, :through=> :encounters_users
+
+  def as_json options=nil
+    {
+      :status=> status,
+      :priority=> priority,
+      :checked => checked,
+      :messages => messages
+    }
+  end
 end
