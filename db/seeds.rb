b197cd43e0ed59173dbc9ff0cd69023a88e18745
Cody Veal <cveal05@gmail.com>
2013-03-19 11:24:31 -0400

    AndreiD - fixing the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ installed = Content.create!(
 	body:"Installed RHS")
 
 
-welcome = Content.create!( 
+welcome = Content.create!(
 	contentsType: 'message',
 	title: 'Welcome',
 	body:'<p>I''m Sandra, one of your RHS Health Advocates here to support you in your quest for a healthier, fuller, life. The more you use RHS, the smarter we will get in providing you a personalized health expierence and better care</p>'
@@ -24,12 +24,10 @@ welcome = Content.create!(
 # Create some default Users
 nancy 	= User.create!(first_name: "Nancy", last_name: "Smith", 	gender:"F", birth_date:"06/18/1950", install_id: "123345")
 bob 	= User.create!(first_name: "Bob", 	last_name: "Jones", 	gender:"M", birth_date:"01/10/1973", install_id: "122233")
-limburg = User.create!(first_name: "Paul", 	last_name: "Limburg",	gender:"M", install_id: "144444") 
-shelly  = User.create!(first_name: "Shelly",last_name: "Norman", 	gender:"F", imageURL: "shelly.png", install_id: "555555")
+limburg = User.create!(first_name: "Paul", 	last_name: "Limburg",	gender:"M", install_id: "144444")
+shelly  = User.create!(first_name: "Shelly",last_name: "Norman", 	gender:"F", install_id: "555555")
 
 
-ContentAuthor.create!(user:shelly, content:installed)
-ContentAuthor.create!(user:shelly, content:welcome)
 
 
 #nancyReadInstalled = UserReading.create!(read_date:Time.zone.now.iso8601, user:nancy, content:installed)
