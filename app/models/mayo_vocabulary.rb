60bdba138140f09f1ed7b111d07e095bd5a4fa52
AndreiD & MichelleW <andrei.dinin@xtremelabs.com>
2013-03-20 18:33:29 -0400

    AndreiD - small fix in the relations

--- app/models/mayo_vocabulary.rb
+++ app/models/mayo_vocabulary.rb
@@ -2,8 +2,9 @@ class MayoVocabulary < ActiveRecord::Base
   attr_accessible :mcvid, :title
   has_many :contents_mayo_vocabularies
   has_many :contents, :through => :contents_mayo_vocabularies
-  has_many :messages
 
+  has_many :mayo_vocabularies_messages
+  has_many :messages, :through => :mayo_vocabularies_messages
   
   def as_json options=nil
     {
