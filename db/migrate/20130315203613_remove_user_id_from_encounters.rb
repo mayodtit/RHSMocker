147c91e6ca9928bd894ae5b9e4814a6f4244ed99
AndreiD & MichelleW <andrei.dinin@xtremelabs.com>
2013-03-20 11:17:28 -0400

    MichelleW/AndreiD - remove migration

--- db/migrate/20130315203613_remove_user_id_from_encounters.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromEncounters < ActiveRecord::Migration
-  def up
-    remove_column :encounters, :user
-  end
-
-  def down
-    add_column :encounters, :user, :references
-  end
-end
