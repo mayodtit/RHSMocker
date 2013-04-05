39e369da35a0110f2802a87177d56ee5c96a5628
AndreiD & MichelleW <andrei.dinin@xtremelabs.com>
2013-04-05 15:42:09 -0400

    MichelleW, AndreiD - changing the migrate

--- db/migrate/20130403212354_rename_symptom_factors_to_symptoms_factors_on_contents_symptoms_factors.rb
+++ db/migrate/20130403212354_rename_symptom_factors_to_symptoms_factors_on_contents_symptoms_factors.rb
@@ -1,6 +1,6 @@
 class RenameSymptomFactorsToSymptomsFactorsOnContentsSymptomsFactors < ActiveRecord::Migration
   def change
     rename_column :contents_symptoms_factors, :symptom_factor_id, :symptoms_factor_id
-    rename_index :contents_symptoms_factors, :symptom_factor_id, :symptoms_factor_id
+    #rename_index :contents_symptoms_factors, :symptom_factor_id, :symptoms_factor_id
   end
 end
