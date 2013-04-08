de38c828845c656992a98a5033c7364e55443b8d
AndreiD & MichelleW <andrei.dinin@xtremelabs.com>
2013-04-08 15:29:09 -0400

    MichelleW - changed sort order of symptoms to be alphabetical

--- app/controllers/api/V1/symptoms_controller.rb
+++ app/controllers/api/V1/symptoms_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SymptomsController < Api::V1::ABaseController
   skip_before_filter :authentication_check, :only=>:index
 
   def index
-    render_success symptoms:Symptom.all
+    render_success symptoms:Symptom.all(:order=>:name)
   end
 
   def check
