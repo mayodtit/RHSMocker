151b60d2bc996814718e4dc3f5b9bb1ee0725fc0
Michelle W <michelle.wai@xtremelabs.com>
2013-04-23 18:05:28 -0400

    MichelleW - [#46674887] documented API failure modes - for messages and password_resets

--- spec/acceptance/api/v1/password_resets_spec.rb
+++ spec/acceptance/api/v1/password_resets_spec.rb
@@ -5,9 +5,10 @@ resource "PasswordResets" do
   header 'Accept', 'application/json'
   header 'Content-Type', 'application/json'
 
-  before(:each) do
+  before(:all) do
     @user = FactoryGirl.create(:user_with_email)
   end
+
 
   post '/api/v1/password_resets' do
     parameter :email, "User's email address"
@@ -20,6 +21,20 @@ resource "PasswordResets" do
       explanation "Emails password reset instructions to the user"
       status.should == 200
       JSON.parse(response_body).should_not be_empty
+    end
+  end
+
+  post '/api/v1/password_resets' do
+    parameter :email, "User's email address"
+    required_parameters :email
+
+    let(:email)     { 'email_does_not_exist@fake.com' }
+    let(:raw_post)  { params.to_json }  # JSON format request body
+
+    example_request "[POST] Reset password (forgot password) (404)" do
+      explanation "Emails password reset instructions to the user"
+      status.should == 404
+      JSON.parse(response_body)['reason'].should_not be_empty
     end
   end
 
