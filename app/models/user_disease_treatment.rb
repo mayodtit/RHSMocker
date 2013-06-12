537d156094314e84285adc8ce661b1aaebce00d9
Kyle Chilcutt <kyle@getbetter.com>
2013-06-12 15:56:34 -0700

    Add join relation for treatment side effects
    Through UserDiseaseTreatmentTreatmentSideEffect.

--- app/models/user_disease_treatment.rb
+++ app/models/user_disease_treatment.rb
@@ -1,8 +1,15 @@
 class UserDiseaseTreatment < ActiveRecord::Base
-  belongs_to :user_disease
-  belongs_to :treatment
   belongs_to :user
-  attr_accessible :amount, :amount_unit, :doctor_user_id, :end_date, :prescribed_by_doctor, :side_effect, :start_date, :successful, :time_duration, :time_duration_unit, :user, :treatment_id, :user_disease_id
+  belongs_to :treatment
+  belongs_to :user_disease
+
+  has_many :user_disease_treatment_treatment_side_effects
+  has_many :treatment_side_effects, :through => :user_disease_treatment_treatment_side_effects
+
+  attr_accessible :user, :treatment, :user_disease
+  attr_accessible :user_id, :treatment_id, :user_disease_id, :amount, :amount_unit, :doctor_user_id,
+                  :end_date, :prescribed_by_doctor, :side_effect, :start_date, :successful, :time_duration,
+                  :time_duration_unit
 
   def as_json options=nil
     {treatment:treatment}
