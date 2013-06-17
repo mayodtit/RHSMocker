56c0ceaaadc1f6447010107d7bfca20f450cdf17
Kyle Chilcutt <kyle@getbetter.com>
2013-06-17 09:49:16 -0700

    Sanitize NPI service call results
    And DRY up the code a bit.

--- lib/search/service/npi.rb
+++ lib/search/service/npi.rb
@@ -3,15 +3,26 @@ class Search::Service::Npi
   base_uri 'http://docnpi.com'
 
   def query(search_params)
-    self.class.get('/api/index.php', :query => search_params.merge(:format => :json))
-              .parsed_response
+    sanitize_response(self.class.get('/api/index.php', :query => search_params.merge(:format => :json))
+                      .parsed_response)
   end
 
   def find(params)
     id = params[:npi_number]
-    self.class.get('/api/index.php', :query => {:ident => id,
-                                                :is_ident => true,
-                                                :format => :json})
-              .parsed_response
+    query({:ident => id, :is_itent => true})[0]
+  end
+
+  private
+
+  def sanitize_response(response)
+    response.values.map do |user|
+      {
+        :first_name => user['first_name'],
+        :last_name => user['last_name'],
+        :npi_number => user['npi'],
+        :city => user['city'],
+        :state => user['state']
+      }
+    end
   end
 end
