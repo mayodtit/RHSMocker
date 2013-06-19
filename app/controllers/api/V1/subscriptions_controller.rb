f787d4afb76189db3bd03b3372d75e4681e4fd63
Kyle Chilcutt <kyle@getbetter.com>
2013-06-19 13:24:29 -0700

    Add SubscriptionsController
    For managing the joins relation between User and Plans.

---
+++ app/controllers/api/V1/subscriptions_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::SubscriptionsController < Api::V1::ABaseController
+  skip_before_filter :authentication_check
+  before_filter :load_user!, :except => :destroy
+
+  def index
+    @subscriptions = subscription_scope.all
+    render_success({subscriptions: @subscriptions})
+  end
+
+  def show
+    @subscription = subscription_scope.find(params[:id])
+    render_success({subscription: @subscription})
+  end
+
+  def create
+    @subscription = subscription_scope.create(params[:subscription])
+    if @subscription.errors.empty?
+      render_success({subscription: @subscription})
+    else
+      render_failure({reason: @subscription.errors.full_messages.to_sentence}, 422)
+    end
+  end
+
+  def update
+    @subscription = subscription_scope.find(params[:id])
+    if @subscription.update_attributes(params[:subscription])
+      render_success({subscription: @subscription})
+    else
+      render_failure({reason: @subscription.errors.full_messages.to_sentence}, 422)
+    end
+  end
+
+  def destroy
+    @subscription = Subscription.find(params[:id])
+    if @subscription.destroy
+      render_success
+    else
+      render_failure({reason: @subscription.errors.full_messages.to_sentence}, 422)
+    end
+  end
+
+  private
+
+  def load_user!
+    @user = User.find(params[:user_id]) if params[:user_id]
+  end
+
+  def subscription_scope
+    @user.try(:subscriptions) || Subscription
+  end
+end
