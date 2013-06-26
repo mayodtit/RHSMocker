f7f2306d712b473d718e79fbe3bc12c577949b37
Kyle Chilcutt <kyle@getbetter.com>
2013-06-26 13:48:06 -0700

    Add inverse_associations to User
    Reverse relationship to see which other users have an association with a
    given user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,9 @@ class User < ActiveRecord::Base
   has_many :associations, :dependent => :destroy
   has_many :associates, :through=>:associations
 
+  has_many :inverse_associations, :class_name => 'Association', :foreign_key => 'associate_id'
+  has_many :inverse_associates, :through => :inverse_associations, :source => :user
+
   has_and_belongs_to_many :institutions
 
   has_many :feedbacks
