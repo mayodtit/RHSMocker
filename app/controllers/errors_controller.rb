ba4485d8706e61666c2f527750ff48d8d835212b
Kyle Chilcutt <kyle@getbetter.com>
2013-06-27 10:59:06 -0700

    Add backtrace with error responses for debugging
    Except in production environment.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,9 +2,10 @@ class ErrorsController < ActionController::Base
   def exception
     @exception = env["action_dispatch.exception"]
     @wrapper = ActionDispatch::ExceptionWrapper.new(env, @exception)
-    render :json => {:status => :failure,
-                     :user_message => "",
-                     :reason => @exception.to_s}.to_json,
-           :status => @wrapper.status_code
+    @json = {:status => :failure,
+             :user_message => '',
+             :reason => @exception.to_s}
+    @json.merge!({:backtrace => @exception.backtrace}) unless Rails.env.production?
+    render :json => @json, :status => @wrapper.status_code
   end
 end
