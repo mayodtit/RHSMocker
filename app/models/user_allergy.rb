8e36dc4ad7eae68af346eb52bc9fb6b8c91cdd3c
Kyle Chilcutt <kyle@getbetter.com>
2013-06-27 11:47:32 -0700

    Add validations to UserAllergy
    Validates presence and uniqueness of attributes.  Add specs to verify
    the model and fix the factory to build valid objects.

--- app/models/user_allergy.rb
+++ app/models/user_allergy.rb
@@ -1,14 +1,17 @@
 class UserAllergy < ActiveRecord::Base
   belongs_to :user
   belongs_to :allergy
-  attr_accessible :user, :allergy #:title, :body
 
+  attr_accessible :user, :allergy
+  attr_accessible :user_id, :allergy_id
 
-  def as_json options=nil
+  validates :user, :allergy, :presence => true
+  validates :allergy_id, :uniqueness => {:scope => :user_id}
+
+  def as_json(options=nil)
     {
       :id=>id,
       :allergy=>allergy
     }
   end
-  
 end
