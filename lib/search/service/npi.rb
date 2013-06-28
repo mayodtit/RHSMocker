4b0e6b312a64e704429d4ede0e0c463eef92cd1b
Kyle Chilcutt <kyle@getbetter.com>
2013-06-28 14:40:14 -0700

    Redo NPI service based on API behavior
    The identifier field is not what it seems.  Search based on the returned
    criteria and then filter based on the NPI number.  Factor out common
    methods between Npi#find and Npi#query.

--- lib/search/service/npi.rb
+++ lib/search/service/npi.rb
@@ -2,33 +2,45 @@ class Search::Service::Npi
   include HTTParty
   base_uri 'http://docnpi.com'
 
-  def query(search_params)
-    response = self.class.get('/api/index.php', :query => search_params.merge(:taxonomy => 'group_1',
-                                                                              :format => :json))
-    raise StandardError, 'Non-success response from NPI database' unless response.success?
-    sanitize_response(response.parsed_response)
+  def query(params)
+    sanitize_response(get_records(params))
   end
 
   def find(params)
-    id = params[:npi_number]
-    result = query({:ident => id, :is_ident => true})[0]
-    raise ActiveRecord::RecordNotFound, "Could not find User with NPI number: #{params[:npi_number]}" unless result
-    result
+    response = get_records(params)
+    unless response[params[:npi_number]]
+      raise ActiveRecord::RecordNotFound, "Could not find User with NPI number: #{params[:npi_number]}"
+    end
+    sanitize_record(response[params[:npi_number]])
   end
 
   private
 
+  def get_records(params)
+    response = self.class.get('/api/index.php', :query => params.merge(:is_person => true,
+                                                                       :is_address => false,
+                                                                       :is_org => false,
+                                                                       :is_ident => false,
+                                                                       :format => :json))
+    raise StandardError, 'Non-success response from NPI database' unless response.success?
+    response.parsed_response
+  end
+
   def sanitize_response(response)
     return response if response.empty?
     response.values.map do |user|
-      {
-        :first_name => user['first_name'].titleize(:underscore => false, :humanize => false),
-        :last_name => user['last_name'].titleize(:underscore => false, :humanize => false),
-        :npi_number => user['npi'],
-        :city => user['city'].titleize(:underscore => false, :humanize => false),
-        :state => user['state'],
-        :expertise => user['description']
-      }
+      sanitize_record(user)
     end
+  end
+
+  def sanitize_record(user)
+    {
+      :first_name => user['first_name'].titleize(:underscore => false, :humanize => false),
+      :last_name => user['last_name'].titleize(:underscore => false, :humanize => false),
+      :npi_number => user['npi'],
+      :city => user['city'].titleize(:underscore => false, :humanize => false),
+      :state => user['state'],
+      :expertise => user['description']
+    }
   end
 end
