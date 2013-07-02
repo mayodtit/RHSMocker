8db4a70a8103d0df0c6b91408d03b4f56cb938b1
Kyle Chilcutt <kyle@getbetter.com>
2013-07-02 16:25:47 -0700

    Add specs for BloodPressuresController#destroy
    And update create spec to make sure create is called.

--- spec/controllers/api/v1/blood_pressures_controller_spec.rb
+++ spec/controllers/api/v1/blood_pressures_controller_spec.rb
@@ -83,6 +83,11 @@ describe Api::V1::BloodPressuresController do
           controller.stub(:authorize!)
         end
 
+        it 'attempts to create the record' do
+          blood_pressures.should_receive(:create).once
+          do_request
+        end
+
         context 'save succeeds' do
           it 'returns success' do
             do_request
@@ -110,5 +115,66 @@ describe Api::V1::BloodPressuresController do
     end
   end
 
-  describe 'delete destroy'
+  describe 'delete destroy' do
+    def do_request
+      delete :destroy
+    end
+
+    before(:each) do
+      BloodPressure.stub(:find => blood_pressure)
+      blood_pressure.stub(:destroy)
+    end
+
+    it 'requires authentication' do
+      do_request
+      response.should_not be_success
+      json = JSON.parse(response.body)
+      json['reason'].should == "Invalid auth_token"
+    end
+
+    context 'user signed-in' do
+      before(:each) do
+        controller.stub(:authentication_check)
+        controller.stub(:current_user => user)
+      end
+
+      it 'requires User authorization' do
+        expect{ do_request }.to raise_error(CanCan::AccessDenied)
+      end
+
+      context 'authorized' do
+        before(:each) do
+          controller.stub(:authorize!)
+        end
+
+        it 'attempts to destroy the record' do
+          blood_pressure.should_receive(:destroy).once
+          do_request
+        end
+
+        context 'destroy succeeds' do
+          before(:each) do
+            blood_pressure.stub(:destroy => true)
+          end
+
+          it 'returns success' do
+            do_request
+            response.should be_success
+          end
+        end
+
+        context 'destroy fails' do
+          before(:each) do
+            blood_pressure.stub(:destroy => false)
+            blood_pressure.errors.add(:base, :invalid)
+          end
+
+          it 'returns failure' do
+            do_request
+            response.should_not be_success
+          end
+        end
+      end
+    end
+  end
 end
