d84f4c144af52e51a2a6d90c4cee1b7114ecc18c
Kyle Chilcutt <kyle@getbetter.com>
2013-07-02 15:30:42 -0700

    Allow CanCan::AccessDenied to propagate
    Rack layer will hander errors.  Don't intercept exceptions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = "Access Denied!"
-    redirect_to root_url
-  end
-
-  private 
-
-  def not_authenticated
-    redirect_to login_url, :notice => "First log in to view this page."
-  end
 end
