7e3dc4430db7b7c182c764dd674642991f79eb0f
Kyle Chilcutt <kyle@getbetter.com>
2013-07-02 15:17:18 -0700

    Strip Users of all abilities
    Start from scratch, remove all abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,5 @@ class Ability
 
   def initialize(user)
     user ||= User.new # support for not logged-in user
-
-    if user.has_role? :admin
-      can :manage, :all
-    elsif user.has_role? :hcp
-      can :manage, Message
-    end
   end
 end
