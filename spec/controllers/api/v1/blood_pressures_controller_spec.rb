af71ca5d43558bee98e889b286c2e9ea2333eda6
Kyle Chilcutt <kyle@getbetter.com>
2013-07-03 09:15:51 -0700

    DRY up Controller specs
    Refactor out authentication and authorization tests to support helpers.
    Half the code, same covereage!
    
    We need to go deeper.  Still a lot of duplication between basic
    controller functionality, we can factor out common functionality for
    each controller action after variables are assigned.

--- spec/controllers/api/v1/blood_pressures_controller_spec.rb
+++ spec/controllers/api/v1/blood_pressures_controller_spec.rb
@@ -18,39 +18,15 @@ describe Api::V1::BloodPressuresController do
       user.stub(:blood_pressures => [blood_pressure])
     end
 
-    # TODO - make this more sensible; we should be intercepting an exception here.
-    it 'requires authentication' do
-      do_request
-      response.should_not be_success
-      json = JSON.parse(response.body)
-      json['reason'].should == "Invalid auth_token"
-    end
+    it_behaves_like 'action requiring authentication and authorization'
 
-    context 'user signed-in' do
-      before(:each) do
-        controller.stub(:authentication_check)
-        controller.stub(:current_user => user)
-      end
+    context 'authenticated and authorized', :user => :authenticate_and_authorize! do
+      it_behaves_like 'success'
 
-      it 'requires User authorization' do
-        expect{ do_request }.to raise_error(CanCan::AccessDenied)
-      end
-
-      context 'authorized' do
-        before(:each) do
-          ability.can :manage, User
-        end
-
-        it 'is successful' do
-          do_request
-          response.should be_success
-        end
-
-        it 'returns an array of user blood pressures' do
-          do_request
-          json = JSON.parse(response.body)
-          json['blood_pressures'].to_json.should == [blood_pressure.as_json].to_json
-        end
+      it 'returns an array of user blood pressures' do
+        do_request
+        json = JSON.parse(response.body)
+        json['blood_pressures'].to_json.should == [blood_pressure.as_json].to_json
       end
     end
   end
@@ -66,56 +42,30 @@ describe Api::V1::BloodPressuresController do
       user.stub(:blood_pressures => blood_pressures)
     end
 
-    it 'requires authentication' do
-      do_request
-      response.should_not be_success
-      json = JSON.parse(response.body)
-      json['reason'].should == "Invalid auth_token"
-    end
+    it_behaves_like 'action requiring authentication and authorization'
 
-    context 'user signed-in' do
-      before(:each) do
-        controller.stub(:authentication_check)
-        controller.stub(:current_user => user)
+    context 'authenticated and authorized', :user => :authenticate_and_authorize! do
+      it 'attempts to create the record' do
+        blood_pressures.should_receive(:create).once
+        do_request
       end
 
-      it 'requires User authorization' do
-        expect{ do_request }.to raise_error(CanCan::AccessDenied)
-      end
+      context 'save succeeds' do
+        it_behaves_like 'success'
 
-      context 'authorized' do
-        before(:each) do
-          ability.can :manage, User
-        end
-
-        it 'attempts to create the record' do
-          blood_pressures.should_receive(:create).once
+        it 'returns the blood pressure' do
           do_request
+          json = JSON.parse(response.body)
+          json['blood_pressure'].to_json.should == blood_pressure.as_json.to_json
+        end
+      end
+
+      context 'save fails' do
+        before(:each) do
+          blood_pressure.errors.add(:base, :invalid)
         end
 
-        context 'save succeeds' do
-          it 'returns success' do
-            do_request
-            response.should be_success
-          end
-
-          it 'returns the blood pressure' do
-            do_request
-            json = JSON.parse(response.body)
-            json['blood_pressure'].to_json.should == blood_pressure.as_json.to_json
-          end
-        end
-
-        context 'save fails' do
-          before(:each) do
-            blood_pressure.errors.add(:base, :invalid)
-          end
-
-          it 'returns failure' do
-            do_request
-            response.should_not be_success
-          end
-        end
+        it_behaves_like 'failure'
       end
     end
   end
@@ -130,56 +80,29 @@ describe Api::V1::BloodPressuresController do
       blood_pressure.stub(:destroy)
     end
 
-    it 'requires authentication' do
-      do_request
-      response.should_not be_success
-      json = JSON.parse(response.body)
-      json['reason'].should == "Invalid auth_token"
-    end
+    it_behaves_like 'action requiring authentication and authorization'
 
-    context 'user signed-in' do
-      before(:each) do
-        controller.stub(:authentication_check)
-        controller.stub(:current_user => user)
+    context 'authenticated and authorized', :user => :authenticate_and_authorize! do
+      it 'attempts to destroy the record' do
+        blood_pressure.should_receive(:destroy).once
+        do_request
       end
 
-      it 'requires User authorization' do
-        expect{ do_request }.to raise_error(CanCan::AccessDenied)
-      end
-
-      context 'authorized' do
+      context 'destroy succeeds' do
         before(:each) do
-          ability.can :manage, User
-          ability.can :manage, BloodPressure
+          blood_pressure.stub(:destroy => true)
         end
 
-        it 'attempts to destroy the record' do
-          blood_pressure.should_receive(:destroy).once
-          do_request
+        it_behaves_like 'success'
+      end
+
+      context 'destroy fails' do
+        before(:each) do
+          blood_pressure.stub(:destroy => false)
+          blood_pressure.errors.add(:base, :invalid)
         end
 
-        context 'destroy succeeds' do
-          before(:each) do
-            blood_pressure.stub(:destroy => true)
-          end
-
-          it 'returns success' do
-            do_request
-            response.should be_success
-          end
-        end
-
-        context 'destroy fails' do
-          before(:each) do
-            blood_pressure.stub(:destroy => false)
-            blood_pressure.errors.add(:base, :invalid)
-          end
-
-          it 'returns failure' do
-            do_request
-            response.should_not be_success
-          end
-        end
+        it_behaves_like 'failure'
       end
     end
   end
