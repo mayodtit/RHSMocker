fa8785fc5ac305ca1774dfad3842d6dcc4071961
Kyle Chilcutt <kyle@getbetter.com>
2013-07-02 17:55:25 -0700

    Clean up BloodPressure API doc
    Minimal acceptance test should be used for API generation and not test
    failure cases.  Need to add failure specs to request and add error block
    to API documentation.

--- spec/acceptance/api/v1/blood_pressures_spec.rb
+++ spec/acceptance/api/v1/blood_pressures_spec.rb
@@ -1,204 +1,71 @@
 require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
-resource "BloodPressures" do
+resource 'BloodPressures' do
   header 'Accept', 'application/json'
   header 'Content-Type', 'application/json'
 
-  before(:all) do
-    @user = FactoryGirl.create(:user_with_email)
-    @user.login
-    @user2 = FactoryGirl.create(:user_with_email)
-    @user2.login
-    @blood_pressure1 = FactoryGirl.create(:blood_pressure, :user=>@user)
-    @blood_pressure2 = FactoryGirl.create(:blood_pressure, :user=>@user)
+  let!(:user) { create(:user) }
+  let(:auth_token) { user.auth_token }
 
-    @associate = FactoryGirl.create(:associate)
-    @association = FactoryGirl.create(:association, :user=>@user, :associate=>@associate)
+  before(:each) do
+    user.login
   end
 
+  parameter :auth_token, "User's auth_token"
+  required_parameters :auth_token
 
-  get 'api/v1/blood_pressures' do
-    parameter :auth_token, "User's auth_token"
-    required_parameters :auth_token
+  get '/api/v1/users/:user_id/blood_pressures' do
+    parameter :user_id, 'User ID for which to get blood pressures'
+    required_parameters :user_id
 
-    let (:auth_token) { @user.auth_token }
-
-    example_request "[GET] Get all user's blood_pressures [DEPRECATED]" do
-      explanation "Returns an array of blood_pressures recorded by the user"
-      status.should == 200
-      JSON.parse(response_body)['blood_pressures'].should be_a Array
-    end
-  end
-
-  get 'api/v1/users/:user_id/blood_pressures' do
-    parameter :user_id, "User ID for which to get blood pressures"
-    parameter :auth_token, "User's auth_token"
-    required_parameters :auth_token
-
-    let(:auth_token) { @user.auth_token }
-    let(:user_id) { @user.id }
+    let(:user_id) { user.id }
+    let!(:blood_pressure) { create(:blood_pressure, :user => user) }
+    let(:raw_post) { params.to_json }
 
     example_request "[GET] Get all blood_pressures for a user" do
-      explanation "Returns an array of blood_pressures recorded by the user"
+      explanation 'Returns an array of blood_pressures recorded by the user'
       status.should == 200
       JSON.parse(response_body)['blood_pressures'].should be_a Array
     end
   end
 
-  describe 'create blood_pressure' do
-    parameter :auth_token,    "User's auth token"
-    parameter :diastolic,     "User's diastolic pressure"
-    parameter :systolic,      "User's systolic pressure"
-    parameter :pulse,         "User's pulse"
-    parameter :taken_at,         "DateTime of when the reading was taken"
-    parameter :collection_type_id,         "collection_type_id optional (will make it 'self-reported' by defaults)"
+  post '/api/v1/users/:user_id/blood_pressures' do
+    parameter :user_id, 'user for which to create a new record'
+    parameter :diastolic, "User's diastolic pressure"
+    parameter :systolic, "User's systolic pressure"
+    parameter :pulse, "User's pulse"
+    parameter :taken_at, "DateTime of when the reading was taken"
+    parameter :collection_type_id, "collection_type_id optional (will make it 'self-reported' by defaults)"
+    required_parameters :user_id, :diastolic, :systolic
 
-    required_parameters :auth_token, :diastolic, :systolic
+    let(:user_id) { user.id }
+    let(:diastolic) { 90 }
+    let(:systolic) { 91 }
+    let(:pulse) { 92 }
+    let(:taken_at) { DateTime.now-20.minutes }
+    let(:collection_type_id) { 1 }
+    let(:raw_post) { params.to_json }
 
-
-    post '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:diastolic)  { 90 }
-      let (:systolic)   { 91 }
-      let (:pulse)      { 92 }
-      let (:taken_at)      { DateTime.now-20.minutes }
-      let (:collection_type_id)      { 1 }
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[POST] Set user's blood pressure" do
-        explanation "Set the user's blood pressure"
-        status.should == 200
-        JSON.parse(response_body).should_not be_empty
-      end
-    end
-
-    post '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:diastolic)  { 90 }
-      let (:pulse)      { 92 }
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[POST] Set user's blood pressure b (412)" do
-        explanation "Set the user's blood pressure"
-        status.should == 412
-        JSON.parse(response_body)['reason'].should_not be_empty
-      end
+    example_request "[POST] Set user's blood pressure" do
+      explanation "Set the user's blood pressure"
+      status.should == 200
+      JSON.parse(response_body).should_not be_empty
     end
   end
 
+  delete '/api/v1/blood_pressures/:id' do
+    parameter :id, "Blood pressure reading id"
+    required_parameters :id
 
-  describe 'create blood_pressure for associate' do
-    parameter :auth_token,    "User's auth token"
-    parameter :diastolic,     "User's diastolic pressure"
-    parameter :systolic,      "User's systolic pressure"
-    parameter :pulse,         "User's pulse"
-    parameter :taken_at,         "DateTime of when the reading was taken"
-    parameter :collection_type_id,         "collection_type_id optional (will make it 'self-reported' by defaults)"
-    parameter :user_id,    "ID of the associate you want to add a blood_pressure reading to"
+    let!(:blood_pressure) { create(:blood_pressure, :user => user) }
+    let(:id) { blood_pressure.id }
+    let(:raw_post) { params.to_json }
 
-    required_parameters :auth_token, :diastolic, :systolic
-
-
-    post '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:diastolic)  { 90 }
-      let (:systolic)   { 91 }
-      let (:pulse)      { 92 }
-      let (:taken_at)      { DateTime.now-20.minutes }
-      let (:collection_type_id)      { 1 }
-      let(:user_id)       { @associate.id }
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[POST] Set associate's blood pressure" do
-        explanation "Set the associates's blood pressure"
-        status.should == 200
-        JSON.parse(response_body).should_not be_empty
-      end
-    end
-
-    post '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:diastolic)  { 90 }
-      let (:systolic)   { 91 }
-      let (:pulse)      { 92 }
-      let (:taken_at)      { DateTime.now-20.minutes }
-      let (:collection_type_id)      { 1 }
-      let(:user_id)       { 234234 }
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[POST] Set associate's blood pressure b (404)" do
-        explanation "Set the associate's blood pressure"
-        status.should == 404
-        JSON.parse(response_body)['reason'].should_not be_empty
-      end
-    end
-
-     post '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:diastolic)  { 90 }
-      let (:systolic)   { 91 }
-      let (:pulse)      { 92 }
-      let (:taken_at)      { DateTime.now-20.minutes }
-      let (:collection_type_id)      { 1 }
-      let(:user_id)       { @user2.id }
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[POST] Set associate's blood pressure b (401)" do
-        explanation "Set the associate's blood pressure"
-        status.should == 401
-        JSON.parse(response_body)['reason'].should_not be_empty
-      end
+    example_request "[DELETE] Remove user's blood pressure reading" do
+      explanation "Remove user's blood pressure reading"
+      status.should == 200
+      JSON.parse(response_body).should_not be_empty
     end
   end
-
-  describe 'delete blood_pressure' do
-    parameter :auth_token,    "User's auth token"
-    parameter :id,            "Blood pressure reading id"
-    parameter :blood_pressure,            "Blood pressure reading object"
-
-    required_parameters :auth_token, :id, :blood_pressure
-    scope_parameters :blood_pressure, [:id]
-
-    delete '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:id) { @blood_pressure1.id }
-
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[DELETE] Remove user's blood pressure reading" do
-        explanation "Remove user's blood pressure reading"
-        status.should == 200
-        JSON.parse(response_body).should_not be_empty
-      end
-    end
-
-    delete '/api/v1/blood_pressures' do
-      let (:auth_token) { @user.auth_token }
-      let (:id) { 2344 }
-
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[DELETE] Remove user's blood pressure reading b (404)" do
-        explanation "Remove user's blood pressure reading"
-        status.should == 404
-        JSON.parse(response_body).should_not be_empty
-      end
-    end
-
-    delete '/api/v1/blood_pressures' do
-      let (:auth_token) { @user2.auth_token }
-      let (:id) { @blood_pressure2.id }
-
-      let (:raw_post)   { params.to_json }  # JSON format request body
-
-      example_request "[DELETE] Remove user's blood pressure reading c (401)" do
-        explanation "Remove user's blood pressure reading"
-        status.should == 401
-        JSON.parse(response_body).should_not be_empty
-      end
-    end
-  end
-
-
 end
