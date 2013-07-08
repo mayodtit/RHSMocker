50905f457fae75200dfae1b395a7e4efdbc5d591
Kyle Chilcutt <kyle@getbetter.com>
2013-07-08 11:04:03 -0700

    Attempt to fix string to integer column conversion
    Need to use a USING clause, fall back to SQL.

--- db/migrate/20130704005446_update_ethnic_group_columns.rb
+++ db/migrate/20130704005446_update_ethnic_group_columns.rb
@@ -1,8 +1,10 @@
 class UpdateEthnicGroupColumns < ActiveRecord::Migration
   def up
     change_column :ethnic_groups, :name, :string, :null => false, :default => ''
-    change_column :ethnic_groups, :ethnicity_code, :integer, :null => false, :default => 0
     change_column :ethnic_groups, :ordinal, :integer, :null => false, :default => 0
+    connection.execute(%q{ALTER TABLE ethnic_groups
+                          ALTER COLUMN ethnicity_code
+                          TYPE integer USING (trim(ethnicity_code)::integer)})
   end
 
   def down
