056a743dc62276fe0ea420d1ea0ebf5af129b3d3
Kyle Chilcutt <kyle@getbetter.com>
2013-07-15 16:24:21 -0700

    Add DelayedJob installation
    Startup script and database tables.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130724224321) do
+ActiveRecord::Schema.define(:version => 20130724233739) do
 
   create_table "agreement_pages", :force => true do |t|
     t.text     "content"
@@ -146,6 +146,22 @@ ActiveRecord::Schema.define(:version => 20130724224321) do
 
   add_index "contents_symptoms_factors", ["content_id"], :name => "index_contents_symptom_factors_on_content_id"
   add_index "contents_symptoms_factors", ["symptoms_factor_id"], :name => "index_contents_symptom_factors_on_symptom_factor_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "diets", :force => true do |t|
     t.string   "name",        :default => "", :null => false
