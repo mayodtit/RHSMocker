90a492f684a2986513bd791dc8cb1b4d5c60e0c2
Kyle Chilcutt <kyle@getbetter.com>
2013-07-16 12:04:38 -0700

    Refactor remaining model specs to use helpers
    Use new model helpers in existing specs.

--- spec/models/user_disease_treatment_side_effect_spec.rb
+++ spec/models/user_disease_treatment_side_effect_spec.rb
@@ -1,32 +1,12 @@
 require 'spec_helper'
 
 describe UserDiseaseTreatmentSideEffect do
-  let(:udtse) { build(:user_disease_treatment_side_effect) }
-
   # TODO - this is a hack to eliminate false positives resulting from dirty database
   before(:each) do
     User.delete_all
   end
 
-  describe 'factory' do
-    it 'creates a valid object' do
-      udtse.should be_valid
-      udtse.save.should be_true
-      udtse.should be_persisted
-    end
-  end
-
-  describe 'validations' do
-    it 'requires a user_disease_treatment' do
-      udtse.should be_valid
-      udtse.user_disease_treatment = nil
-      udtse.should_not be_valid
-    end
-
-    it 'requires a side_effect' do
-      udtse.should be_valid
-      udtse.side_effect = nil
-      udtse.should_not be_valid
-    end
-  end
+  it_has_a 'valid factory'
+  it_validates 'presence of', :user_disease_treatment
+  it_validates 'presence of', :side_effect
 end
