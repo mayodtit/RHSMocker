b8df061295621581badd519d435c5153e0954d61
Kyle Chilcutt <kyle@getbetter.com>
2013-07-15 20:39:44 -0700

    Add shared examples for common tests
    Eliminate the repetition.

---
+++ spec/support/model_helpers.rb
@@ -0,0 +1,17 @@
+shared_examples 'valid factory' do
+  it 'builds a valid object' do
+    model = build(described_class.name.underscore.to_sym)
+    model.should be_valid
+    model.save.should be_true
+    model.should be_persisted
+  end
+end
+
+shared_examples 'presence of' do |property|
+  it "requires a value for #{property}" do
+    model = build_stubbed(described_class.name.underscore.to_sym)
+    model.send(:"#{property}=", nil)
+    model.should_not be_valid
+    model.errors[property.to_sym].should include("can't be blank")
+  end
+end
