424080f43c691da5ba5eda7a9c153e05ecfbb7e0
Kyle Chilcutt <kyle@getbetter.com>
2013-07-16 11:45:15 -0700

    Add support helper for scoped uniqueness
    These specs will be pretty common, pull out a helper for them.

--- spec/models/user_allergy_spec.rb
+++ spec/models/user_allergy_spec.rb
@@ -4,11 +4,5 @@ describe UserAllergy do
   it_has_a 'valid factory'
   it_validates 'presence of', :user
   it_validates 'presence of', :allergy
-
-  it 'allows only unique allergies for a user' do
-    ua = create(:user_allergy)
-    ua2 = build(:user_allergy, :user => ua.user, :allergy => ua.allergy)
-    ua2.should_not be_valid
-    ua2.should have(1).error_on(:allergy_id)
-  end
+  it_validates 'scoped uniqueness of', :allergy_id, :user_id
 end
--- spec/support/model_helpers.rb
+++ spec/support/model_helpers.rb
@@ -15,3 +15,13 @@ shared_examples 'presence of' do |property|
     model.errors[property.to_sym].should include("can't be blank")
   end
 end
+
+shared_examples 'scoped uniqueness of' do |property, scope|
+  it "requires a value for #{property}" do
+    model = create(described_class.name.underscore.to_sym)
+    duplicate = build_stubbed(described_class.name.underscore.to_sym, property => model.send(property),
+                                                                      scope => model.send(scope))
+    duplicate.should_not be_valid
+    duplicate.errors[property.to_sym].should include("has already been taken")
+  end
+end
