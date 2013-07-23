83e378e0738af0c646571521823c5d2b727914ec
Kyle Chilcutt <kyle@getbetter.com>
2013-07-23 11:06:20 -0700

    Only create sessions for Members
    Do not use the User class to make new sessions.

--- app/controllers/api/V1/sessions_controller.rb
+++ app/controllers/api/V1/sessions_controller.rb
@@ -7,11 +7,11 @@ class Api::V1::SessionsController < Api::V1::ABaseController
       return render_failure({reason:"Incorrect credentials"}, 401) unless user
       if params[:auth_token] && params[:auth_token]!=user.auth_token
         #merge user_readings
-        original_user = User.find_by_auth_token params[:auth_token]
+        original_user = Member.find_by_auth_token params[:auth_token]
         user.merge(original_user) if original_user
       end
     elsif params[:install_id]
-      user = User.find_by_install_id(params[:install_id])
+      user = Member.find_by_install_id(params[:install_id])
       if user.present?
         if user.email.present?
           user = nil
