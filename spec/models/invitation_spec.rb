b3c7086a133a89f6067467e49763b13887e2b8bf
Kyle Chilcutt <kyle@getbetter.com>
2013-07-24 14:25:49 -0700

    Fix Invitations spec and factories
    Make a new factory to build all the invitations things!

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -4,5 +4,5 @@ describe Invitation do
   it_has_a 'valid factory'
   it_validates 'presence of', :member
   it_validates 'presence of', :invited_member
-  it_validates 'scoped uniqueness of', :member_id, :invited_member_id
+  it_validates 'scoped uniqueness of', :invited_member_id, :member_id
 end
