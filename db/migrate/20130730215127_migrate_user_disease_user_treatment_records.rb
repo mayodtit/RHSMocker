7ac99febf32e3450fa731acbce71c06c98e313aa
Kyle Chilcutt <kyle@getbetter.com>
2013-08-05 18:16:27 -0500

    Only search for a UserDisease if one was assigned
    Fix migration for unlinked UserDiseaseTreatment case.

--- db/migrate/20130730215127_migrate_user_disease_user_treatment_records.rb
+++ db/migrate/20130730215127_migrate_user_disease_user_treatment_records.rb
@@ -1,7 +1,7 @@
 class MigrateUserDiseaseUserTreatmentRecords < ActiveRecord::Migration
   def up
     UserDiseaseTreatment.find_each do |udt|
-      udt.user_diseases << UserDisease.find(udt.user_disease_id)
+      udt.user_diseases << UserDisease.find(udt.user_disease_id) if udt.user_disease_id
     end
 
     remove_column :user_disease_treatments, :user_disease_id
