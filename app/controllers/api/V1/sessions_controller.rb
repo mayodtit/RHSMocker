202d63537c869adf76f847bafac52932bb3f5940
Kyle Chilcutt <kyle@getbetter.com>
2013-08-28 09:45:34 -0700

    Simplify SessionsController
    Remove login with install_id and simplify branching logic allowed for
    login.

--- app/controllers/api/V1/sessions_controller.rb
+++ app/controllers/api/V1/sessions_controller.rb
@@ -2,36 +2,16 @@ class Api::V1::SessionsController < Api::V1::ABaseController
   skip_before_filter :authentication_check, :only =>:create
 
   def create
-    if params[:email] && params[:password]
-      user = login(params[:email], params[:password])
-      return render_failure({reason:"Incorrect credentials", user_message: 'Invalid email and/or password'}, 401) unless user
-      if params[:auth_token] && params[:auth_token]!=user.auth_token
-        #merge user_readings
-        original_user = Member.find_by_auth_token params[:auth_token]
-        user.merge(original_user) if original_user
-      end
-    elsif params[:install_id]
-      user = Member.find_by_install_id(params[:install_id])
-      if user.present?
-        if user.email.present?
-          user = nil
-        else
-          auto_login(user)
-        end
-      end
-    end
-
-    if user.blank?
-      render_failure({reason:"Incorrect credentials", user_message: 'Invalid email and/or password'}, 401) unless performed?
+    @user = login(params[:email], params[:password])
+    if @user.try_method(:login)
+      render_success(auth_token: @user.auth_token, user: @user)
     else
-      user.login
-      render_success(auth_token:user.auth_token, user:user)
+      render_failure({reason:"Incorrect credentials", user_message: 'Invalid email and/or password'}, 401)
     end
   end
 
   def destroy
-    current_user.auth_token = nil
-    current_user.save
+    current_user.logout
     logout
     render_success
   end
