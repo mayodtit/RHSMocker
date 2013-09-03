f82a7d9e58c63134bb7d0043b86fc51fc8cf815f
Kyle Chilcutt <kyle@getbetter.com>
2013-09-03 08:20:45 -0700

    Clean up Allergy model
    Add specs, remove unnecessary as_json override.  Standardize factory.

--- spec/factories/allergy_factory.rb
+++ spec/factories/allergy_factory.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  factory :allergy do   
-    sequence(:name) { |n| "dust #{n}" }
-    sequence(:snomed_name) { |n| "fluffy stuff #{n}" }
-    sequence(:snomed_code) { |n| "#{n+100}" }
+  factory :allergy do
+    sequence(:name) { |n| "Allergy #{n}" }
+    sequence(:snomed_name) { |n| "Allergy #{n} SNOMED Name" }
+    sequence(:snomed_code) { |n| "#{n + 100}" }
     food_allergen { [true, false].sample }
     environment_allergen { [true, false].sample }
     medication_allergen { [true, false].sample }
