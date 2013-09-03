b8b7f4b8155aa968dc54e9c7fd11dcab8353cc07
Tiem Song <tiem@songfamily.org>
2013-09-03 14:19:27 -0700

    create method to assign s3 bucket based on environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,7 @@
+def s3_bucket
+  ENV['S3_BUCKET'] || 'btr01-devlocal'
+end
+
 CarrierWave.configure do |config|
   config.fog_credentials = {
 
@@ -12,7 +16,7 @@ CarrierWave.configure do |config|
       #:endpoint               => 'https://s3.example.com:8080' # defaults to nil
   }
 
-  config.fog_directory  = 'btr01'                                 # required. s3 bucket name
+  config.fog_directory  = s3_bucket                               # required. s3 bucket name
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
