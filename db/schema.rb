04bcfc0683c01faee1298dd70f1d2efd58ba43e4
Geoffrey Clapp <geoff.clapp@gmail.com>
2013-09-16 14:10:49 -0700

    [#54223958] Schema file for migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130915203422) do
+ActiveRecord::Schema.define(:version => 20130916193956) do
 
   create_table "agreement_pages", :force => true do |t|
     t.text     "content"
@@ -150,14 +150,16 @@ ActiveRecord::Schema.define(:version => 20130915203422) do
   create_table "contents", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.string   "content_type"
     t.text     "abstract"
     t.text     "question"
     t.text     "keywords"
     t.datetime "content_updated_at"
     t.string   "mayo_doc_id"
+    t.boolean  "show_call_option"
+    t.boolean  "show_checker_option"
   end
 
   create_table "contents_symptoms", :force => true do |t|
