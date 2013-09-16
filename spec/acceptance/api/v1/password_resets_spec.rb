5c8cf874fc3d8be7eec879914b8fa3b558159a21
Kyle Chilcutt <kyle@getbetter.com>
2013-09-16 15:16:24 -0700

    Update API documentation for reset password
    Deprecate the old path and add specs for the new one.

--- spec/acceptance/api/v1/password_resets_spec.rb
+++ spec/acceptance/api/v1/password_resets_spec.rb
@@ -14,24 +14,10 @@ resource "PasswordResets" do
     let(:email)     { user.email }
     let(:raw_post)  { params.to_json }  # JSON format request body
 
-    example_request "[POST] Reset password (forgot password)" do
+    example_request "[DEPRECATED] [POST] Reset password (forgot password)" do
       explanation "Emails password reset instructions to the user"
       status.should == 200
       JSON.parse(response_body).should_not be_empty
-    end
-  end
-
-  post '/api/v1/password_resets' do
-    parameter :email, "User's email address"
-    required_parameters :email
-
-    let(:email)     { 'email_does_not_exist@fake.com' }
-    let(:raw_post)  { params.to_json }  # JSON format request body
-
-    example_request "[POST] Reset password (forgot password) (404)" do
-      explanation "Emails password reset instructions to the user"
-      status.should == 404
-      JSON.parse(response_body)['reason'].should_not be_empty
     end
   end
 end
