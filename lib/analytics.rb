d4ea21b01bc7166b731569078da095788ca508d8
Tiem Song <tiem@songfamily.org>
2013-09-18 17:41:52 -0700

    new analytics logging methods should log asynchronously

--- lib/analytics.rb
+++ lib/analytics.rb
@@ -16,18 +16,22 @@ class Analytics
     def log_started_symptoms_checker(user_ga_uuid)
       LogAnalyticsJob.new(user_ga_uuid, 'started_symptom_checker').log_ga
     end
+    handle_asynchronously :log_started_symptoms_checker
 
     def log_user_login(user_ga_uuid)
       LogAnalyticsJob.new(user_ga_uuid, 'user_logged_in').log_ga
     end
+    handle_asynchronously :log_user_login
 
     def log_user_logout(user_ga_uuid)
       LogAnalyticsJob.new(user_ga_uuid, 'user_logged_out').log_ga
     end
+    handle_asynchronously :log_user_logout
 
     def log_content_search(user_ga_uuid, search_term)
       event_data = {event_label: search_term, event_category: SEARCH_CATEGORY}
       LogAnalyticsJob.new(user_ga_uuid, 'content_search', event_data).log_ga
     end
+    handle_asynchronously :log_content_search
   end
 end
