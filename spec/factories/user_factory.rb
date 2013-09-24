e680f045d2a0d885612854003e9ea518276970bc
Kyle Chilcutt <kyle@getbetter.com>
2013-09-24 16:09:00 -0700

    Fix Member factory to always have email
    Validations require email, provide email.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,21 +3,18 @@ FactoryGirl.define do
     first_name            { ["Harold", "Kumar", "Alice", "Bob"].sample }
     last_name             { ["Lee", "Patel", "Carol", "Dan"].sample }
 
-    factory :member, class: Member do
+    factory :member, class: Member, aliases: [:user_with_email] do
       sequence(:install_id) { |n| n }
+      sequence(:email)    { |n| "user#{n}@test.com" }
       password              "password"
       password_confirmation "password"
 
-      factory :user_with_email do
-        sequence(:email)    { |n| "user#{n}@test.com" }
+      factory :admin do
+        after(:create) {|user| user.add_role(:admin)}
+      end
 
-        factory :admin do
-          after(:create) {|user| user.add_role(:admin)}
-        end
-
-        factory :hcp_user do
-          after(:create) {|user| user.add_role(:hcp)}
-        end
+      factory :hcp_user do
+        after(:create) {|user| user.add_role(:hcp)}
       end
     end
   end
