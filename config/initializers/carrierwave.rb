db8df960632d8881baa03f8edbc6d4b0bb16f22f
Tiem Song <tiem@songfamily.org>
2013-09-24 15:04:13 -0700

    DRY up references to S3 bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,3 @@
-def s3_bucket
-  ENV['S3_BUCKET'] || 'btr01-devlocal'
-end
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
 
@@ -16,7 +12,7 @@ CarrierWave.configure do |config|
       #:endpoint               => 'https://s3.example.com:8080' # defaults to nil
   }
 
-  config.fog_directory  = s3_bucket                               # required. s3 bucket name
+  config.fog_directory  = S3_BUCKET                               # required. s3 bucket name
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
