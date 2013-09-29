6235fb85a841bba529b99fdffddafc000fde833c
Kyle Chilcutt <kyle@getbetter.com>
2013-09-29 10:44:40 -0700

    Restore some comments to ContentDecorator
    So developers know where to find things.

--- app/decorators/content_decorator.rb
+++ app/decorators/content_decorator.rb
@@ -1,10 +1,12 @@
 class ContentDecorator < Draper::Decorator
   delegate_all
 
+  # card preview, first 100 words and strip out Definition header
   def preview
     body.split(' ').slice(0, 101).join(' ').gsub(/\ADefinition<p>/, "")
   end
 
+  # insert the "would you like to talk to someone" text
   def formatted_body
     if !show_call_option?
       body
