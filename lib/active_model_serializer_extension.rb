9c8c148ea5a9674761673a3a3e5b9e94a144ac4b
Kyle Chilcutt <kyle@getbetter.com>
2013-10-01 11:37:40 -0700

    Add ActiveModelSerializerExtension
    Get a serializer directly from an object instance.  Needed because we
    nest our object hashes before we render them to json.

---
+++ lib/active_model_serializer_extension.rb
@@ -0,0 +1,10 @@
+module ActiveModelSerializerExtension
+  extend ActiveSupport::Concern
+
+  def active_model_serializer_instance
+    active_model_serializer.try(:new, self)
+  end
+end
+
+ActiveRecord::Base.send(:include, ActiveModelSerializerExtension)
+ActiveRecord::Relation.send(:include, ActiveModelSerializerExtension)
