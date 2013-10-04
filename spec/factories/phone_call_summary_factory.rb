4f3b287f553fd3bf5794c3917416ffc0d94e269b
Kyle Chilcutt <kyle@getbetter.com>
2013-10-04 14:16:17 -0700

    Fix PhoneCallSummarySpec
    Update the factory to create valid objects with the new validations.

--- spec/factories/phone_call_summary_factory.rb
+++ spec/factories/phone_call_summary_factory.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :phone_call_summary do
     association :caller, :factory => :member
     association :callee, :factory => :member
+    message { association(:message, :user => callee) }
     body 'Phone call summary text'
   end
 end
