cab47b523e1d7d55e1ecce125a7d8c9c821a1fb7
Tiem Song <tiem@songfamily.org>
2013-10-04 18:04:35 -0700

    use like/unlike instead of integer - can cause potential boolean conflicts.  also add attr_accessibles

--- spec/models/user_content_like_spec.rb
+++ spec/models/user_content_like_spec.rb
@@ -3,34 +3,34 @@ require 'spec_helper'
 describe UserContentLike do
   describe 'validations' do
     it 'should only allow a unique user_id + content_id pair' do
-      create(:user_content_like, user_id: 1, content_id: 1, action: 1)
-      create(:user_content_like, user_id: 1, content_id: 2, action: 1)
-      create(:user_content_like, user_id: 2, content_id: 1, action: 1)
+      create(:user_content_like, user_id: 1, content_id: 1, action: 'like')
+      create(:user_content_like, user_id: 1, content_id: 2, action: 'like')
+      create(:user_content_like, user_id: 2, content_id: 1, action: 'like')
 
-      expect { create(:user_content_like, user_id: 1, content_id: 1, action: 1) }.to raise_error
-      expect { create(:user_content_like, user_id: 2, content_id: 1, action: 1) }.to raise_error
+      expect { create(:user_content_like, user_id: 1, content_id: 1, action: 'like') }.to raise_error
+      expect { create(:user_content_like, user_id: 2, content_id: 1, action: 'like') }.to raise_error
 
       # user should not be able to both like and unlike a content object
-      expect { create(:user_content_like, user_id: 1, content_id: 1, action: -1) }.to raise_error
-      expect { create(:user_content_like, user_id: 2, content_id: 1, action: -1) }.to raise_error
+      expect { create(:user_content_like, user_id: 1, content_id: 1, action: 'dislike') }.to raise_error
+      expect { create(:user_content_like, user_id: 2, content_id: 1, action: 'dislike') }.to raise_error
     end
 
     it 'should only create if user_id, content_id, and action are present' do
-      create(:user_content_like, user_id: 1, content_id: 1, action: 1)
+      create(:user_content_like, user_id: 1, content_id: 1, action: 'like')
 
       expect { create(:user_content_like) }.to raise_error
       expect { create(:user_content_like, user_id: 2) }.to raise_error
       expect { create(:user_content_like, content_id: 2) }.to raise_error
-      expect { create(:user_content_like, action: 2) }.to raise_error
+      expect { create(:user_content_like, action: 'like') }.to raise_error
       expect { create(:user_content_like, user_id: 2, content_id: 2) }.to raise_error
-      expect { create(:user_content_like, user_id: 2, action: 2) }.to raise_error
-      expect { create(:user_content_like, content_id: 2, action: 2) }.to raise_error
+      expect { create(:user_content_like, user_id: 2, action: 'like') }.to raise_error
+      expect { create(:user_content_like, content_id: 2, action: 'like') }.to raise_error
     end
 
     it 'should only allow 1 or -1 for action' do
-      create(:user_content_like, user_id: 1, content_id: 1, action: 1)
-      create(:user_content_like, user_id: 2, content_id: 2, action: -1)
-      expect { create(:user_content_like, user_id: 3, content_id: 3, action: 2) }.to raise_error
+      create(:user_content_like, user_id: 1, content_id: 1, action: 'like')
+      create(:user_content_like, user_id: 2, content_id: 2, action: 'dislike')
+      expect { create(:user_content_like, user_id: 3, content_id: 3, action: 'foobar') }.to raise_error
     end
   end
 end
