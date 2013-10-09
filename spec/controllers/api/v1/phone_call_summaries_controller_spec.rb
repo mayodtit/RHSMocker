564564a4564cb6e082f967d7780b89ecfb1ca9cd
Kyle Chilcutt <kyle@getbetter.com>
2013-10-09 14:41:43 -0700

    Add controller specs for PhoneCallSummaries
    Test basic authentication and authorization.

---
+++ spec/controllers/api/v1/phone_call_summaries_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Api::V1::PhoneCallSummariesController do
+  let(:phone_call_summary) { build_stubbed(:phone_call_summary) }
+  let(:user) { phone_call_summary.callee }
+  let(:ability) { Object.new.extend(CanCan::Ability) }
+
+  before(:each) do
+    controller.stub(:current_ability => ability)
+    PhoneCallSummary.stub(:find => phone_call_summary)
+  end
+
+  describe 'GET show' do
+    def do_request
+      get :show, auth_token: user.auth_token
+    end
+
+    it_behaves_like 'action requiring authentication and authorization'
+    context 'authenticated and authorized', :user => :authenticate_and_authorize! do
+      it_behaves_like 'success'
+
+      it 'returns the phone_call_summary' do
+        do_request
+        json = JSON.parse(response.body, :symbolize_names => true)
+        json[:phone_call_summary][:id].should == phone_call_summary.as_json['id']
+      end
+    end
+  end
+end
