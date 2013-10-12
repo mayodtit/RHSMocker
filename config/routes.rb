29f9d519f69eea7ec416d09497987e7090074898
Kyle Chilcutt <kyle@getbetter.com>
2013-10-11 17:18:26 -0700

    Remove UserReadingsController, specs, and routes
    Out with the old.  Remove soon-to-be-unused code.

--- config/routes.rb
+++ config/routes.rb
@@ -84,14 +84,6 @@ RHSMocker::Application.routes.draw do
       post "user/update_password" => "users#update_password", :as=>"update_password"
       post "user/update_email" => "users#update_email", :as=>"update_email"
 
-      #reading list
-      get "user_readings" => "user_readings#index", :as=>"user_readings_index"
-      get "inbox(/:page)(/:per_page)" => "user_readings#inbox", :as=>"inbox"
-      post "contents/mark_read" => "user_readings#mark_read", :as=>"contents_mark_read"
-      post "contents/dismiss" => "user_readings#dismiss", :as=>"contents_dismiss"
-      post "contents/save" => "user_readings#save", :as=>"contents_read_later"
-      post "contents/reset" => "user_readings#reset", :as=>"contents_reset"
-
       get "factors/:id" => "factors#index"
       post "symptoms/check" => "factors#check"
     end
