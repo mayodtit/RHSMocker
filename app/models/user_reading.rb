ebf71aecabcc36a4cde3ee02653e15ab5d34da02
Kyle Chilcutt <kyle@getbetter.com>
2013-10-14 11:41:35 -0700

    Add incrementors to UserReading
    Helpers for finding, creating, and incrementing counts.

--- app/models/user_reading.rb
+++ app/models/user_reading.rb
@@ -6,4 +6,32 @@ class UserReading < ActiveRecord::Base
 
   validates :user, :content, :view_count, :save_count, :dismiss_count, :share_count, presence: true
   validates :content_id, :uniqueness => {:scope => :user_id}
+
+  def self.increment_view!(user, content)
+    increment_attribute!(user, content, :view_count)
+  end
+
+  def self.increment_save!(user, content)
+    increment_attribute!(user, content, :save_count)
+  end
+
+  def self.increment_dismiss!(user, content)
+    increment_attribute!(user, content, :dismiss_count)
+  end
+
+  def self.increment_share!(user, content)
+    increment_attribute!(user, content, :share_count)
+  end
+
+  private
+
+  def self.increment_attribute!(user, content, type)
+    ur = where(:user_id => user.id, :content_id => content.id).first_or_initialize
+    ur.instance_eval{ increment_attribute!(type) }
+  end
+
+  def increment_attribute!(type)
+    increment(type).update_attributes!(type => self[type])
+    self
+  end
 end
--- spec/models/user_reading_spec.rb
+++ spec/models/user_reading_spec.rb
@@ -8,4 +8,41 @@ describe UserReading do
   it_validates 'presence of', :save_count
   it_validates 'presence of', :dismiss_count
   it_validates 'presence of', :share_count
+
+  describe 'incrementors' do
+    let(:user) { create(:member) }
+    let(:content) { create(:content) }
+
+    describe '::increment_view!' do
+      it 'increments view_count and saves the record' do
+        ur = described_class.increment_view!(user, content)
+        ur.view_count.should == 1
+        ur.should be_persisted
+      end
+    end
+
+    describe '::increment_save!' do
+      it 'increments save_count and saves the record' do
+        ur = described_class.increment_save!(user, content)
+        ur.save_count.should == 1
+        ur.should be_persisted
+      end
+    end
+
+    describe '::increment_dismiss!' do
+      it 'increments dismiss_count and saves the record' do
+        ur = described_class.increment_dismiss!(user, content)
+        ur.dismiss_count.should == 1
+        ur.should be_persisted
+      end
+    end
+
+    describe '::increment_share!' do
+      it 'increments share_count and saves the record' do
+        ur = described_class.increment_share!(user, content)
+        ur.share_count.should == 1
+        ur.should be_persisted
+      end
+    end
+  end
 end
