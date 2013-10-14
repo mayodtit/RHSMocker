032156faafac2d696e000152151b1487883b8cfa
Kyle Chilcutt <kyle@getbetter.com>
2013-10-14 16:54:54 -0700

    Clean up Plan
    Model and spec.  Simplify and add validations.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,15 +1,11 @@
 class Plan < ActiveRecord::Base
-  attr_accessible :monthly, :name
-
   has_many :plan_offerings
   has_many :offerings, :through => :plan_offerings
+  has_many :subscriptions
+  has_many :users, :through => :subscriptions
 
-  def as_json options=nil
-    {
-      id: id,
-      name: name,
-      monthly: monthly,
-      offerings: Offering.with_credits(plan_offerings.with_offering_counts)
-    }
-  end
+  attr_accessible :name, :monthly
+
+  validates :name, presence: true, uniqueness: true
+  validates :monthly, :inclusion => {:in => [true, false]}
 end
