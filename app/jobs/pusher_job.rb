fd8e2aa206f936c5299bbb7f3fc77de8bf3be35e
Kyle Chilcutt <kyle@getbetter.com>
2013-10-15 23:26:24 -0700

    Remove push_status from PusherJob
    Unused.  The client receives the status change in the response to the
    update request.

--- app/jobs/pusher_job.rb
+++ app/jobs/pusher_job.rb
@@ -11,11 +11,4 @@ class PusherJob
     PusherModule.broadcast(user.pusher_id, 'newcontent', content.id, content.content_type)
   end
   handle_asynchronously :push_content
-
-  def push_status(user_id, user_reading_id, status)
-    user = Member.find(user_id)
-    user_reading = user.user_readings.find(user_reading_id)
-    PusherModule.broadcast(user.pusher_id, status, user_reading.content_id, user_reading.content.content_type)
-  end
-  handle_asynchronously :push_status
 end
