f8d593d33266e57b45e14220f577ef9e806fa1e7
Kyle Chilcutt <kyle@getbetter.com>
2013-10-22 12:29:50 -0400

    Track content statistics in UserReading
    Increment save and dismiss counts after state transitions.

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,11 +10,12 @@ class Card < ActiveRecord::Base
   validates :state_changed_at, presence: true, unless: :unread?
 
   before_validation :set_default_priority
+  after_create :create_user_reading, :if => lambda{|c| c.content_card? }
 
   delegate :title, :content_type, :preview, :abstract, :body, :formatted_body, to: :resource
 
   def self.inbox
-    where(:state => [:unread, :read]).by_priority.reject {|c| c.resource.content_type.downcase == 'disease' if c.resource_type == 'Content'}
+    where(:state => [:unread, :read]).by_priority.reject {|c| c.resource.content_type.downcase == 'disease' if c.content_card? }
   end
 
   def self.timeline
@@ -42,6 +43,10 @@ class Card < ActiveRecord::Base
     resource.try_method(:root_share_url).try(:+, "/#{id}")
   end
 
+  def content_card?
+    resource_type.constantize == Content
+  end
+
   private
 
   def set_default_priority
@@ -50,6 +55,10 @@ class Card < ActiveRecord::Base
     else
       self.priority ||= 0
     end
+  end
+
+  def create_user_reading
+    UserReading.where(:user_id => user.id, :content_id => resource.id).first_or_create!
   end
 
   # TODO - hack this in so the client doesn't have to change field names yet
@@ -91,6 +100,14 @@ class Card < ActiveRecord::Base
 
     before_transition any => :unread do |card, transition|
       card.state_changed_at = nil
+    end
+
+    after_transition any => :saved do |card, transition|
+      UserReading.increment_save!(card.user, card.resource) if card.content_card?
+    end
+
+    after_transition any => :dismissed do |card, transition|
+      UserReading.increment_dismiss!(card.user, card.resource) if card.content_card?
     end
 
     after_transition any => [:saved, :dismissed] do |card, transition|
