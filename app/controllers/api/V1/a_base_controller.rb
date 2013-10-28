dcaa3c608a17915bdaa041140176a44bcaf4a3a0
Abhik Pramanik <abhik@getbetter.com>
2013-10-28 16:12:01 -0700

    Removing 401 authenticate header when requests come from the nurse portal.  - Prevents browser authentication window from popping up

--- app/controllers/api/V1/a_base_controller.rb
+++ app/controllers/api/V1/a_base_controller.rb
@@ -17,7 +17,10 @@ module Api
       end
 
       def render_failure resp=Hash.new, status=401
-        headers["WWW-Authenticate"] = %(Basic realm="Better") if status == 401
+        if status == 401 && request.headers['Authorization'] != 'BasicCustom'
+          headers["WWW-Authenticate"] = %(Basic realm="Better")
+        end
+
         json = {status_code: status, status:"failure", user_message:""}.merge(resp).as_json
         render :json => json, :status => status
       end
