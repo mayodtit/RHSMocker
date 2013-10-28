401fc648bfb0d8b972aece1fa7e38deecaf210ac
Kyle Chilcutt <kyle@getbetter.com>
2013-10-28 08:50:13 -0700

    Add Program model
    A Program will have many CustomCards and Content resources.  A resource
    will be an object that can be rendered as a Card by the system.
    
    Conflicts:
    	db/schema.rb

---
+++ app/models/program.rb
@@ -0,0 +1,5 @@
+class Program < ActiveRecord::Base
+  attr_accessible :title
+
+  validates :title, presence: true
+end
---
+++ spec/factories/program_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :program do
+    title "Program title"
+  end
+end
