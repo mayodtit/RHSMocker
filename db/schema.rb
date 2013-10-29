3569ea2db3d204b6ac8df81a579df4f1fb154e47
Tiem Song <tiem@getbetter.com>
2013-10-29 16:28:59 -0700

    Don't set default value for body column on contents table
    [finishes #59799558}

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131029192002) do
+ActiveRecord::Schema.define(:version => 20131029232302) do
 
   create_table "agreements", :force => true do |t|
     t.text     "text"
@@ -130,7 +130,7 @@ ActiveRecord::Schema.define(:version => 20131029192002) do
 
   create_table "contents", :force => true do |t|
     t.string   "title",               :default => "",   :null => false
-    t.text     "body",                :default => "",   :null => false
+    t.text     "body"
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
     t.string   "content_type",        :default => "",   :null => false
