c7729d249bbb73c699d9060c47300ebe5a4b3cdb
Geoffrey Clapp <geoff.clapp@gmail.com>
2013-10-31 16:18:12 -0700

    [#59010562] Add in selfcare model to SymptomsAPI

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,6 +1,7 @@
 class Symptom < ActiveRecord::Base
   has_many :symptoms_factors
   has_many :factors, through: :symptoms_factors
+  has_one  :symptom_selfcare
   has_and_belongs_to_many :contents
 
   attr_accessible :name, :patient_type, :description, :selfcare, :gender
