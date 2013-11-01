d7e6d1e3cc781ae39aee69993084a73776de13f4
Tiem Song <tiem@songfamily.org>
2013-11-01 16:28:56 -0700

    remove unused specs and factories

--- spec/factories/symptom_selfcare_items.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :symptom_selfcare_item do
-    description "MyString"
-    symptom_selfcare nil
-  end
-end
