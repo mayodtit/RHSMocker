7903e1833e36d71b99e2098d2b8381938379e909
Kyle Chilcutt <kyle@getbetter.com>
2013-11-11 11:17:26 -0800

    Prevent creation of duplicate associations
    Validate uniqueness of user associations by type.
    
    [Finishes #60435876]

--- spec/models/association_spec.rb
+++ spec/models/association_spec.rb
@@ -1,32 +1,15 @@
 require 'spec_helper'
 
 describe Association do
-  describe 'factory' do
-    let(:association) { build(:association) }
-
-    it 'creates a valid object' do
-      association.should be_valid
-      association.save.should be_true
-      association.should be_persisted
-    end
-  end
-
+  it_has_a 'valid factory'
   describe 'validations' do
-    it 'requires a user' do
-      build_stubbed(:association, user: nil).should_not be_valid
-    end
-
-    it 'requires a associate' do
-      build_stubbed(:association, associate: nil).should_not be_valid
-    end
-
-    it 'requires a association_type' do
-      build_stubbed(:association, association_type: nil).should_not be_valid
-    end
-
+    it_validates 'presence of', :user
+    it_validates 'presence of', :associate
+    it_validates 'presence of', :association_type
     it 'requires the user is not the associate' do
       user = build_stubbed(:user)
       build_stubbed(:association, user: user, associate: user).should_not be_valid
     end
+    it_validates 'uniqueness of', :associate_id, :user_id, :association_type_id
   end
 end
