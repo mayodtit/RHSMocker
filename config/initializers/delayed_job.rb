11288f389ef88166b16d4a0a1d729a1cf4f65502
Misha Dragojevic <misha@cloudgurus.net>
2013-11-20 12:03:26 -0800

    delayed_job log file

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,9 +2,9 @@
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.logger ||= Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 #
-#Delayed::Worker.destroy_failed_jobs = false
-#Delayed::Worker.sleep_delay = 60
-#Delayed::Worker.max_attempts = 3
-#Delayed::Worker.max_run_time = 5.minutes
-#Delayed::Worker.read_ahead = 10
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.read_ahead = 10
 #Delayed::Worker.default_queue_name = 'default'
