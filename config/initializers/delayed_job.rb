86869d5dab3d797fbbf0ab2d6ff249ad3bafcc64
Tiem Song <tiem@songfamily.org>
2013-11-20 15:58:37 -0800

    temporarily commenting out config to disable delayed_jobs in test

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,5 @@
 # Misha, 11/20/2013
-Delayed::Worker.delay_jobs = !Rails.env.test?
+#Delayed::Worker.delay_jobs = !Rails.env.test? # temporarily commenting out; causes test errors.  See Pivotal #61176198
 Delayed::Worker.logger ||= Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 #
 Delayed::Worker.destroy_failed_jobs = false
