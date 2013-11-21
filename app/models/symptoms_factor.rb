b3c9741d31612a27508b5bd3085910650b41bc2a
Kyle Chilcutt <kyle@getbetter.com>
2013-11-21 11:19:26 -0800

    Re-add serializable_hash to SymptomsFactor
    Put special rendering back for symptoms checker.  Serializers not
    supported by the controller, so just override the hash.

--- app/models/symptoms_factor.rb
+++ app/models/symptoms_factor.rb
@@ -10,4 +10,13 @@ class SymptomsFactor < ActiveRecord::Base
 
   validates :symptom, :factor, :factor_group, presence: true
   validates :factor_id, uniqueness: {scope: [:symptom_id, :factor_group_id]}
+
+  def serializable_hash options=nil
+    {
+      id: id,
+      name: factor.name,
+      doctor_call_worthy: doctor_call_worthy,
+      er_worthy: er_worthy
+    }
+  end
 end
