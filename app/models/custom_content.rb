d57606612f0386f24f04db82388acea145b9f752
Kyle Chilcutt <kyle@getbetter.com>
2013-11-25 08:43:30 -0800

    Set defaults for CustomContent on create
    Automatically set up editable fields so that the record can be created
    with a minimum of inputs.  The admin can change the values later.

--- app/models/custom_content.rb
+++ app/models/custom_content.rb
@@ -1,7 +1,14 @@
 class CustomContent < Content
   private
 
+  def self.max_id
+    order('id DESC').first.id
+  end
+
   def set_defaults
+    self.document_id = "RHS#{'%05d' % (self.class.max_id + 1)}" if document_id.blank?
+    self.raw_body = 'Body for new CustomContent' if raw_body.blank?
+    self.content_type = 'Content' if content_type.blank?
     self.show_call_option = true if show_call_option.nil?
     self.show_checker_option = false if show_checker_option.nil?
     self.show_mayo_copyright = false if show_mayo_copyright.nil?
