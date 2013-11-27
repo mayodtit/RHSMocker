4ee4e4826683f5e0605378c0be7af035c17a9792
Kyle Chilcutt <kyle@getbetter.com>
2013-11-26 21:13:16 -0800

    Remove UsersController#upgrade_legacy_users
    Clients should no longer be signing up anonymously.  If they are, Paul
    should yell at those users.

--- app/controllers/api/V1/users_controller.rb
+++ app/controllers/api/V1/users_controller.rb
@@ -7,7 +7,6 @@ class Api::V1::UsersController < Api::V1::ABaseController
 
   skip_before_filter :authentication_check, :only => [:create, :reset_password, :check_reset_password, :update_password_from_reset]
   before_filter :load_user!, :only => :update
-  before_filter :upgrade_legacy_users, :only => :create
   before_filter :load_reset_token_then_user!, :only => [:check_reset_password, :update_password_from_reset]
 
   def index
@@ -26,18 +25,6 @@ class Api::V1::UsersController < Api::V1::ABaseController
   def create
     @user = Member.create(create_params)
     if @user.errors.empty?
-      render_success(:user => @user, :auth_token => @user.auth_token)
-    else
-      render_failure({:reason => @user.errors.full_messages.to_sentence,
-                      :user_message => @user.errors.full_messages.to_sentence}, 422)
-    end
-  end
-
-  # TODO - remove this before filter when old clients supporting anonymous user are removed
-  def upgrade_legacy_users
-    @user = Member.find_by_install_id(params[:user][:install_id])
-    return if @user.nil? || @user.email.present?
-    if @user.update_attributes(create_params)
       render_success(:user => @user, :auth_token => @user.auth_token)
     else
       render_failure({:reason => @user.errors.full_messages.to_sentence,
