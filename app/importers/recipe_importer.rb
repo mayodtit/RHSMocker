f84c0e576971f9384642519819d142a1fbb76848
Kyle Chilcutt <kyle@getbetter.com>
2013-12-02 09:59:15 -0800

    Improve RecipeImporter and add specs
    Simplify RecipeImporter methods and add spec to check overall
    functionality.

--- app/importers/recipe_importer.rb
+++ app/importers/recipe_importer.rb
@@ -2,6 +2,9 @@ class RecipeImporter < ContentImporter
   def import
     extract_required_params_from_xml!
     build_html!
+    add_intro_to_body!
+    add_ingredients_to_body!
+    add_directions_to_body!
     create_content!
     create_content_vocabularies!
     :success
@@ -12,8 +15,17 @@ class RecipeImporter < ContentImporter
   def build_html!
     @html = Nokogiri::HTML::DocumentFragment.parse "<body></body>"
     @body = @html.child
+  end
+
+  def add_intro_to_body!
     @body.add_child('<div class="intro">' + CGI.unescapeHTML(@data.at('DietitianTip').text.remove_newlines_and_tabs) + '</div>')
+  end
+
+  def add_ingredients_to_body!
     @body.add_child('<div class="ingredients"><h3>Ingredients</h3>' + CGI.unescapeHTML(@data.at('Ingredients').text.remove_newlines_and_tabs) + '</div>')
+  end
+
+  def add_directions_to_body!
     @body.add_child('<div class="directions"><h3>Directions</h3>' + CGI.unescapeHTML(@data.at('Directions').text.remove_newlines_and_tabs) + '</div>')
   end
 
