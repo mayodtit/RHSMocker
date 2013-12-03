678160d4ee61bf45b2065c93f67df15300b602e6
Kyle Chilcutt <kyle@getbetter.com>
2013-12-03 15:13:40 -0800

    Add ResetPasswordController and specs
    Prepare to clean up the UsersController.  Move reset password logic to
    its own controller.

---
+++ app/controllers/api/V1/reset_password_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::ResetPasswordController < Api::V1::ABaseController
+  skip_before_filter :authentication_check
+  before_filter :load_user_from_email!, only: :create
+  before_filter :load_user_from_token!, only: [:show, :update]
+
+  def create
+    @user.deliver_reset_password_instructions!
+    render_success
+  end
+
+  def show
+    render_success
+  end
+
+  def update
+    if @user.change_password!(params[:password])
+      render_success
+    else
+      render_failure({reason: @user.errors.full_messages.to_sentence}, 422)
+    end
+  end
+
+  private
+
+  def load_user_from_email!
+    @user = Member.find_by_email!(params[:email])
+  end
+
+  def load_user_from_token!
+    @user = Member.find_by_reset_password_token!(params[:id])
+  end
+end
