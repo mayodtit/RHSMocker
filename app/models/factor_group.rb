39cf263617982ae6cfbbcb9efaf1328ffe50e041
Kyle Chilcutt <kyle@getbetter.com>
2013-12-26 23:54:03 -0500

    Add ordinal to FactorGroup
    Add arbitrary ordering support to FactorGroup.  Allows us to order
    FactorGroup results based on document order.

--- app/models/factor_group.rb
+++ app/models/factor_group.rb
@@ -2,8 +2,22 @@ class FactorGroup < ActiveRecord::Base
   belongs_to :symptom
   has_many :factors
 
-  attr_accessible :symptom, :symptom_id, :name
+  attr_accessible :symptom, :symptom_id, :name, :ordinal
 
-  validates :symptom, :name, presence: true
+  validates :symptom, :name, :ordinal, presence: true
   validates :name, uniqueness: {scope: :symptom_id}
+  validates :ordinal, uniqueness: {scope: :symptom_id}
+
+  before_validation :set_ordinal
+
+  private
+
+  def self.max_ordinal_for_symptom_id(symptom_id)
+    where(symptom_id: symptom_id).order('ordinal DESC').first.try(:ordinal) || 0
+  end
+
+  def set_ordinal
+    return true if ordinal.try(:>, 0)
+    self.ordinal = self.class.max_ordinal_for_symptom_id(symptom_id) + 1
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131223201153) do
+ActiveRecord::Schema.define(:version => 20131227044211) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -232,6 +232,7 @@ ActiveRecord::Schema.define(:version => 20131223201153) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "symptom_id"
+    t.integer  "ordinal"
   end
 
   create_table "factors", :force => true do |t|
