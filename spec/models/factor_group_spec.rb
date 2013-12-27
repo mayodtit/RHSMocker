7e8f1690f3681ffa82293aa84e7b7cda76485c58
Kyle Chilcutt <kyle@getbetter.com>
2013-12-26 22:28:21 -0500

    Clean up FactorGroup specs and factory
    Fix factory to create valid objects, add specs for all validations.

--- spec/models/factor_group_spec.rb
+++ spec/models/factor_group_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 describe FactorGroup do
   it_has_a 'valid factory'
+  it_validates 'presence of', :symptom
   it_validates 'presence of', :name
+  it_validates 'uniqueness of', :name, :symptom_id
 end
