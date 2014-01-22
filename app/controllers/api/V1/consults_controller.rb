50da63cfe55965250f1ac18de35e4f872c4b22e4
Kyle Chilcutt <kyle@getbetter.com>
2014-01-22 15:27:58 -0800

    Send automatic welcome message from the PHA
    When the user sets up their welcome call.  This hack needs to be cleaned
    up in the future.
    
    [Finishes #64281748]

--- app/controllers/api/V1/consults_controller.rb
+++ app/controllers/api/V1/consults_controller.rb
@@ -54,20 +54,42 @@ class Api::V1::ConsultsController < Api::V1::ABaseController
   def scheduled_phone_call_attributes
     attributes = params.require(:consult).permit(scheduled_phone_call: [:scheduled_at, :callback_phone_number])
     if attributes.any?
-      scheduled_phone_call = ScheduledPhoneCall.where(state: :assigned)
-                                               .where(scheduled_at: attributes[:scheduled_phone_call][:scheduled_at])
-                                               .first!
-      scheduled_phone_call.assign_attributes(state_event: :book,
-                                             user_id: @user.id,
-                                             booker_id: @user.id,
-                                             callback_phone_number: attributes[:scheduled_phone_call][:callback_phone_number])
+      @scheduled_phone_call = ScheduledPhoneCall.where(state: :assigned)
+                                                .where(scheduled_at: attributes[:scheduled_phone_call][:scheduled_at])
+                                                .first!
+      @scheduled_phone_call.assign_attributes(state_event: :book,
+                                              user_id: @user.id,
+                                              booker_id: @user.id,
+                                              callback_phone_number: attributes[:scheduled_phone_call][:callback_phone_number])
       {
         user: @user,
-        scheduled_phone_call: scheduled_phone_call
+        scheduled_phone_call: @scheduled_phone_call,
+        created_at: Time.now + 1.second
       }
     else
       nil
     end
+  end
+
+  def pha_message
+    <<-eos
+Hi there!
+I'm #{@scheduled_phone_call.owner.first_name} and I'll be your Personal Health Assistant.
+I'm looking forward to starting our conversation with an old fashioned telephone call.
+
+To get ready:
+ - Have your insurance card ready
+ - Fill out your Health Profile
+
+Questions or cancellations? Send me a message right here, and I'll get back to you.
+    eos
+  end
+
+  def pha_message_attributes
+    {
+      user: @scheduled_phone_call.owner,
+      text: pha_message,
+    }
   end
 
   def messages_attributes
@@ -76,6 +98,7 @@ class Api::V1::ConsultsController < Api::V1::ABaseController
     attributes << message_attributes.merge!(user: @user) if message_attributes
     attributes << phone_call_attributes.merge!(user: @user, text: phone_call_text) if phone_call_attributes
     attributes << scheduled_phone_call_attributes.merge!(user: @user, text: scheduled_phone_call_text) if scheduled_phone_call_attributes
+    attributes << pha_message_attributes if @scheduled_phone_call
     attributes.any? ? attributes : nil
   end
 
