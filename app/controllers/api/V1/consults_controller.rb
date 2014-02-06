837e70b4a7fd9e2f13160a5a2898cbb4569d3bcc
Kyle Chilcutt <kyle@getbetter.com>
2014-02-06 10:00:46 -0800

    Update copy of automated response for onboarding
    Copy changes!
    
    [Finishes #65224922]

--- app/controllers/api/V1/consults_controller.rb
+++ app/controllers/api/V1/consults_controller.rb
@@ -90,7 +90,7 @@ To get ready:
  - Have your insurance card ready
  - Fill out your Health Profile
 
-Questions or cancellations? Send me a message right here, and I'll get back to you.
+Questions or cancellations? Email me at premium@getbetter.com.
     eos
   end
 
