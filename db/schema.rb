9f8344bc5c8811f52dc5be28f5f17e9da095b17c
Kyle Chilcutt <kyle@getbetter.com>
2014-02-13 23:34:10 -0800

    Set state for existing Associations to enabled
    All Associations that are already in the system should be set to
    enabled.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140213160849) do
+ActiveRecord::Schema.define(:version => 20140214073320) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
