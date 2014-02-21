d9874a1e314b997399e359a34f3991f0ca900d91
Tiem Song <tiem@songfamily.org>
2014-02-21 13:00:11 -0800

    harden taxonomy code lookup from bloom return results

--- lib/search/service/bloom.rb
+++ lib/search/service/bloom.rb
@@ -1,3 +1,6 @@
+# Code values explained here (in case you ever need some light bedtime reading)
+# http://www.cms.gov/Regulations-and-Guidance/HIPAA-Administrative-Simplification/NationalProvIdentStand/downloads/Data_Dissemination_File-Code_Values.pdf
+
 class Search::Service::Bloom
   include HTTParty
   base_uri 'http://bloomapi.getbetter.com/'
@@ -55,7 +58,7 @@ class Search::Service::Bloom
       fax: p['fax']
     }
 
-    hcp_code = record['provider_details'].last['healthcare_taxonomy_code']
+    hcp_code = get_hcp_code(record['provider_details'])
     {
       :first_name => prettify(record['first_name']),
       :last_name => prettify(record['last_name']),
@@ -75,5 +78,20 @@ class Search::Service::Bloom
 
   def prettify(string)
     string.titleize(underscore: false, humanize: false) unless string.nil?
+  end
+
+  # Find the provider's Healthcare Provider Taxonomy Code.  There can only be one per NPI record
+  def get_hcp_code(details_ary)
+    if details_ary.size == 1
+      details_ary.first['healthcare_taxonomy_code']
+    else
+      details_ary.each do |d|
+        return d['healthcare_taxonomy_code'] if d['taxonomy_switch'] == 'yes'
+      end
+
+      # This code shouldn't be executed since a NPI record *should* have a taxonomy code.
+      # Return last value, instead of nil, in case there's bad data.
+      details_ary.last['healthcare_taxonomy_code']
+    end
   end
 end
