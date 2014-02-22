0a332e1052673998d850cf7e819fa625185d09b3
Abhik Pramanik <abhik@getbetter.com>
2014-02-21 17:33:21 -0800

    Messages now publish that a new message was created to pub sub server after create.  - Controlled by Metadata, off by default.

---
+++ config/initializers/pub_sub.rb
@@ -0,0 +1,3 @@
+require 'pub_sub'
+PUB_SUB_SECRET = ENV['PUB_SUB_SECRET'] || 'THIS_IS_A_SECRET_SHHH'
+PUB_SUB_HOST = ENV['PUB_SUB_HOST'] || 'http://localhost:3001'
---
+++ lib/pub_sub.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+
+class PubSub
+  def publish(channel, data)
+    return unless Metadata.find_by_mkey('use_pub_sub').try(:mvalue) == 'true'
+    message = {
+      channel: "/#{Rails.env}#{channel}",
+      data: data,
+      ext: { secret: PUB_SUB_SECRET }
+    }
+
+    uri = URI.parse PUB_SUB_HOST
+    Net::HTTP.post_form(uri, message: message.to_json)
+  end
+  #handle_asynchronously :publish
+end
