dffee961ca0e4068cc449e60a762409814ca5bff
Kyle Chilcutt <kyle@getbetter.com>
2014-02-24 16:53:27 -0800

    Clean up PermissionSerializer
    Reorganize levels into an array of hashes and add display_names for
    messaging to the user.

--- app/serializers/permission_serializer.rb
+++ app/serializers/permission_serializer.rb
@@ -2,18 +2,21 @@ class PermissionSerializer < ActiveModel::Serializer
   self.root = false
 
   attributes :subject_id, :basic_info, :medical_info, :care_team,
-             :basic_info_levels, :medical_info_levels, :care_team_levels,
-             :created_at, :updated_at
+             :levels, :display_names, :created_at, :updated_at
 
-  def basic_info_levels
-    Permission::BASIC_INFO_LEVELS
+  def levels
+    [
+      basic_info: Permission::BASIC_INFO_LEVELS,
+      medical_info: Permission::MEDICAL_INFO_LEVELS,
+      care_team: Permission::CARE_TEAM_LEVELS
+    ]
   end
 
-  def medical_info_levels
-    Permission::MEDICAL_INFO_LEVELS
-  end
-
-  def care_team_levels
-    Permission::CARE_TEAM_LEVELS
+  def display_names
+    {
+      basic_info: 'Basic Information',
+      medical_info: 'Medical Information',
+      care_team: 'Care Team'
+    }
   end
 end
