64b121ca1ef106da50b621846ebc3806153bb629
Kyle Chilcutt <kyle@getbetter.com>
2014-03-05 13:20:27 -0800

    Clean up Role model
    Add validations when appropriate, improve factory and add specs.
    
    [Finishes #63395586]

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,9 @@
 class Role < ActiveRecord::Base
-  has_and_belongs_to_many :users, :join_table => :users_roles
-  belongs_to :resource, :polymorphic => true
+  has_and_belongs_to_many :users, join_table: :users_roles
+  belongs_to :resource, polymorphic: true
 
-  attr_accessible :resource
-  attr_accessible :name, :resource_id, :resource_type
+  attr_accessible :name, :resource, :resource_id, :resource_type
+
+  validates :name, presence: true,
+                   uniqueness: {scope: %i(resource_id resource_type)}
 end
