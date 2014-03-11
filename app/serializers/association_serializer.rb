349cfdedd8328319ea5acf3e35754b681894af5d
Kyle Chilcutt <kyle@getbetter.com>
2014-03-10 18:23:50 -0700

    Clean up serializer and view for associations
    Clean up rendering for association cards.

--- app/serializers/association_serializer.rb
+++ app/serializers/association_serializer.rb
@@ -11,4 +11,44 @@ class AssociationSerializer < ViewSerializer
   def is_default_hcp
     user.default_hcp_association_id == object.id
   end
+
+  def body
+    controller.render_to_string(template: 'api/v1/cards/preview',
+                                layout: 'serializable',
+                                formats: :html,
+                                locals: {card: nil, resource: self, current_user: scope})
+  end
+  alias_method :preview, :body
+
+  def raw_body
+    controller.render_to_string(template: 'api/v1/cards/preview',
+                                formats: :html,
+                                locals: {card: nil, resource: self, current_user: scope})
+  end
+  alias_method :raw_preview, :raw_body
+
+  def title
+    'Sharing Request'
+  end
+
+  def content_type
+    'Association'
+  end
+  alias_method :content_type_display, :content_type
+
+  def share_url
+    nil
+  end
+
+  def card_actions
+    []
+  end
+
+  def fullscreen_actions
+    []
+  end
+
+  def timeline_action
+    {}
+  end
 end
