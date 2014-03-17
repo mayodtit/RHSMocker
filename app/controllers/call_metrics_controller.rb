f4e22c6dab799be68fe3b44352ab9e42e240db17
Tiem Song <tiem@songfamily.org>
2014-03-17 11:08:52 -0700

    CHECKPOINT: add new nurseline metrics and change return to json

--- app/controllers/call_metrics_controller.rb
+++ app/controllers/call_metrics_controller.rb
@@ -6,17 +6,34 @@ class CallMetricsController < ApplicationController
     beginning_of_this_week = Time.now.beginning_of_week(:sunday)
 
     processing_week = phone_call_epoch.beginning_of_week(:sunday)
-    @results = []
+    @results = {}
 
     while processing_week <= beginning_of_this_week do
-      cm = NurseCallMetrics.new(processing_week, processing_week.end_of_week(:sunday))
-      @results << [processing_week.strftime('%Y-%m-%d'), cm]
+      cm = NurseCallMetrics.new(processing_week, processing_week.end_of_week(:sunday)).to_json
+
+      # calculate cumulative stats
+      cm[:num_calls][:all_time] = cm[:num_calls][:new].clone
+      cm[:ended_calls_per_nurse][:all_time] = cm[:ended_calls_per_nurse][:new].clone
+      unless @results.empty?
+        cm[:ended_calls_per_nurse][:all_time].default = 0
+        prev_totals = @results.values.last
+        calls_all_time = prev_totals[:num_calls][:all_time]
+        calls_all_time.each do |k,v|
+          cm[:num_calls][:all_time][k] += v
+        end
+
+        nurse_all_time = prev_totals[:ended_calls_per_nurse][:all_time]
+        nurse_all_time.each do |k,v|
+
+          # since we have the default hash value set above, we don't have to worry about checking for non-existent keys
+          cm[:ended_calls_per_nurse][:all_time][k] += v
+        end
+      end
+
+      @results.merge!({"#{processing_week.strftime('%Y-%m-%d')}".to_sym => cm})
       processing_week += 1.week
     end
 
-    # get list of all nurses who have taken a nurseline call longer than a minute
-    unique_claimer_ids = PhoneCall.valid_nurse_call.pluck(:claimer_id).uniq
-    @nurses = User.find(unique_claimer_ids)
-    @nurse_totals = Hash.new(0)
+    render json: @results, root: false
   end
 end
--- lib/nurse_call_metrics.rb
+++ lib/nurse_call_metrics.rb
@@ -1,17 +1,22 @@
 class NurseCallMetrics
-  attr_reader :num_calls_claimed_but_not_ended
-
   def initialize(start_time, end_time)
+    @end_time = end_time
+    @all_calls = PhoneCall.where(created_at: start_time..end_time)
     @completed_calls = PhoneCall.valid_nurse_call.where(claimed_at: start_time..end_time) # calls claimed within this time frame
-    @num_calls_claimed_but_not_ended = PhoneCall.where(claimed_at: start_time..end_time).to_nurse_line.where('claimed_at IS NOT NULL').where('ended_at IS NULL').count
+    @calls_claimed_but_not_ended = PhoneCall.where(claimed_at: start_time..end_time).to_nurse_line.where('claimed_at IS NOT NULL').where('ended_at IS NULL')
   end
 
-  def num_completed_calls
-    @completed_calls.count
+  def average_call_length
+    len = completed_call_lengths.length
+    if len == 0
+      0.0
+    else
+      completed_call_lengths.inject(:+) / len / 60.0
+    end
   end
 
   def median_call_length
-    call_lengths = @completed_calls.map {|c| c.ended_at - c.claimed_at}
+    call_lengths = completed_call_lengths
     call_lengths.sort!
 
     len = call_lengths.length
@@ -23,17 +28,52 @@ class NurseCallMetrics
     end
   end
 
-  def avg_calls_per_member
-    cm_hash = @completed_calls.group(:user_id).count
-    cm_hash.length == 0 ? 0 : cm_hash.values.sum / cm_hash.length.to_f
+  def total_member_count
+    @tmc ||= Member.where(created_at: DateTime.parse('1970-01-01')..@end_time).count
   end
 
-  def avg_calls_per_nurse
-    cm_hash = calls_per_nurse
-    cm_hash.length == 0 ? 0 : cm_hash.values.sum / cm_hash.length.to_f
+  def calls_per_member
+    cm_hash = @completed_calls.group(:user_id).count
+    only_callers = (cm_hash.length == 0 ? 0.0 : cm_hash.values.sum / cm_hash.length.to_f)
+    all = @completed_calls.length / total_member_count
+    {all_members: all, only_callers: only_callers}
   end
 
   def calls_per_nurse
-    @completed_calls.group(:claimer_id).count
+    cm_hash = ended_calls_per_nurse
+    cm_hash.length == 0 ? 0 : cm_hash.values.sum / cm_hash.length.to_f
+  end
+
+  def ended_calls_per_nurse
+    @ecpn ||= @completed_calls.group(:claimer_id).count
+  end
+
+  def to_json
+    {
+      num_calls: {
+        new: {
+          total: @all_calls.count,
+          claimed_but_not_ended: @calls_claimed_but_not_ended.count,
+          completed: @completed_calls.count
+        },
+      },
+      call_length: {
+        average: average_call_length,
+        median: median_call_length
+      },
+      total_member_count: total_member_count,
+      calls_per_member: calls_per_member,
+      calls_per_nurse: calls_per_nurse,
+      ended_calls_per_nurse: {
+        new: ended_calls_per_nurse,
+        all_time: {}
+      }
+    }
+  end
+
+  private
+
+  def completed_call_lengths
+    @ccl ||= @completed_calls.map {|c| c.ended_at - c.claimed_at}
   end
 end
