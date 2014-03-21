48201ff1cd5b310a73099b2063092d4398c3fbea
Kyle Chilcutt <kyle@getbetter.com>
2014-03-20 17:13:14 -0700

    Remove hard failure from migration
    Let it fail silently, it will be more fun that way.

--- db/migrate/20140320182059_backfill_parent_associations.rb
+++ db/migrate/20140320182059_backfill_parent_associations.rb
@@ -5,7 +5,7 @@ class BackfillParentAssociations < ActiveRecord::Migration
                .where("associates_associations.id != associates_associations.owner_id")
                .where('associates_associations.owner_id != users.id')
                .readonly(false).each do |a|
-                 a.update_attributes(parent: Association.where(user_id: a.associate.owner_id, associate_id: a.user_id).first!)
+                 a.update_attributes(parent: Association.where(user_id: a.associate.owner_id, associate_id: a.user_id).first)
                end
   end
 
