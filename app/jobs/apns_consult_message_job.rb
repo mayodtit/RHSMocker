518513f112e423ac5785202cdd85d6b43652e639
Kyle Chilcutt <kyle@getbetter.com>
2014-03-25 13:51:24 -0700

    Update APNS message notification to not say "your"
    Because it might not be "your PHA".  Update the notification so it will
    always be factually correct.
    
    [Finishes #68154622]

--- app/jobs/apns_consult_message_job.rb
+++ app/jobs/apns_consult_message_job.rb
@@ -12,7 +12,7 @@ class ApnsConsultMessageJob < Struct.new(:user_id, :consult_id)
     user = User.find(user_id)
     consult = Consult.find(consult_id)
     if user.apns_token
-      APNS.send_notification(user.apns_token, alert: 'You have a new message from your Personal Health Assistant.',
+      APNS.send_notification(user.apns_token, alert: 'You have a new message from a Personal Health Assistant.',
                                               sound: :default,
                                               other: {nb: {cmd: 'openConsult',
                                                            args: {id: consult.id}}})
