d95b5868462eb0cb5cd19e55a85f23a80007a3f5
Kyle Chilcutt <kyle@getbetter.com>
2014-03-28 10:13:45 -0700

    Add support for messaging the master consult
    Allow the client to specify "current" as the consult_id in order to
    route to the member's master consult.

--- spec/requests/api/v1/messages_spec.rb
+++ spec/requests/api/v1/messages_spec.rb
@@ -19,6 +19,22 @@ describe 'Messages' do
         expect(body[:messages].to_json).to eq([message].serializer.as_json.to_json)
       end
     end
+
+    describe 'GET /api/v1/consults/current/messages' do
+      def do_request
+        get '/api/v1/consults/current/messages', auth_token: user.auth_token
+      end
+
+      let!(:master) { create(:consult, :master, initiator: user) }
+      let!(:master_message) { create(:message, consult: master) }
+
+      it "indexes messages for the users's master consult" do
+        do_request
+        expect(response).to be_success
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(body[:messages].to_json).to eq([master_message].serializer.as_json.to_json)
+      end
+    end
   end
 
   describe 'POST /api/v1/consults/:consult_id/messages' do
@@ -26,6 +42,23 @@ describe 'Messages' do
       post "/api/v1/consults/#{consult.id}/messages", params.merge!(auth_token: user.auth_token)
     end
 
+    let(:message_params) { {message: {text: 'test message'}} }
+
+    it 'create a new message for the consult' do
+      expect{ do_request(message_params) }.to change(Message, :count).by(2) # TODO - creates 2 messages including auto-response
+      expect(response).to be_success
+      body = JSON.parse(response.body, symbolize_names: true)
+      message = Message.find(body[:message][:id])
+      expect(body[:message].to_json).to eq(message.serializer.as_json.to_json)
+    end
+  end
+
+  describe 'POST /api/v1/consults/current/messages' do
+    def do_request(params={})
+      post '/api/v1/consults/current/messages', params.merge!(auth_token: user.auth_token)
+    end
+
+    let!(:consult) { create(:consult, :master, initiator: user) }
     let(:message_params) { {message: {text: 'test message'}} }
 
     it 'create a new message for the consult' do
