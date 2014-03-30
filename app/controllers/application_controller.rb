37d1de5131fdb919b17cfd6081ccf4ec7d5ec65c
Kyle Chilcutt <kyle@getbetter.com>
2014-03-30 15:34:22 -0700

    Remove default subject for permitted_params
    Some params are dependent on params being nil, undo the default subject.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def permitted_params(subject=nil)
-    @permitted_params ||= PermittedParams.new(params, current_user, subject || @user)
+    @permitted_params ||= PermittedParams.new(params, current_user, subject)
   end
 end
