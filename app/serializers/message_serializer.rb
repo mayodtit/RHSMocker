e9983310314714ae6429accc6aaa3cfeb6ee2a44
Kyle Chilcutt <kyle@getbetter.com>
2014-04-04 14:19:22 -0700

    Accept and render content_id for messages
    Let the client set it, render it back to the client.

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,7 +1,8 @@
 class MessageSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :id, :text, :created_at, :consult_id, :title, :image_url, :type, :consult_title
+  attributes :id, :text, :created_at, :consult_id, :title, :image_url, :type,
+             :consult_title, :content_id
   has_one :user
   has_one :phone_call
   has_one :phone_call_summary
