c352f8cf53c0afb50df0c4026646236963019ee7
Tiem Song <tiem@songfamily.org>
2014-04-04 13:57:48 -0700

    Birthday card should not be sent to new users
    [finishes #68860206]

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
 class Question < ActiveRecord::Base
   attr_accessible :title, :view
 
-  symbolize :view, in: [:gender, :diet, :allergies, :birthdate]
+  symbolize :view, in: [:gender, :diet, :allergies]
 
   validates :title, :view, presence: true, uniqueness: true
 
   def self.new_member_questions
-    where(:view => [:gender, :birthdate])
+    where(:view => [:gender])
   end
 end
