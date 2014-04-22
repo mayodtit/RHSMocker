ea177ff9f59e201f8ad187bee319387002fafe85
Abhik Pramanik <abhik@getbetter.com>
2014-04-22 14:45:56 -0700

    Nurseline follow up task should serialize message id of phone call
    [Finishes #69964706]

--- app/serializers/follow_up_task_serializer.rb
+++ app/serializers/follow_up_task_serializer.rb
@@ -1,8 +1,12 @@
 class FollowUpTaskSerializer < TaskSerializer
 
-  attributes :phone_call_id
+  attributes :phone_call_id, :message_id
 
   has_one :member
+
+  def message_id
+    object.phone_call && object.phone_call.message && object.phone_call.message.id
+  end
 
   def type
     'follow-up'
