dfb151d68f1633dfefdd33529d23e9e5f2736c91
Kyle Chilcutt <kyle@getbetter.com>
2014-04-29 10:39:46 -0700

    Create InvitationJob for sending invitation_mail
    Encapsulate sending invitation_mail into a delayed job.

---
+++ spec/jobs/mails/invitation_job_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Mails::InvitationJob do
+  let!(:member) { create(:member) }
+  let(:url) { 'http://www.getbetter.com/whatever' }
+
+  before do
+    Timecop.freeze(Date.today.to_time)
+  end
+
+  after do
+    Timecop.return
+  end
+
+  describe '::create' do
+    it 'enqueues the job to run now' do
+      expect{ described_class.create(member.id, url) }.to change(Delayed::Job, :count).by(1)
+      job = Delayed::Job.last
+      expect(job.run_at).to eq(Time.now)
+    end
+  end
+
+  describe '#perform' do
+    it 'works' do
+      described_class.new(member.id, url).perform
+    end
+  end
+end
