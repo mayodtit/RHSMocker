cc998dffcdd21ebc703950639871df70b390d5e0
Tiem Song <tiem@songfamily.org>
2014-05-21 10:44:02 -0700

    Add list of paying members emails
    [finishes #71780174]

--- app/controllers/api/V1/metrics_controller.rb
+++ app/controllers/api/V1/metrics_controller.rb
@@ -18,20 +18,34 @@ class Api::V1::MetricsController < Api::V1::ABaseController
       {
         description: 'Inbound calls by week csv',
         url: inbound_by_week_api_v1_metrics_url
+      },
+      {
+        description: 'List of emails of paying members',
+        url: paying_members_emails_api_v1_metrics_url
       }
     ]
     render json: items, root: false
   end
 
+  def paying_members_emails
+    csv = CSV.generate do |c|
+      c << ['email']
+      StripeExtension.subscriber_emails.each do |email|
+        c << [email]
+      end
+    end
+
+    send_data csv, type: 'text/csv', filename: 'paying_members_emails.csv'
+  end
+
   def inbound
-    pha = Role.find_by_name!('pha')
     task_count = PhoneCallTask.pha.count
     message_count = MessageTask.count
 
-    csv = CSV.generate do |csv|
-      csv << ["type", "count"]
-      csv << ["Phone", task_count]
-      csv << ["Message", message_count]
+    csv = CSV.generate do |c|
+      c << ["type", "count"]
+      c << ["Phone", task_count]
+      c << ["Message", message_count]
     end
 
     send_data csv, type: 'text/csv', filename: 'inbound.csv'
@@ -44,14 +58,14 @@ class Api::V1::MetricsController < Api::V1::ABaseController
     processing_week = task_epoch.beginning_of_week :sunday
 
     i = 0
-    csv = CSV.generate do |csv|
-      csv << ["week", "phone", "message"]
+    csv = CSV.generate do |c|
+      c << ["week", "phone", "message"]
 
       while processing_week <= beginning_of_this_week do
         phone_call_count = PhoneCallTask.pha.where(created_at: processing_week..processing_week.end_of_week(:sunday)).count
         message_count = MessageTask.pha.where(created_at: processing_week..processing_week.end_of_week(:sunday)).count
 
-        csv << [i += 1, phone_call_count, message_count]
+        c << [i += 1, phone_call_count, message_count]
 
         processing_week += 1.week
       end
