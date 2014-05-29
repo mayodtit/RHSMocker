afb51b4f7b33ecd0622884653970d4e9fe6afea4
Kyle Chilcutt <kyle@getbetter.com>
2014-05-28 19:09:09 -0700

    Add support for invitation referrals
    Hitting this endpoint will sign the member up for an invitation and
    associate them with an onboarding group if a referral code is present.

--- spec/models/referral_code_spec.rb
+++ spec/models/referral_code_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ReferralCode do
   it_has_a 'valid factory'
+  it_has_a 'valid factory', :with_onboarding_group
   it_validates 'presence of', :creator
   it_validates 'presence of', :code
   it_validates 'foreign key of', :onboarding_group
