fb00f73248a250835c8d94de2c2740139cbeb84a
Kyle Chilcutt <kyle@getbetter.com>
2014-05-29 08:08:51 -0700

    User has_many Addresses
    Change has_one relation to has_many.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ActiveRecord::Base
-  belongs_to :user, inverse_of: :address
+  belongs_to :user, inverse_of: :addresses
 
   attr_accessible :user, :user_id, :address, :address2, :city, :state, :postal_code
 
