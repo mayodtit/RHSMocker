c7fc8f2aa5c8d0d173a8e953c0d9cf092d923d50
Abhik Pramanik <abhik@getbetter.com>
2014-06-05 15:55:35 -0700

    Phone call task now serializes with transferrer in shallow mode.

--- app/serializers/phone_call_task_serializer.rb
+++ app/serializers/phone_call_task_serializer.rb
@@ -2,8 +2,18 @@ class PhoneCallTaskSerializer < TaskSerializer
 
   attributes :phone_call_id, :transferrer
 
+  def attributes
+    if options[:shallow]
+      super.tap do |attributes|
+        attributes[:transferrer] = transferrer
+      end
+    else
+      super
+    end
+  end
+
   def transferrer
-    object.phone_call.transferred_from_phone_call && MemberSerializer.new(object.phone_call.transferred_from_phone_call.claimer)
+    object.phone_call.transferred_from_phone_call && object.phone_call.transferred_from_phone_call.claimer.serializer(options)
   end
 
   def type
