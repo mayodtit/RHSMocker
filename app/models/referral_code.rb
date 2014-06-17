5f660a4edc55ac54b2257eac711391854e27d43d
Kyle Chilcutt <kyle@getbetter.com>
2014-06-17 16:09:08 -0700

    Automatically generate ReferralCode codes
    Generate a code on create if one is not provided.

--- app/models/referral_code.rb
+++ app/models/referral_code.rb
@@ -9,8 +9,23 @@ class ReferralCode < ActiveRecord::Base
   attr_accessible :creator, :creator_id, :onboarding_group,
                   :onboarding_group_id, :name, :code
 
-  validates :code, presence: true
+  validates :code, presence: true, uniqueness: true
   validates :user, presence: true, if: ->(r){r.user_id}
   validates :creator, presence: true, if: ->(r){r.creator_id}
   validates :onboarding_group, presence: true, if: ->(r){r.onboarding_group_id}
+
+  before_validation :set_code, on: :create
+
+  private
+
+  def set_code
+    self.code ||= loop do
+      new_code = base32_code
+      break new_code unless self.class.exists?(code: new_code)
+    end
+  end
+
+  def base32_code
+    Base32::Crockford.encode(SecureRandom.random_number(1000000000), length: 6)
+  end
 end
--- spec/models/referral_code_spec.rb
+++ spec/models/referral_code_spec.rb
@@ -3,8 +3,36 @@ require 'spec_helper'
 describe ReferralCode do
   it_has_a 'valid factory'
   it_has_a 'valid factory', :with_onboarding_group
-  it_validates 'presence of', :code
-  it_validates 'foreign key of', :user
-  it_validates 'foreign key of', :creator
-  it_validates 'foreign key of', :onboarding_group
+
+  describe 'validations' do
+    before do
+      described_class.any_instance.stub(:set_code)
+    end
+
+    it_validates 'presence of', :code
+    it_validates 'uniqueness of', :code
+    it_validates 'foreign key of', :user
+    it_validates 'foreign key of', :creator
+    it_validates 'foreign key of', :onboarding_group
+  end
+
+  describe 'callbacks' do
+    describe '#set_code' do
+      it 'automatically sets the code on create' do
+        referral_code = build(:referral_code, code: nil)
+        expect(referral_code.code).to be_nil
+        referral_code.save!
+        expect(referral_code.code).to_not be_nil
+      end
+
+      it 'does not set the same code twice' do
+        referral_code = create(:referral_code)
+        new_referral_code = build(:referral_code, code: nil)
+        new_referral_code.stub(:base32_code).and_return(referral_code.code, 'NEWCODE')
+        expect(new_referral_code.code).to be_nil
+        new_referral_code.save!
+        expect(new_referral_code.code).to eq('NEWCODE')
+      end
+    end
+  end
 end
