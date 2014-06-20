32de41ab6021e6365068d32948608b28ee34cfee
Kyle Chilcutt <kyle@getbetter.com>
2014-06-20 10:04:41 -0700

    Send a confirmation for booking an appointment
    When the user sends a UserRequest, automatically generate a message
    confirming recept.
    
    [Finishes #73591328]

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -8,4 +8,20 @@ class UserRequest < ActiveRecord::Base
                   :user_request_type, :user_request_type_id, :request_data
 
   validates :user, :subject, :user_request_type, :name, presence: true
+
+  after_create :send_confirmation_message
+
+  private
+
+  def send_confirmation_message
+    return unless user.master_consult
+
+    text = "We're working on your request. " +
+           "#{user.pha.try(:first_name) || 'Your PHA'} will get back to you " +
+           "with an appointment confirmation soon."
+
+    user.master_consult.messages.create(user: Member.robot,
+                                        text: text,
+                                        off_hours: true)
+  end
 end
