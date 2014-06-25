a4af08f29ee2de4a0286e313ff645eaf9087028e
Kyle Chilcutt <kyle@getbetter.com>
2014-06-25 11:47:35 -0700

    Serializer UserRequest metadata
    Add attributes to UserRequestSerializer for care portal consumption.

--- app/serializers/user_request_serializer.rb
+++ app/serializers/user_request_serializer.rb
@@ -1,7 +1,9 @@
 class UserRequestSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :user_id, :subject_id, :name
+  attributes :id, :user_id, :subject_id, :name, :request_data
 
+  has_one :user, embed: :objects
+  has_one :subject, embed: :objects
   has_one :user_request_type
 end
