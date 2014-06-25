23fa6346fb2d9c4d4f0a4e2e68a4dbce1910183b
Kyle Chilcutt <kyle@getbetter.com>
2014-06-25 14:22:06 -0700

    Update appointment request confirmation copy
    Change copy for resulting message.

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -18,9 +18,8 @@ class UserRequest < ActiveRecord::Base
   def send_confirmation_message
     return unless user.master_consult
 
-    text = "We're working on your request. " +
-           "#{user.pha.try(:first_name) || 'Your PHA'} will get back to you " +
-           "with an appointment confirmation soon."
+    text = "#{user.pha.try(:first_name) || 'Your PHA'}'s working on your " +
+           "appointment request. Look out for more details soon."
 
     user.master_consult.messages.create(user: Member.robot,
                                         text: text,
