ca478586083596d708aa391b25c9c4f328861904
Kyle Chilcutt <kyle@getbetter.com>
2014-07-01 07:52:28 -0700

    Seed new user card deck through migration
    Ensure that the cards and content are there before attempting to migrate
    users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140629224607) do
+ActiveRecord::Schema.define(:version => 20140701145032) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
