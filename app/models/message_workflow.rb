735bc970ab06116a3bf9ea9f23c537de685d2711
Kyle Chilcutt <kyle@getbetter.com>
2014-07-08 11:48:29 -0700

    Add support for adding members to MessageWorkflows
    Schedule message templates for a member when they are added to a
    MessageWorkflow.

--- app/models/message_workflow.rb
+++ app/models/message_workflow.rb
@@ -5,4 +5,20 @@ class MessageWorkflow < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, presence: true, uniqueness: true
+
+  def add_to_member(member)
+    initial_time = nine_oclock_on_date(Time.now.pacific)
+    message_workflow_templates.each do |mwt|
+      ScheduledMessage.create!(sender: member.pha,
+                               consult: member.master_consult,
+                               text: mwt.message_template.text,
+                               publish_at: initial_time + mwt.days_delayed.days)
+    end
+  end
+
+  private
+
+  def nine_oclock_on_date(time)
+    Time.new(time.year, time.month, time.day, 9, 0, 0, time.utc_offset)
+  end
 end
