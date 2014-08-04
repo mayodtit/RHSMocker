d712023405ec9ee3d2406475dd0c459b1715207b
Abhik Pramanik <abhik@getbetter.com>
2014-08-04 16:56:17 -0700

    Updating priorities for tasks as per https://docs.google.com/a/getbetter.com/spreadsheets/d/1PLfO6WAcRZa5ZjFeu8h08ZMy_6nW1jk-Yp_GjRtUg7M/edit#gid=0
    [Finishes #74816678]

--- app/models/parsed_nurseline_record_task.rb
+++ app/models/parsed_nurseline_record_task.rb
@@ -1,6 +1,6 @@
 class ParsedNurselineRecordTask < Task
   include ActiveModel::ForbiddenAttributesProtection
-
+  PRIORITY = 8
   belongs_to :parsed_nurseline_record
 
   attr_accessible :member, :member_id, :parsed_nurseline_record,
@@ -8,4 +8,8 @@ class ParsedNurselineRecordTask < Task
 
   validates :parsed_nurseline_record, presence: true
   validates :member, presence: true, if: ->(p){p.member_id}
+
+  def set_priority
+    self.priority = PRIORITY
+  end
 end
