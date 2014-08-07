a3950acd7a68a3fe1cdad5ca1b0d345b899193ce
Kyle Chilcutt <kyle@getbetter.com>
2014-08-07 14:13:56 -0700

    Add base class for scheduled serializers
    All ScheduledCommunications serializers require the same attributes,
    create a parent class with defaults.

--- app/serializers/scheduled_message_serializer.rb
+++ app/serializers/scheduled_message_serializer.rb
@@ -1,7 +1,3 @@
-class ScheduledMessageSerializer < ActiveModel::Serializer
-  self.root = false
-
-  attributes :id, :sender_id, :recipient_id, :message_id, :text, :state,
-             :publish_at, :delivered_at, :created_at, :updated_at,
-             :state_events
+class ScheduledMessageSerializer < ScheduledCommunicationSerializer
+  attributes :message_id, :text
 end
--- app/serializers/scheduled_plain_text_email_serializer.rb
+++ app/serializers/scheduled_plain_text_email_serializer.rb
@@ -1,7 +1,3 @@
-class ScheduledPlainTextEmailSerializer < ActiveModel::Serializer
-  self.root = false
-
-  attributes :id, :sender_id, :recipient_id, :text, :state,
-             :publish_at, :delivered_at, :created_at, :updated_at,
-             :state_events
+class ScheduledPlainTextEmailSerializer < ScheduledCommunicationSerializer
+  attributes :text
 end
