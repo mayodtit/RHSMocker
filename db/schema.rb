1d01e331d4ac9ee3b5e0ec90cc619b64c3c6dadf
Kyle Chilcutt <kyle@getbetter.com>
2014-08-07 15:18:48 -0700

    Create system messages from ScheduledMessage
    When the system_message flag is set, create an instance of the new
    subclass.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140807155944) do
+ActiveRecord::Schema.define(:version => 20140807220715) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
