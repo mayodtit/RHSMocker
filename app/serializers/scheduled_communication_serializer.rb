1b73bad6413834734644dbc446dd47dc4f15e347
Kyle Chilcutt <kyle@getbetter.com>
2014-08-07 17:51:28 -0700

    Serialize type with ScheduledCommunications
    Show the type to the clients so they can render it to the
    administrators.

--- app/serializers/scheduled_communication_serializer.rb
+++ app/serializers/scheduled_communication_serializer.rb
@@ -2,5 +2,6 @@ class ScheduledCommunicationSerializer < ActiveModel::Serializer
   self.root = false
 
   attributes :id, :sender_id, :recipient_id, :state, :publish_at,
-             :delivered_at, :created_at, :updated_at, :state_events
+             :delivered_at, :created_at, :updated_at, :state_events,
+             :type
 end
