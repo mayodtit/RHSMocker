4e1e71ad261464b8dc771fb5f235d949f36932bb
Kyle Chilcutt <kyle@getbetter.com>
2014-08-11 18:34:38 -0700

    Seed Meg as the Mayo Pilot PHA
    Set her PhaProfile#mayo_pilot to true to have her set as the Mayo Pilot
    PHA.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140811215817) do
+ActiveRecord::Schema.define(:version => 20140812012656) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
