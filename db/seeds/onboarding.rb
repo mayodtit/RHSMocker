93d562f4e85f79ae5bab2037a94b6c2a6d30b55e
Abhik Pramanik <abhik@getbetter.com>
2014-08-19 16:37:29 -0700

    Response JSON after signing up now returns 'text_referencing_nux_answer' for use on the bio page.
    [Finishes #76996482]

--- db/seeds/onboarding.rb
+++ db/seeds/onboarding.rb
@@ -3,5 +3,5 @@ OnboardingGroup.find_or_create_by_name(name: 'Generic 14-day trial onboarding gr
                                        free_trial_days: 14)
 
 Metadata.find_or_create_by_mkey(mkey: 'nux_question_text', mvalue: 'First, choose what you need help with. Then we’ll pair you up with a Personal Health Assistant to work with you and your loved ones.')
-NuxAnswer.upsert_attributes({name: 'medical conditions'}, text: 'Managing medical conditions', phrase: 'managing medical conditions', active: true, ordinal: 2)
+NuxAnswer.upsert_attributes({name: 'medical conditions'}, text: 'Managing medical conditions', phrase: 'managing your medical conditions', active: true, ordinal: 2)
 NuxAnswer.upsert_attributes({name: 'something else'}, text: 'Something else', phrase: 'working with you', active: true, ordinal: 1)
