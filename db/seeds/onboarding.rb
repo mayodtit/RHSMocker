aa40b378a8ba6c68addfbeb645b7801dcba3fa28
Abhik Pramanik <abhik@getbetter.com>
2014-08-21 17:17:58 -0700

    Updating NUX copy.

--- db/seeds/onboarding.rb
+++ db/seeds/onboarding.rb
@@ -2,7 +2,7 @@ OnboardingGroup.find_or_create_by_name(name: 'Generic 14-day trial onboarding gr
                                        premium: true,
                                        free_trial_days: 14)
 
-Metadata.find_or_create_by_mkey(mkey: 'nux_question_text', mvalue: 'You’re invited to experience the service of a Personal Health Assistant. Choose an immediate need or focus for your free trial:')
+Metadata.find_or_create_by_mkey(mkey: 'nux_question_text', mvalue: 'You’re just two steps away from the experience of a Personal Health Assistant. What would you like to focus on during your free trial:')
 
 # Provider search --
 
