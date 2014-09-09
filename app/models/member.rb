1815fdf733d8990cca8ace8a7d93f86c955dc9ca
Kyle Chilcutt <kyle@getbetter.com>
2014-09-09 14:55:25 -0700

    Remove Member#engaged? and specs
    No longer used with new offboarding method.

--- app/models/member.rb
+++ app/models/member.rb
@@ -178,11 +178,6 @@ class Member < User
     @role_names << role.name.to_s if @role_names
   end
 
-  def engaged?
-    tasks.joins(:service_type).where(service_types: {bucket: ['wellness', 'care coordination', 'insurance', 'other']}).count > 0 ||
-    messages.count > 1
-  end
-
   def admin?
     has_role?(:admin)
   end
