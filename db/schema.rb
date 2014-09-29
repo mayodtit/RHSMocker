81d434d58589d242a8d1d6f58abb5f2c65201549
Kyle Chilcutt <kyle@getbetter.com>
2014-09-29 08:52:59 -0700

    Add content to scheduled messages
    Allow scheduling of content attachment with scheduled messages.
    
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140924183301) do
+ActiveRecord::Schema.define(:version => 20140929155016) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -614,6 +614,7 @@ ActiveRecord::Schema.define(:version => 20140924183301) do
     t.string   "reference_type"
     t.string   "reference_event"
     t.integer  "relative_days"
+    t.integer  "content_id"
   end
 
   create_table "scheduled_phone_calls", :force => true do |t|
