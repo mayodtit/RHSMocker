a5fb6dfd8ed5271ae78bc89602688a5bc06b64ae
Kyle Chilcutt <kyle@getbetter.com>
2014-09-30 08:52:14 -0700

    Add seed_fast db:seed ENV target
    Skip contents and symptoms.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
      load File.join(Rails.root, 'db', 'seeds', "#{filename}.rb")
    end
 
-if ENV['seed_users'] || ENV['seed_all']
+if ENV['seed_users'] || ENV['seed_fast'] || ENV['seed_all']
   load File.join(Rails.root, 'db', 'seeds', 'users.rb')
 end
 
@@ -25,10 +25,10 @@ if ENV['seed_symptoms'] || ENV['seed_all']
   Rake::Task["admin:import_symptoms"].execute
 end
 
-if ENV['seed_metadata'] || ENV['seed_all']
+if ENV['seed_metadata'] || ENV['seed_fast'] ||  ENV['seed_all']
   load File.join(Rails.root, 'db', 'seeds', 'metadata.rb')
 end
 
-if ENV['seed_onboarding'] || ENV['seed_all']
+if ENV['seed_onboarding'] || ENV['seed_fast'] || ENV['seed_all']
   load File.join(Rails.root, 'db', 'seeds', 'onboarding.rb')
 end
