12b947a18fc4bede95f79b1c14779b6fcc3dca33
Kyle Chilcutt <kyle@getbetter.com>
2014-10-16 21:34:17 -0700

    Change ON_CALL_START_HOUR to 6AM
    Open for business at 6AM.

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,4 +1,4 @@
-ON_CALL_START_HOUR = 7
+ON_CALL_START_HOUR = 6
 ON_CALL_END_HOUR = 18
 BusinessTime::Config.beginning_of_workday = "#{ON_CALL_START_HOUR}:00 am"
 BusinessTime::Config.end_of_workday = "#{ON_CALL_END_HOUR % 12}:00 pm"
