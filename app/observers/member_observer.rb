38532efd5b76be03bdb0b10ca71f14504d638c31
Kyle Chilcutt <kyle@getbetter.com>
2014-10-19 19:04:23 -0700

    Fix early exit condition when status is unchanged
    Always bail if member's status is not changed.

--- app/observers/member_observer.rb
+++ app/observers/member_observer.rb
@@ -6,9 +6,8 @@ class MemberObserver < ActiveRecord::Observer
   private
 
   def add_automated_communication_workflows(member)
-    return unless member.trial? || member.previous_changes[:status]
+    return unless member.trial? && member.previous_changes[:status]
     return unless member.inbound_scheduled_communications.empty?
-
     member.transaction do
       if Metadata.automated_onboarding?
         CommunicationWorkflow.automated_onboarding.try(:add_to_member, member)
