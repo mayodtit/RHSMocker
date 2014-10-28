a3b9d1441eba2f915be2d348774c5dab704866ea
Kyle Chilcutt <kyle@getbetter.com>
2014-10-28 13:04:36 -0700

    Add unique_id to NUX story
    Provide a way to reference the story in seeds.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141028185036) do
+ActiveRecord::Schema.define(:version => 20141028195714) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -468,6 +468,7 @@ ActiveRecord::Schema.define(:version => 20141028185036) do
     t.boolean  "show_nav_signup"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.string   "unique_id"
   end
 
   create_table "onboarding_group_cards", :force => true do |t|
--- spec/factories/nux_stories.rb
+++ spec/factories/nux_stories.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     html "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fermentum erat et felis eleifend consequat at id est. Nulla ultricies est justo, non fringilla lacus egestas eget."
     action_button_text 'Next'
     show_nav_signup true
+    sequence(:unique_id) {|n| "NuxStory#{n}"}
   end
 end
