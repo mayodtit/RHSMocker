3881c314df08507d20049ec5f1dce291f96fc7a6
Kyle Chilcutt <kyle@getbetter.com>
2014-10-27 20:58:02 -0700

    Add credit card NUX placeholder text
    Placeholder for the iOS client.

--- app/controllers/api/V1/next_nux_step_controller.rb
+++ app/controllers/api/V1/next_nux_step_controller.rb
@@ -9,7 +9,17 @@ class Api::V1::NextNuxStepController < Api::V1::ABaseController
 
   def next_step
     {
-      next: :credit_card
+      next: :credit_card,
+      credit_card_description: credit_card_description,
+      credit_card_action: credit_card_action
     }
+  end
+
+  def credit_card_description
+    "Credit Card Description will go here, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas justo sapien, vulputate ac ligula quis, scelerisque placerat lacus."
+  end
+
+  def credit_card_action
+    "Get Your Free Trial"
   end
 end
