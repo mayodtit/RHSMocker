e2d09220cffc2659d00838552cb36b03c20e9aba
Emilio Zand <emilio@getbetter.com>
2014-10-28 18:35:52 -0700

    Fixed bugs to implement the notification system, also added copy for notifications [#81558064]

--- app/models/task_change.rb
+++ app/models/task_change.rb
@@ -19,9 +19,9 @@ class TaskChange < ActiveRecord::Base
     else
       actor_name = actor.first_name
     end
-
-    if (event == 'update' && data.has_key?('owner_id')) || (event.nil? && !task.owner_id.nil? && task.owner_id != actor_id)
-      if !data['owner_id'].second.nil? && data['owner_id'].second != actor_id
+    # For existing tasks assigned to someone
+    if event == 'update' && data.has_key?('owner_id')
+      if data['owner_id'].second != actor_id
         case task.type
           when 'MemberTask'
             message = "#{actor_name} assigned you a #{task.service_type.bucket} task"
@@ -40,6 +40,26 @@ class TaskChange < ActiveRecord::Base
         end
         PubSub.publish "/users/#{task.owner_id}/tasks/owned/notification", {msg: message, id: task_id, assignedTo: task.owner_id}
       end
+    # For new tasks assigned to someone
+    elsif (event.nil? && !task.owner_id.nil? && task.owner_id != actor_id)
+        case task.type
+          when 'MemberTask'
+            message = "#{actor_name} assigned you a #{task.service_type.bucket} task"
+          when 'PhoneCallTask'
+            message = "#{actor_name} assigned you to a call"
+          when 'MessageTask'
+            message = "#{actor_name} assigned you a message"
+          when 'UserRequestTask'
+            message = "#{actor_name} assigned you an appointment request"
+          when 'ParsedNurselineRecordTask'
+            message = "#{actor_name} assigned you a nurseline sumamry"
+          when 'UpgradeTask'
+            message = "#{actor_name} assigned you a user upgrade task"
+          when 'OffboardMemberTask'
+            message = "#{actor_name} assigned you an offboard task"
+        end
+        PubSub.publish "/users/#{task.owner_id}/tasks/owned/notification", {msg: message, id: task_id, assignedTo: task.owner_id}
+    # For new tasks unassigned
     elsif event.nil? && task.owner_id.nil?
       case task.type
         when 'MemberTask', 'task', 'member'
