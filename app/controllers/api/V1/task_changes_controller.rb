bc6a73aaa9acea9ac2378b33de24847c90ed927b
Abhik Pramanik <abhik@getbetter.com>
2014-11-07 10:59:28 -0800

    Task changes is now paginated.

--- app/controllers/api/V1/task_changes_controller.rb
+++ app/controllers/api/V1/task_changes_controller.rb
@@ -1,12 +1,27 @@
 class Api::V1::TaskChangesController < Api::V1::ABaseController
   before_filter :load_user!
-  before_filter :authorize_user!
 
   def index
-    index_resource TaskChange.where(actor_id: current_user.id).order('created_at DESC').includes(:actor, task: :member).serializer
+    authorize! :read, TaskChange
+    task_changes = TaskChange
+      .where(actor_id: current_user.id).order('created_at DESC')
+      .page(page).per(per)
+      .includes(:actor, task: :member)
+
+    render_success task_changes: task_changes.serializer,
+                   page: page,
+                   per: per,
+                   total_count: task_changes.total_count
   end
 
   def authorize_user!
-    authorize! :read, TaskChange
+  end
+
+  def page
+    params[:page] || 1
+  end
+
+  def per
+    params[:per] || 50
   end
 end
--- spec/acceptance/api/v1/task_changes_spec.rb
+++ spec/acceptance/api/v1/task_changes_spec.rb
@@ -11,23 +11,28 @@ resource "TaskChanges" do
   let(:auth_token) { session.auth_token }
   let!(:task_change) { create(:task_change, actor: pha, created_at: 3.days.ago)}
   let!(:another_task_change) { create(:task_change, actor: pha, created_at: 4.days.ago)}
+  let!(:task_change_3) { create(:task_change, actor: pha, created_at: 5.days.ago)}
+  let!(:task_change_4) { create(:task_change, actor: pha, created_at: 6.days.ago)}
   let!(:yet_another_task_change) { create(:task_change, actor: other_pha, created_at: 1.days.ago)}
 
   describe 'tasks' do
     parameter :auth_token, 'Performing hcp\'s auth_token'
     parameter :member_id, 'PHA to retrieve tasks for'
-
+    parameter :page, 'Page of tasks'
+    parameter :per, 'Changes per page'
     required_parameters :auth_token, :member_id
 
     let(:auth_token) { session.auth_token }
     let(:member_id) { pha.id }
+    let(:page) { 2 }
+    let(:per) { 2 }
 
     get '/api/v1/members/:member_id/task_changes' do
       example_request '[GET] Get all task changes' do
         explanation 'Get all task changes for a PHA where they are the actor, most recent first. Accessible only by HCPs'
         status.should == 200
         response = JSON.parse response_body, symbolize_names: true
-        response[:task_changes].to_json.should == [task_change, another_task_change].serializer.to_json
+        response[:task_changes].to_json.should == [task_change_3, task_change_4].serializer.to_json
       end
     end
   end
