fe2db1622a3118fee3dd3de3c08608e601251a39
Kyle Chilcutt <kyle@getbetter.com>
2014-11-18 10:52:43 -0800

    Mock out Stripe in PlansControllerSpec
    Don't connect to the interwebs, just check the interfaces.

--- spec/controllers/api/v1/plans_controller_spec.rb
+++ spec/controllers/api/v1/plans_controller_spec.rb
@@ -1,9 +1,28 @@
+require 'spec_helper'
+require 'stripe_mock'
+
 describe Api::V1::PlansController do
   let(:user) { build_stubbed(:user) }
   let(:ability) { Object.new.extend(CanCan::Ability) }
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  let(:plan_id) { 'bp20' }
 
   before do
+    StripeMock.start
+    Stripe::Plan.create(amount: 1999,
+                        interval: :month,
+                        name: 'Single Membership',
+                        currency: :usd,
+                        id: plan_id,
+                        metadata: {
+                          display_name: 'Single Membership',
+                          display_price: '$19.99/month'
+                        })
     controller.stub(current_ability: ability)
+  end
+
+  after do
+    StripeMock.stop
   end
 
   describe 'GET index' do
