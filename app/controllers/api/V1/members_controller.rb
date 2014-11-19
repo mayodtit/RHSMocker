40cc0c163dfb4cabdbfc9b18df3c51f622484fd8
Kyle Chilcutt <kyle@getbetter.com>
2014-11-18 19:46:13 -0800

    Add SendWelcomeCallEmailService for new users
    Invoke a new service instead of the callback in order to reliably send
    welcome emails.

--- app/controllers/api/V1/members_controller.rb
+++ app/controllers/api/V1/members_controller.rb
@@ -35,6 +35,7 @@ class Api::V1::MembersController < Api::V1::ABaseController
       rescue => e
         logger.error("StripeSubscriptionService error: #{e}")
       end
+      SendWelcomeEmailService.new(@member).call
       SendConfirmEmailService.new(@member).call
       render_success user: @member.serializer,
                      member: @member.reload.serializer,
