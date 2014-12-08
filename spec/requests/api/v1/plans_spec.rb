8ca0e861b5757008855c77ba6567695641a7b117
Wuang Qin <wuang@getbetter.com>
2014-12-08 13:11:44 -0800

    Plans#index should return the text header for the plans page
    changed the plans controller#index method, so the json response would contain
    the text header, also added spec to test it.
    
    [Finishes: #83782152]

--- spec/requests/api/v1/plans_spec.rb
+++ spec/requests/api/v1/plans_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe 'Plans' do
   let!(:user) { create(:member) }
-
+  let!(:session) { user.sessions.create }
   describe 'GET /api/v1/plans' do
     def do_request
-      get '/api/v1/plans', auth_token: user.auth_token
+      get '/api/v1/plans', auth_token: session.auth_token
     end
 
+    it 'return plans with text header' do
+      do_request
+      byebug
+      body = JSON.parse(response.body, symbolize_names: true)
+      expect(body[:text_header]).to eq('Please select the plan below:')
+    end
+
+    it_behaves_like 'success'
     # it 'returns all the plans' do
     #   do_request
     #   expect(response).to be_success
