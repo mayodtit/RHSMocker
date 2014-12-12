41bf5aa64c91c004e9bb268395b3f284f9f0ba81
Kyle Chilcutt <kyle@getbetter.com>
2014-12-11 18:29:12 -0800

    Always set_pha on member create
    Even when in free state in order to allow us to show a PHA in the "Your
    PHA" section.
    
    [#84263126]

--- app/models/member.rb
+++ app/models/member.rb
@@ -114,7 +114,7 @@ class Member < User
   before_validation :unset_free_trial_ends_at
   before_validation :set_invitation_token
   before_validation :unset_invitation_token
-  before_validation :set_pha, if: ->(m){m.is_premium? && m.status_changed?}
+  before_validation :set_pha, if: ->(m){m.signed_up? && m.status_changed?}
   before_validation :set_master_consult, if: ->(m){m.is_premium? && m.status_changed?}
   after_create :add_new_member_content
   after_create :add_owned_referral_code
