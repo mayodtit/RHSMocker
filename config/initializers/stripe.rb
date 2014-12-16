c9d25ae7908434fbd15ed559cc1df2afbe454936
Wuang Qin <wuang@getbetter.com>
2014-12-16 12:13:33 -0800

    When a referree pays for their first month, the referrer's account should be credited for 1 month of the referrer's subscription
    initial commit, for a code review about the workflow and logic

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,9 +6,30 @@ module Stripe
   end
 end
 
+OneTimeFiftyPercentOffCoupon = 'OneTimeFiftyPercentOffCoupon'
+
 StripeEvent.configure do |events|
   events.subscribe 'charge.failed' do |event|
     Rails.logger.info("Received Stripe charge.failed, #{event.id} - #{event.type}")
     UserMailer.delay.notify_bosses_when_user_payment_fail(event)
+  end
+
+  events.subscribe 'change.succeeded' do |event|
+    stripe_customer_id = event.data.object.card.customer
+    referee = User.find(stripe_customer_id)
+    referral_code ||= ReferralCode.find(referee.referral_code_id)
+    referrer = User.find(referral_code.user_id) if referral_code && referral_code.user_id
+    referrer.coupon_number += 1
+    referee.referral_code_id = nil
+  end
+
+  events.subscribe 'invoice.created' do |event|
+    stripe_customer_id = event.customer
+    referrer_stripe_customer = Stripe::Customer.retrieve(stripe_customer_id)
+    referrer = User.find(stripe_customer_id)
+    if referrer.coupon_number != 0 && referrer_stripe_customer.discount.nil?
+      referrer_stripe_customer.coupon = OneTimeFiftyPercentOffCoupon
+      referrer.coupon_number -= 1
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141215001744) do
+ActiveRecord::Schema.define(:version => 20141216022653) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -1156,6 +1156,7 @@ ActiveRecord::Schema.define(:version => 20141215001744) do
     t.string   "advertiser_media_source"
     t.string   "advertiser_campaign"
     t.integer  "impersonated_user_id"
+    t.integer  "coupon_number",                                 :default => 0,     :null => false
   end
 
   add_index "users", ["email", "member_flag"], :name => "index_users_on_email_and_member_flag", :unique => true
