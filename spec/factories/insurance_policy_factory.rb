c7e10c949f9d075f0b93cf92ad3364303592efd0
Neel Anand <neel@getbetter.com>
2014-12-20 18:48:50 -0800

    Adding functionality to add parameters for the front & back of the insurance card image. Adding association with the UserImage and the InsurancePolicy. [Finishes #84345508]

--- spec/factories/insurance_policy_factory.rb
+++ spec/factories/insurance_policy_factory.rb
@@ -3,7 +3,21 @@ FactoryGirl.define do
     user
     company_name "InsurancePolicy company name"
     plan_type "InsurancePolicy plan type"
+    plan "InsurancePolicy plan"
     sequence(:policy_member_id) {|n| "InsurancePolicy policy member id #{n}"}
-    notes "notes"
+    sequence(:group_number) {|n| "InsurancePolicy group number #{n}"}
+    effective_date "12/11/1989"
+    termination_date "12/12/1989"
+    sequence(:member_services_number) {|n| "InsurancePolicy member services number #{n}"}
+    authorized false
+    insurance_image "InsuranceImage Insurance Image"
+    sequence(:user_image_id) {|n| "InsurancePolicy Insurance image id #{n}"}
+    sequence(:user_image_client_guid) {|n| "InsurancePolicy Insurance image client guid #{n}"}
+    image :base64_test_image
+    subscriber_name "InsurancePolicy subscriber name"
+    family_individual "InsurancePolicy Family Individual"
+    employer_individual "InsurancePolicy Employer Individual"
+    employer_exchange "InsurancePolicy Employer Exchange"
+    notes "InsurancePolicy notes"
   end
 end
