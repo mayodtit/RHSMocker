f8e143b03b62e352b249c6201ca860dac8e556ee
Neel Anand <neel@getbetter.com>
2014-12-22 15:15:50 -0800

    Changing the attributes in the insurance_policy_factory.

--- spec/factories/insurance_policy_factory.rb
+++ spec/factories/insurance_policy_factory.rb
@@ -10,14 +10,16 @@ FactoryGirl.define do
     termination_date "12/12/1989"
     sequence(:member_services_number) {|n| "InsurancePolicy member services number #{n}"}
     authorized false
-    insurance_image "InsuranceImage Insurance Image"
-    sequence(:user_image_id) {|n| "InsurancePolicy Insurance image id #{n}"}
-    sequence(:user_image_client_guid) {|n| "InsurancePolicy Insurance image client guid #{n}"}
-    image :base64_test_image
     subscriber_name "InsurancePolicy subscriber name"
     family_individual "InsurancePolicy Family Individual"
     employer_individual "InsurancePolicy Employer Individual"
     employer_exchange "InsurancePolicy Employer Exchange"
+    insurance_card_front nil
+    insurance_card_back nil
+    sequence(:insurance_card_front_id) {|n| "InsurancePolicy Insurance Card Front Id#{n}"}
+    sequence(:insurance_card_back_id) {|n| "InsurancePolicy Insurance Card Back Id#{n}"}
+    sequence(:insurance_card_front_client_guid) {|n| "InsurancePolicy Insurance Card Front Client GUID#{n}"}
+    sequence(:insurance_card_back_client_guid) {|n| "InsurancePolicy Insurance Card Back Client GUID#{n}"}
     notes "InsurancePolicy notes"
   end
 end
