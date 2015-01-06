888ccff4ead17c25226ab710ad3add4a946ff5ff
Emilio Zand <emilio@getbetter.com>
2015-01-06 15:26:14 -0800

    Fixed tests and factory for guides.

--- spec/factories/task_guide_factory.rb
+++ spec/factories/task_guide_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
-  factory :guide do
+  factory :task_guide do
     task_template
+    title "title"
     description "description"
   end
 end
--- spec/models/task_guide_spec.rb
+++ spec/models/task_guide_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe TaskGuide do
   it_has_a 'valid factory'
   it_validates 'presence of', :task_template
-  it_validates 'presence of', :description
+  it_validates 'presence of', :title
 end
