c74799d1de9736639312e311dbbdd16d793f814d
Wuang Qin <wuang@getbetter.com>
2015-01-06 18:36:25 -0800

    Changes to UserInformation should be logged in UserChange
    In a similar way to UserCondition
    
    [Finishes: #82999292]

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -5,4 +5,17 @@ class UserInformation < ActiveRecord::Base
 
   validates :user, presence: true
   validates :user_id, uniqueness: true
+
+  after_destroy :track_destroy
+  after_create :track_create
+
+  def track_create
+    self.actor_id ||= Member.robot.id
+    UserChange.create! user: user, actor_id: actor_id, action: 'add', data: {infomations: [user.first_name, user.last_name]}.to_s
+  end
+
+  def track_destroy
+    self.actor_id ||= Member.robot.id
+    UserChange.create! user: user, actor_id: actor_id, action: 'destroy', data: {informations: [user.first_name, user.last_name]}.to_s
+  end
 end
