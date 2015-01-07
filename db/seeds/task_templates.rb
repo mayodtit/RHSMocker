d686695772c043b164f47c97ece7d71beb97246a
Emilio Zand <emilio@getbetter.com>
2015-01-06 17:41:59 -0800

    Changed Copy again.

--- db/seeds/task_templates.rb
+++ db/seeds/task_templates.rb
@@ -50,18 +50,10 @@ TaskTemplate.find_or_create_by_name(
     service_ordinal: 1
 )
 TaskTemplate.find_or_create_by_name(
-    name: "complete member profile",
-    service_template: ServiceTemplate.find_by_name('30 day experience'),
-    title: "Complete Profile",
-    description: "Data to complete:\n\n - Address\n - Birthday\n - Gender\n - Insurance Information\n - Current Care Team\n - Medical Conditions\n - Height and Weight (for WM only)\n - Due date (for Pregnancy only)",
-    time_estimate: 10080,
-    service_ordinal: 1
-)
-TaskTemplate.find_or_create_by_name(
     name: "offer insurance review service",
     service_template: ServiceTemplate.find_by_name('30 day experience'),
     title: "Offer Insurance Review Service",
-    description: "Message member to offer service:\n\n\"Chances are you may not know everything about your health insurance coverage, like if you’re using all of your benefits or what appointments are covered. I’d like to break down your plan to see if you’re getting the most out of your insurance. Does that sound helpful to you?\"",
+    description: "Message member to offer service:\n\nChances are you may not know everything about your health insurance coverage, like if you’re using all of your benefits or what appointments are covered. I’d like to break down your plan to see if you’re getting the most out of your insurance. Does that sound helpful to you?",
     time_estimate: 10080,
     service_ordinal: 1
 )
@@ -69,7 +61,7 @@ TaskTemplate.find_or_create_by_name(
     name: "prevention screenings",
     service_template: ServiceTemplate.find_by_name('30 day experience'),
     title: "Perform Preventive Screening Service",
-    description: "See website for details based on age/gender (http://betterpha.squarespace.com/30-day-experience-1-1-3)",
+    description: "See website for details based on age/gender - http://betterpha.squarespace.com/30-day-experience-1-1-3",
     time_estimate: 10080,
     service_ordinal: 1
 )
@@ -77,7 +69,15 @@ TaskTemplate.find_or_create_by_name(
     name: "offer creating care team service",
     service_template: ServiceTemplate.find_by_name('30 day experience'),
     title: "Offer Creating Care Team Service",
-    description: "Message member to offer service:\n\n\"We want to ensure that you have doctors you need and like, are organized with your annual appointment schedule, and have a health record that is shared among their current providers. Would this be helpful for you?\"",
+    description: "Message member to offer service:\n\nWe want to ensure that you have doctors you need and like, are organized with your annual appointment schedule, and have a health record that is shared among their current providers. Would this be helpful for you?",
+    time_estimate: 10080,
+    service_ordinal: 1
+)
+TaskTemplate.find_or_create_by_name(
+    name: "complete member profile",
+    service_template: ServiceTemplate.find_by_name('30 day experience'),
+    title: "Complete Profile",
+    description: "Data to complete:\n\n - Address\n - Birthday\n - Gender\n - Insurance Information\n - Current Care Team\n - Medical Conditions\n - Height and Weight (for WM only)\n - Due date (for Pregnancy only)",
     time_estimate: 10080,
     service_ordinal: 1
 )
