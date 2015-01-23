f14ec9c6cb9e40fcc87e51516cb097b300cbeb8d
Wuang Qin <wuang@getbetter.com>
2015-01-23 15:43:07 -0800

    changed the track_update method to track changes

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -7,22 +7,12 @@ class UserInformation < ActiveRecord::Base
   validates :user, presence: true
   validates :user_id, uniqueness: true
 
-  after_destroy :track_destroy
-  after_create :track_create
-  after_update :track_update
+  after_destroy track_changes('destroy')
+  after_create track_create('add')
+  after_update track_update('update')
 
-  def track_create
+  def track_changes(action)
     self.actor_id ||= Member.robot.id
-    UserChange.create! user: user, actor_id: actor_id, action: 'add', data: {informations: [user.first_name, user.last_name]}.to_s
-  end
-
-  def track_update
-    self.actor_id ||= Member.robot.id
-    UserChange.create! user: user, actor_id: actor_id, action: 'update', data: {information: [user.first_name, user.last_name]}.to_s
-  end
-
-  def track_destroy
-    self.actor_id ||= Member.robot.id
-    UserChange.create! user: user, actor_id: actor_id, action: 'destroy', data: {informations: [user.first_name, user.last_name]}.to_s
+    UserChange.create! user: user, actor_id: actor_id, action: action, data: {changed_attributes: [ UserInformation.changed ]}.to_s
   end
 end
