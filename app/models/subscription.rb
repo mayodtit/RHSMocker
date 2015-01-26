abfa628c3a804c986381f39ac0d026558ea8fb2d
Kyle Chilcutt <kyle@getbetter.com>
2015-01-25 23:57:34 -0800

    Reduce validations of Subscription
    Simplify restrictions of Subscription model to allow for more use cases.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,15 +1,5 @@
 class Subscription < ActiveRecord::Base
   belongs_to :owner, class_name: 'Member'
 
-  validates :owner, presence: true
-  validates :owner_id, uniqueness: true
-  validate :owner_is_premium
-
-  private
-
-  def owner_is_premium
-    unless owner.try(:is_premium?)
-      errors.add(:owner, 'must be premium')
-    end
-  end
+  validates :owner, presence: true, if: ->(s){s.owner_id}
 end
