f915c18aea4493aa91a85617fb60a3c936ca0267
Wuang Qin <wuang@getbetter.com>
2015-01-27 13:56:49 -0800

    transfer available plans from subscription controller to plans controller

--- app/controllers/api/V1/plans_controller.rb
+++ app/controllers/api/V1/plans_controller.rb
@@ -6,7 +6,21 @@ class Api::V1::PlansController < Api::V1::ABaseController
                    text_header: text_header)
   end
 
+  def available_plans
+    render_success(available_plans: available_plans)
+  end
+
   private
+
+  def load_available_plans!
+    subscribe_options = []
+    Stripe::Plan.all.each{|plan| subscribe_options << plan }
+    if current_user.stripe_customer_id
+      subscribe_options.delete_if{|option| option.amount < Stripe::Customer.retrieve(current_user.stripe_customer_id).subscriptions.data[0].plan.amount}
+    else
+      subscribe_options
+    end
+  end
 
   def load_plans!
     @plans = Stripe::Plan.all.inject([]) do |array, plan|
