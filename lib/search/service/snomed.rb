543592781bd49222324b15f28c9db3d452593c2d
mishuan <yuanshihao1994@gmail.com>
2015-01-28 15:47:35 -0800

    Can now properly send and recieve to and from snomed

--- lib/search/service/snomed.rb
+++ lib/search/service/snomed.rb
@@ -4,15 +4,20 @@ class Search::Service::Snomed
   base_uri 'http://127.0.0.1:4000/snomed/en-edition/v20140901/'
 
   def query(params)
-    if params[:controller] == 'api/v1/allergies'
-      query_params = allergy_query(params)
-    else
-      query_params = condition_query(params)
-    end
     byebug
-    response = self.class.get('/concepts', :query => query_params)
+    query_params = select_query(params)
+    response = self.class.get('/concepts/' + query_params)
     raise StandardError, 'Non-success response from SNOMED database' unless response.success?
     sanitize_response(response.parsed_response)
+  end
+
+  def select_query(params)
+    if params[:controller] == 'api/v1/allergies'
+      new_params = allergy_query(params)
+    else
+      new_params = condition_query(params)
+    end
+    new_params
   end
 
   def allergy_query(params)
