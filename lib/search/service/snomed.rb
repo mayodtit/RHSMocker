ff8eccd436623d49da2ef89efed9d9dc34f49191
mishuan <yuanshihao1994@gmail.com>
2015-01-28 15:24:34 -0800

    refactored some methods and have requests partially working

--- lib/search/service/snomed.rb
+++ lib/search/service/snomed.rb
@@ -1,25 +1,31 @@
 class Search::Service::Snomed
   include HTTParty
   #TODO: version should be a changeable global variable set somewhere
-  base_uri 'http://107.170.143.181/api/snomed/en-edition/v20140731/'
+  base_uri 'http://127.0.0.1:4000/snomed/en-edition/v20140901/'
 
   def query(params)
-    byebug
     if params[:controller] == 'api/v1/allergies'
       query_params = allergy_query(params)
     else
       query_params = condition_query(params)
     end
-    response = self.class.get('/api/search', :query => query_params)
+    byebug
+    response = self.class.get('/concepts', :query => query_params)
     raise StandardError, 'Non-success response from SNOMED database' unless response.success?
     sanitize_response(response.parsed_response)
   end
 
   def allergy_query(params)
     params[:q] = 'dog'
+    result = '91936005'
+    result
   end
 
   def condition_query(params)
+
+  end
+
+  def sanitize_response(response)
 
   end
 end
