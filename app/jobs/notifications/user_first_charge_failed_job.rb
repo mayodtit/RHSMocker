9f44e45c02379070343537995c6c39e83b46ecdc
Wuang Qin <wuang@getbetter.com>
2015-01-29 14:56:16 -0800

    added the send push-notification function

---
+++ app/jobs/notifications/user_first_charge_failed_job.rb
@@ -0,0 +1,22 @@
+class Notifications::UserFirstChargeFailedJob < Struct.new(:user_id)
+  def self.create(user_id)
+    Delayed::Job.enqueue(new(user_id))
+  end
+
+  def perform
+    user = User.find(user_id)
+    sessions = user.sessions.where('apns_token IS NOT NULL')
+    if sessions.any?
+      sessions.each do |session|
+        APNS.send_notification(session.apns_token, alert: alert_text,
+                               sound: :default)
+      end
+    end
+  end
+
+  private
+
+  def alert_text
+    'Your Charge Failed! Pay your money,man!'
+  end
+end
