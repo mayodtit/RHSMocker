8f25ff806fc1b5a7fea9238db030cf39ca70c1d4
Wuang Qin <wuang@getbetter.com>
2015-02-03 15:28:33 -0800

    starting the spec

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -10,9 +10,6 @@ ONE_TIME_FIFTY_PERCENT_OFF_COUPON_CODE = 'OneTimeFiftyPercentOffCoupon'
 ONE_TIME_HUNDRED_PERCENT_OFF_COUPON_CODE = "OneTimeHundredPercentOffCoupon"
 
 StripeEvent.configure do |events|
-  customer = Stripe::Customer.retrieve(event.data.object.customer)
-  user = User.find_by_stripe_customer_id(customer.id)
-
   events.subscribe 'charge.failed' do |event|
     Rails.logger.info("Received Stripe charge.failed, #{event.id} - #{event.type}")
     UserMailer.delay.notify_bosses_when_user_payment_fail(event)
---
+++ spec/services/send_charge_failed_notification.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'stripe_mock'
+
+describe '#send' do
+
+end
