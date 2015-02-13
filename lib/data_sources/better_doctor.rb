e651df9d6c551e8abbe12602de2f65c032c81b19
Peter Christensen <peter@pchristensen.com>
2015-02-13 14:57:32 -0800

    BetterDoctor - add doctor search, refactor - make supporting methods private - write API wrapper fn to handle urls, error handling

--- lib/data_sources/better_doctor.rb
+++ lib/data_sources/better_doctor.rb
@@ -1,6 +1,22 @@
 class DataSources::BetterDoctor
   include HTTParty
 
+  ## API Methods
+
+  def self.lookup_by_npi(npi)
+    wrap_api_call("doctors/npi/#{npi}") do |data|
+      parse_doctor_response(data)
+    end
+  end
+
+  def self.search(opts = default_search_opts)
+    wrap_api_call("doctors?#{build_search_query(opts)}") do |data|
+      data.map{|doctor_response| parse_doctor_response(doctor_response)}
+    end
+  end
+
+  private
+
   ## TODO Store in config, environment, ??
   BETTER_DOCTOR_API = {
     api_key: "edd4c37c129961549d4f1882251b261c",
@@ -9,7 +25,7 @@ class DataSources::BetterDoctor
     ## TODO Our key doesn't work for the "2015-01-27" version of the API
   }
 
-  def self.build_query_str(arg_str)
+  def self.build_query_url(arg_str)
     return if arg_str.empty?
 
     query_param_suffix = if arg_str =~ /\?/
@@ -20,25 +36,53 @@ class DataSources::BetterDoctor
 
     arg_str += query_param_suffix
 
-    BETTER_DOCTOR_API[:base_url] +
-      BETTER_DOCTOR_API[:version] + "/" +
-      arg_str +
-      "user_key=" + BETTER_DOCTOR_API[:api_key]
+    "#{BETTER_DOCTOR_API[:base_url]}#{BETTER_DOCTOR_API[:version]}/#{arg_str}user_key=#{BETTER_DOCTOR_API[:api_key]}"
   end
 
-  def self.lookup_by_npi(npi)
-    npi_query_section = "doctors/npi/#{npi}"
-    query_url = build_query_str(npi_query_section)
+  ## TODO See API for payload fields - https://developer.betterdoctor.com/documentation
+  def self.parse_doctor_response(doctor_response)
+    { profile: doctor_response["profile"],
+      ratings: doctor_response["ratings"].map{|r| r["rating"]},
+      image_url: doctor_response["profile"]["image_url"]
+    }
+  end
 
-    response = HTTParty.get(query_url)
+  def self.default_search_opts
+    { user_location: { lat: "37.773", lon: "-122.413" },
+      distance: 10,
+      query: "pediatrician",
+      gender: "female"
+      ## TODO More fields (specialty, insurance, etc) available - https://developer.betterdoctor.com/documentation
+    }
+  end
+
+  ## TODO Extend if we want to search on more fields
+  def self.build_search_query(opts)
+    q = ""
+    ## Params that can be included w/o transformation: https://developer.betterdoctor.com/documentation
+    [:query, :gender].each do |k|
+      if opts[k].present?
+        q += "#{k}=#{opts[k]}&"
+      end
+    end
+
+    ## Params that need to be formatted
+    if opts[:user_location].present?
+      loc = "#{opts[:user_location][:lat]},#{opts[:user_location][:lon]}"
+      q += "user_location=#{loc}&"
+      if opts[:distance].present?
+        q += "location=#{loc},#{opts[:distance]}&"
+      end
+    end
+
+    q.gsub(/&$/,'')
+  end
+
+  def self.wrap_api_call(query_str)
+    response = HTTParty.get(build_query_url(query_str))
 
     if response.success?
-      ## TODO See API for payload fields - https://developer.betterdoctor.com/documentation
-      data = response["data"]
-      { profile: data["profile"],
-        ratings: data["ratings"].map{|r| r["rating"]},
-        image_url: data["profile"]["image_url"]
-      }
+      yield response["data"]
     else
       response_metadata = response["meta"]
       { error: response_metadata["message"], error_code: response_metadata["error_code"]}
