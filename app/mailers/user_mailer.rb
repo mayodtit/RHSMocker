0e30b5abfb4599b50c85d2a10388f58815be0e8b
Wuang Qin <wuang@getbetter.com>
2015-02-18 16:53:27 -0800

    adjust spacing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: lambda{
-    email = ( Rails.env.production? || Rails.env.demo? ) ? 'support@getbetter.com' : "support@#{Rails.env}.getbetter.com"
+    email = (Rails.env.production? || Rails.env.demo?) ? 'support@getbetter.com' : "support@#{Rails.env}.getbetter.com"
     from_address = Mail::Address.new(email)
     from_address.display_name = 'Better'
     from_address.format
