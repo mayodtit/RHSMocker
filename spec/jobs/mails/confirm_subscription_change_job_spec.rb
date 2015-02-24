9f5c8686dfa9e03d299375f66778171c028638f5
Wuang Qin <wuang@getbetter.com>
2015-02-24 15:58:54 -0800

    refactor the rspec

--- spec/jobs/mails/confirm_subscription_change_job_spec.rb
+++ spec/jobs/mails/confirm_subscription_change_job_spec.rb
@@ -1,31 +1,22 @@
 require 'spec_helper'
-require 'stripe_mock'
 
 describe Mails::ConfirmSubscriptionChangeJob do
   let(:stripe_helper) { StripeMock.create_test_helper }
   let(:credit_card_token) { stripe_helper.generate_card_token }
   let!(:user) { create(:member, :free) }
+  let!(:plan) { double( 'plan', :name => "stripe_plan")}
+  let!(:subscription) { double( 'subscription', :customer => "stripe_customer", :plan => plan)}
 
   before do
     Timecop.freeze(Date.today.to_time)
-    StripeMock.start
-    Stripe::Plan.create(amount: 1999,
-                        interval: :month,
-                        name: 'Better Premium, Single Membership',
-                        currency: :usd,
-                        id: 'bp20')
-    subscription = CreateStripeSubscriptionService.new(user: user, plan_id: 'bp20', credit_card_token: credit_card_token).call
     user.update_attribute(:stripe_customer_id, subscription.customer)
   end
 
   after do
     Timecop.return
-    StripeMock.stop
   end
 
   describe '::create' do
-    let!(:subscription) { subscription }
-
     it 'enqueues the job to run now' do
       expect{ described_class.create(user.id, subscription) }.to change(Delayed::Job, :count).by(1)
       job = Delayed::Job.last
@@ -34,8 +25,6 @@ describe Mails::ConfirmSubscriptionChangeJob do
   end
 
   describe '#perform' do
-    let!(:subscription) { subscription }
-
     it 'works' do
       described_class.new(user.id, subscription).perform
     end
