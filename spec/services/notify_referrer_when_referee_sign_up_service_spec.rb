88624d0470d00b5b129788523fd341c7a7caa57a
Wuang Qin <wuang@getbetter.com>
2015-02-24 16:28:35 -0800

    change spec according to the change of method

--- spec/services/notify_referrer_when_referee_sign_up_service_spec.rb
+++ spec/services/notify_referrer_when_referee_sign_up_service_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe NotifyReferrerWhenRefereeSignUpService do
-  let( :referrer ) { create(:member, :premium) }
-  let( :referee ) { create(:member)}
-
+  let(:referrer) { create(:member)}
+  let( :referral_code ) { create(:referral_code, user: referrer) }
+  let( :member ) { create(:member)}
   describe '#call' do
     it 'should send the notification email via delayed job' do
       Mails::NotifyReferrerOfSignUpJob.should_receive(:create).and_call_original
-      expect{ described_class.new( referrer: referrer, referee: referee ).call }.to change(Delayed::Job, :count).by(1)
+      expect{ described_class.new( referral_code, member ).call }.to change(Delayed::Job, :count).by(1)
     end
   end
 end
