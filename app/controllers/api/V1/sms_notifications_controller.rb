24fa30c27251b1c35ce0217b73c254b6d9e7f219
Kyle Chilcutt <kyle@getbetter.com>
2015-02-28 10:43:02 -0800

    Support sending SMS download link on signup
    For a web signup, when a certain flag is set during sign up, send an SMS
    to the user to instruct them to download the app. Move SMS functionality
    to a service to reuse infrastructure from the
    SmsNotificationsController.
    
    [#89343914]

--- app/controllers/api/V1/sms_notifications_controller.rb
+++ app/controllers/api/V1/sms_notifications_controller.rb
@@ -2,13 +2,7 @@ class Api::V1::SmsNotificationsController < Api::V1::ABaseController
   skip_before_filter :authentication_check
 
   def download
-    TwilioModule.message_now(params[:phone_number], download_text)
+    SendDownloadLinkService.new(params[:phone_number]).call
     head :ok
-  end
-
-  private
-
-  def download_text
-    "Download the Better app and get your own Personal Health Assistant http://www.getbetter.com/app"
   end
 end
