4c3bb98ac53dc7ba2a2815d5c19f75460526d9b5
mishuan <yuanshihao1994@gmail.com>
2015-03-02 14:35:49 -0800

    Mailcheck Pull Request Comments -made changes brought to attention in the comments

--- app/controllers/api/V1/validate_email_controller.rb
+++ app/controllers/api/V1/validate_email_controller.rb
@@ -1,28 +1,24 @@
-require 'valid_email'
 class Api::V1::ValidateEmailController < Api::V1::ABaseController
   skip_before_filter :authentication_check
 
   def index
-    email = params[:email]
-    @suggestion = suggest(email)
-    if @suggestion == false 
-      check_domain(email) ? render_success() : render_failure({reason: 'Invalid email domain, no suggestions found'}, 422)
+    if suggestion
+      valid_domain? ? render_success(suggestion) : render_failure(suggestion, 422)
     else
-      check_domain(email) ? render_success(@suggestion) : render_failure(@suggestion, 422)
+      valid_domain? ? render_success() : render_failure({reason: 'Invalid email domain, no suggestions found'}, 422)
     end
   end
 
   private
 
-  def suggest(email)
-    mailcheck = Mailcheck2.new(
+  def suggestion
+    @suggestion = Mailcheck2.new(
       :domains => MAILCHECK_DOMAINS,
       :top_level_domains => MAILCHECK_TOP_LEVEL_DOMAINS
-    )
-    mailcheck.suggest(email)
+    ).suggest(params[:email])
   end
 
-  def check_domain(email) 
-    ValidateEmail.valid?(email) && ValidateEmail.mx_valid?(email)
+  def valid_domain?
+    ValidateEmail.valid?(params[:email]) && ValidateEmail.mx_valid?(params[:email])
   end
 end
