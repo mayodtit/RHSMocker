0f1d77034e01fa0d28a16cf5ee18fe74aaec09c8
mishuan <yuanshihao1994@gmail.com>
2015-03-04 13:20:26 -0800

    update db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150228212218) do
+ActiveRecord::Schema.define(:version => 20150304202008) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
