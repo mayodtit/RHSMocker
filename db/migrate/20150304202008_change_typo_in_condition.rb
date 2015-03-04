8593d3f6ae9b133e28fca4fba3d517078b288e50
mishuan <yuanshihao1994@gmail.com>
2015-03-04 12:33:56 -0800

    Styling fixes -fixed poor ruby styling -last commit, i promise (hopefully)

--- db/migrate/20150304202008_change_typo_in_condition.rb
+++ db/migrate/20150304202008_change_typo_in_condition.rb
@@ -1,7 +1,6 @@
 class ChangeTypoInCondition < ActiveRecord::Migration
   def change
-    condition = Condition.find_by_name('Evelated Blood Pressure')
-    if condition
+    if condition = Condition.find_by_name('Evelated Blood Pressure')
       condition.name = 'Elevated Blood Pressure'
       condition.save!
     end
