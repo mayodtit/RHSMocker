404534a547c01713fe22e96b5f68ef1fc0eb1a8c
Wuang Qin <wuang@getbetter.com>
2015-03-05 18:24:51 -0800

    MemberServices controller should be combined with ServicesController

--- app/controllers/api/V1/services_controller.rb
+++ app/controllers/api/V1/services_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::ServicesController < Api::V1::ABaseController
-  before_filter :load_user!
-  before_filter :load_service!
+  before_filter :load_user!, only: [:show, :update]
+  before_filter :load_member!, only: [:index, :create]
+  before_filter :load_service!, only: [:show, :update]
+  before_filter :load_service_template!, only: :create
+
+  def index
+    authorize! :read, Service
+    services = @member.services.where(params.permit(:subject_id, :state)).order('due_at, created_at ASC')
+    index_resource services.serializer, name: :services
+  end
+
+  def create
+    authorize! :read, @service_template
+    authorize! :create, Service
+    create_params = service_template_attributes
+    @service = @service_template.create_service! create_params
+    render_success(service: @service.serializer)
+  end
 
   def show
     authorize! :read, @service
@@ -39,4 +55,17 @@ class Api::V1::ServicesController < Api::V1::ABaseController
       :reason_abandoned, :due_at
     )
   end
+
+  def service_template_attributes
+    params.permit(:title, :description, :subject_id, :due_at, :owner_id, :service_template_id, :auth_token).tap do |attributes|
+      attributes[:creator] = current_user
+      attributes[:member] = @member
+    end
+  end
+
+  def load_service_template!
+    @service_template = ServiceTemplate.find params[:service_template_id]
+  end
 end
+
+
