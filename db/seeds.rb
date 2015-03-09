b23bb64575405837bd66327d8552a554bb647be1
Wuang Qin <wuang@getbetter.com>
2015-03-09 11:52:15 -0700

    restart the indexing proccess after database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-$original_sunspot_session = Sunspot.session
-::Sunspot.session = ::Sunspot::Rails::StubSessionProxy.new($original_sunspot_session)
+original_sunspot_session = Sunspot.session
+Sunspot.session = ::Sunspot::Rails::StubSessionProxy.new($original_sunspot_session)
 
 %w(agreements
    allergies
@@ -43,5 +43,9 @@ if ENV['seed_nux'] || ENV['seed_fast'] || ENV['seed_all']
   load File.join(Rails.root, 'db', 'seeds', 'nux.rb')
 end
 
-::Sunspot.session = $original_sunspot_session
-
+Sunspot.session = original_sunspot_session
+Content.reindex
+Allergy.reindex
+Condition.reindex
+Treatment.reindex
+Sunspot.commit
