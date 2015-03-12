7abb1b7561108838dbf2142a127a14c82de2dc05
Emilio Zand <emilio@getbetter.com>
2015-03-11 18:34:54 -0700

    Fixed the due dates of the templates and adjusted the name to be more unique

--- db/seeds/task_templates.rb
+++ db/seeds/task_templates.rb
@@ -1,7 +1,7 @@
 # Mayo Pilot 2 --
 # Week 1
 TaskTemplate.find_or_create_by_name(
-    name: "day 2",
+    name: "mayo pilot 2 - day 2",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHow are you feeling today? As a reminder, I am extension of your care team at Mayo Clinic and here to help out with your transition after the hospital",
@@ -10,7 +10,7 @@ TaskTemplate.find_or_create_by_name(
     service_ordinal: 0
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 3",
+    name: "mayo pilot 2 - day 3",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHi___, how are things going?",
@@ -19,7 +19,7 @@ TaskTemplate.find_or_create_by_name(
     service_ordinal: 1
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 4",
+    name: "mayo pilot 2 - day 4",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nJust checking in, ___. How are you today?",
@@ -28,7 +28,7 @@ TaskTemplate.find_or_create_by_name(
     service_ordinal: 2
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 5",
+    name: "mayo pilot 2 - day 5",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHi ___, Do you have any questions for me as you recuperate?",
@@ -38,67 +38,67 @@ TaskTemplate.find_or_create_by_name(
 )
 # Week 2
 TaskTemplate.find_or_create_by_name(
-    name: "day 8",
+    name: "mayo pilot 2 - day 8",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHi ___, wanted to see how you're doing today?",
-    time_estimate: 1440,
+    time_estimate: 2880,
     priority: 1,
     service_ordinal: 4
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 10",
+    name: "mayo pilot 2 - day 10",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nI hope you are doing well ___. Please let me know if I can help out with anything.",
-    time_estimate: 1440,
+    time_estimate: 2880,
     priority: 1,
     service_ordinal: 5
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 12",
+    name: "mayo pilot 2 - day 12",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHow are you feeling today, ____?",
-    time_estimate: 1440,
+    time_estimate: 2880,
     priority: 1,
     service_ordinal: 6
 )
 # Week 3
 TaskTemplate.find_or_create_by_name(
-    name: "day 15",
+    name: "mayo pilot 2 - day 15",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHi ____. I wanted to check in with you and see how you're doing today.",
-    time_estimate: 1440,
+    time_estimate: 4320,
     priority: 1,
     service_ordinal: 7
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 18",
+    name: "mayo pilot 2 - day 18",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nPlease let me know if I can do anything for you, ___. Here to help out!",
-    time_estimate: 1440,
+    time_estimate: 4320,
     priority: 1,
     service_ordinal: 8
 )
 # Week 4
 TaskTemplate.find_or_create_by_name(
-    name: "day 22",
+    name: "mayo pilot 2 - day 22",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nHi ____. I wanted to check in with you and see how you're doing today.",
-    time_estimate: 1440,
+    time_estimate: 4320,
     priority: 1,
     service_ordinal: 9
 )
 TaskTemplate.find_or_create_by_name(
-    name: "day 25",
+    name: "mayo pilot 2 - day 25",
     service_template: ServiceTemplate.find_by_name('mayo pilot 2'),
     title: "Check In with Stroke Patient",
     description: "*Check in with engaged patient/about services being performed or use messaging below:*\n**Messaging**\nPlease let me know if I can do anything for you, ___. Here to help out!",
-    time_estimate: 1440,
+    time_estimate: 4320,
     priority: 1,
     service_ordinal: 10
 )
