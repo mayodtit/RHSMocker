df61df808f2ae7b43aca63f0398a079de4f62a33
Wuang Qin <wuang@getbetter.com>
2015-04-08 10:16:12 -0700

    redo the uploader, follow the flow on carrierwave

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,12 +1,6 @@
 class UserFile < ActiveRecord::Base
   belongs_to :user
-
   attr_accessible :file, :user_id
-  validates :user, :file, presence: true
-
   mount_uploader :file, UserFileUploader
-
-  def url
-    file.url
-  end
+  validates :user, presence: true
 end
