dbc6ec9ceee05917e21cb305079975266da3ba6a
Emilio Zand <emilio@getbetter.com>
2015-04-09 11:09:58 -0700

    Actually Fix failing spec

--- spec/acceptance/api/v1/services_spec.rb
+++ spec/acceptance/api/v1/services_spec.rb
@@ -93,8 +93,8 @@ resource "Services" do
 
     required_parameters :auth_token
 
-    let!(:open_service) { create :service, member: pha, subject: member, user_facing: true}
-    let!(:completed_service) { create :service, :completed, member: pha, subject: member, user_facing: true}
+    let!(:open_service) { create :service, member: pha, subject: pha, user_facing: true}
+    let!(:completed_service) { create :service, :completed, member: pha, subject: pha, user_facing: true}
     let!(:abandoned_service) { create :service, :abandoned, member: pha, subject: relative, user_facing: true}
     let!(:suggestion) { create :suggested_service, user: pha}
 
