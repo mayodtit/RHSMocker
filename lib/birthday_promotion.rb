350c606afb1e1be97f33883927df5fb1a5be5431
mishuan <yuanshihao1994@gmail.com>
2015-04-15 18:56:46 -0700

    Free weeks

---
+++ lib/birthday_promotion.rb
@@ -0,0 +1,18 @@
+class BirthdayPromotion
+  def initialize(member)
+    @member = member
+  end
+
+  def promote(user)
+    case member.status
+    when :free
+      
+    when :trial 
+
+    when :premium
+
+    when :chamath
+
+    end
+  end
+end
