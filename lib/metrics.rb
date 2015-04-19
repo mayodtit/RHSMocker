acbffd733419e51e14bf2552a70ab92683257296
Kyle Chilcutt <kyle@getbetter.com>
2015-04-18 19:15:36 -0700

    Create Metrics class to backfill data
    Backfill events based on user messages and responses. Add specs to check
    validity with different scenarios.

---
+++ lib/metrics.rb
@@ -0,0 +1,85 @@
+class Metrics
+  include Singleton
+
+  MESSAGE_RESPONSE_EVENT = 'Message Response'
+
+  # For each incoming message (where the message user is the consult initiator)
+  # identify the response and calculate the time between the message and the
+  # response.
+  #
+  # Track most recently seen message per consult to prevent duplicate events.
+  # Track messages that do not have responses separately so they do not affect
+  # response time metrics until they receive a response.
+  #
+  def backfill_message_response_events(start_at)
+    messages(start_at).find_each do |message|
+      next unless message.user_id == message.consult.initiator_id
+      next if last_seen_message_ids[message.consult_id] > message.id
+      if response = identify_response(message)
+        track_event(message, response)
+      else
+        messages_without_response << message
+      end
+      last_seen_message_ids[message.consult_id] = (response || message).id
+    end
+    messages_without_response
+  end
+
+  def reload
+    instance_variables.each{|var| remove_instance_variable(var) }
+    self
+  end
+  alias_method :reload!, :reload
+
+  private
+
+  def messages(start_at)
+    Message.where('created_at > ?', start_at).includes(:consult)
+  end
+
+  def identify_response(message)
+    message.consult.messages
+                   .where('id > ?', message.id)
+                   .where('user_id != ?', message.user_id)
+                   .first
+  end
+
+  def track_event(message, response)
+    properties = {
+      time: message.created_at.to_i,
+      user_id: message.user_id,
+      responder_id: response.user_id,
+      message_id: message.id,
+      response_id: response.id,
+      duration: response.created_at.to_i - message.created_at.to_i,
+      import_time: import_time.to_i
+    }
+
+    tracker.import(api_key,
+                   message.user_id,
+                   MESSAGE_RESPONSE_EVENT,
+                   properties)
+  end
+
+  # track, per consult id, the latest seen message; this prevents duplicate
+  # events when a user sends us multiple messages in a row
+  def last_seen_message_ids
+    @last_seen_message_ids ||= Hash.new(0)
+  end
+
+  def messages_without_response
+    @messages_without_response ||= []
+  end
+
+  def tracker
+    @tracker ||= Mixpanel::Tracker.new(ENV['MIXPANEL_TOKEN'])
+  end
+
+  def api_key
+    @api_key ||= ENV['MIXPANEL_API_KEY']
+  end
+
+  def import_time
+    @import_time ||= Time.now
+  end
+end
---
+++ spec/lib/metrics_spec.rb
@@ -0,0 +1,77 @@
+describe Metrics do
+  before do
+    Timecop.freeze(Date.today.to_time)
+    Metrics.instance.reload!
+  end
+
+  after do
+    Timecop.return
+  end
+
+  describe '#backfill_message_response_events' do
+    def call_method
+      described_class.instance.backfill_message_response_events(Time.now - 2.days)
+    end
+
+    let(:tracker) { double(:mixpanel_tracker) }
+    let(:pha) { create(:pha) }
+    let(:user) { create(:member, :premium, pha: pha) }
+    let(:consult) { user.master_consult }
+
+    before do
+      Mixpanel::Tracker.stub(new: tracker)
+    end
+
+    context 'one message, one response' do
+      let!(:user_message) { create(:message, user: user, consult: consult, created_at: Time.now - 1.minute) }
+      let!(:response_message) { create(:message, user: pha, consult: consult, created_at: Time.now) }
+      let(:expected_properties) do
+        {
+          time: user_message.created_at.to_i,
+          user_id: user.id,
+          responder_id: pha.id,
+          message_id: user_message.id,
+          response_id: response_message.id,
+          duration: 60,
+          import_time: Time.now.to_i
+        }
+      end
+
+      it 'tracks the event' do
+        tracker.should_receive(:import).with(ENV['MIXPANEL_API_KEY'], user.id, 'Message Response', expected_properties).once
+        call_method
+      end
+    end
+
+    context 'two messages, one response' do
+      let!(:first_user_message) { create(:message, user: user, consult: consult, created_at: Time.now - 1.minute) }
+      let!(:second_user_message) { create(:message, user: user, consult: consult, created_at: Time.now - 30.seconds) }
+      let!(:response_message) { create(:message, user: pha, consult: consult, created_at: Time.now) }
+      let(:expected_properties) do
+        {
+          time: first_user_message.created_at.to_i,
+          user_id: user.id,
+          responder_id: pha.id,
+          message_id: first_user_message.id,
+          response_id: response_message.id,
+          duration: 60,
+          import_time: Time.now.to_i
+        }
+      end
+
+      it 'tracks the first event' do
+        tracker.should_receive(:import).with(ENV['MIXPANEL_API_KEY'], user.id, 'Message Response', expected_properties).once
+        call_method
+      end
+    end
+
+    context 'one unanswered message' do
+      let!(:user_message) { create(:message, user: user, consult: consult, created_at: Time.now - 1.minute) }
+
+      it 'returns messages requiring a response to the console' do
+        tracker.should_not_receive(:import)
+        expect(call_method).to eq([user_message])
+      end
+    end
+  end
+end
