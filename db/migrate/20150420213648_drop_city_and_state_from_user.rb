21e5a978145450e6f79296ff3bea3b9545e7430b
Peter Christensen <peter@pchristensen.com>
2015-04-20 16:17:17 -0700

    Add SQL to check for User.state column, drop if present [#91862844]

--- db/migrate/20150420213648_drop_city_and_state_from_user.rb
+++ db/migrate/20150420213648_drop_city_and_state_from_user.rb
@@ -1,9 +1,26 @@
 class DropCityAndStateFromUser < ActiveRecord::Migration
   def up
     remove_column :users, :city
-    state_column_exists = false ## TODO SQL figure it out!
+
+    ## Determine if the User table has the state column - see https://www.pivotaltracker.com/story/show/91862844
+    ## There was a problem dropping users.state in the past, so the schema didn't show it but the production db
+    ##   still had the column. This code below lets the migration run on prod without breaking dev/qa dbs that
+    ##   don't have that weird state
+    q = <<-SQL
+      SELECT count(column_name)
+        FROM information_schema.columns
+       WHERE table_name = "users"
+         AND column_name = "state"
+    SQL
+    ActiveRecord::Base.establish_connection
+    state_column_exists = (ActiveRecord::Base.connection.execute(q).entries[0][0] != 0) ## I know this is gross
     if state_column_exists
-      ## TODO SQL ALTER TABLE to drop it
+      puts "*** STATE COLUMN EXISTS - DROPPING VIA SQL ***"
+      q = <<-SQL
+            ALTER TABLE users
+            DROP COLUMN state
+          SQL
+      ActiveRecord::Base.connection.execute(q)
     end
   end
 
