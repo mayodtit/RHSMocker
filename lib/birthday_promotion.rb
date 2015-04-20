2bba9fd715b3596290690dda698147515dc63caa
mishuan <yuanshihao1994@gmail.com>
2015-04-20 12:11:22 -0700

    Birthday Promotion Refactoring -refactored code according to PR comments

--- lib/birthday_promotion.rb
+++ lib/birthday_promotion.rb
@@ -6,19 +6,13 @@ class BirthdayPromotion
   def promote
     case member.status
     when 'free'
-      promo_id = Promotion.find_by_name('Birthday Promotion').id
-      unless has_promotion?(promo_id)
+      promotion = Promotion.find_by_name('Birthday Promotion')
+      unless has_promotion?(promotion)
         member.free_trial_ends_at.blank? ? time = Time.now + 2.week : time = member.free_trial_ends_at + 2.week
         member.update_attributes(status_event: :grant_free_trial, free_trial_ends_at: time)
-        member.user_promotions.create(promotion_id: promo_id)
+        member.user_promotions.create(promotion_id: promotion.id)
         member.save
       end
-    when 'trial' 
-      #Promotion for people on trial
-    when 'premium'
-      #Promotion for people on premium
-    else
-
     end
   end
   
@@ -28,10 +22,7 @@ class BirthdayPromotion
     @member
   end
 
-  def has_promotion?(promotion_id)
-    promos = member.user_promotions
-    return false unless promos
-    promos.each{ |promo| return true if promo.promotion_id.eql? promotion_id }
-    return false
+  def has_promotion?(promotion)
+    member.promotions.include?(promotion)
   end
 end
