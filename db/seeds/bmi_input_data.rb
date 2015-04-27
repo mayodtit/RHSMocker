6763e9ce2df2bc3882e535e023197c646b0cc6f7
Neel Anand <neel@getbetter.com>
2015-04-27 13:43:16 -0700

    Add two hashes for upsert_attributes.

--- db/seeds/bmi_input_data.rb
+++ db/seeds/bmi_input_data.rb
@@ -2,10 +2,10 @@ filename = 'bmiagerev.csv'
 encoding = 'ISO-8859-1'
 puts "Seeding BMI input data..."
 CSV.foreach(Rails.root.join('lib','assets',filename), encoding: encoding, headers: true) do |row|
-  BmiDataLevel.upsert_attributes!(gender: row['Sex'],
-                                  age: row['Agemos'],
-                                  l: row['L'],
+  BmiDataLevel.upsert_attributes!({gender: row['Sex'],
+                                  age: row['Agemos']},
+                                  {l: row['L'],
                                   m: row['M'],
-                                  s: row['S'])
+                                  s: row['S']})
 end
 puts "Seeding BMI input data completed!"
