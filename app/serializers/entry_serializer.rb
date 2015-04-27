06197e3977065679a5b64a4c8bddf9df036353b3
Emilio Zand <emilio@getbetter.com>
2015-04-27 10:49:37 -0700

    Revert "Revert "Add image_url to EntrySerializer""
    This reverts commit 7d0b660ad7bc09036a29f5d2dd7d63705b835ceb.

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,7 +1,8 @@
 class EntrySerializer < ViewSerializer
   self.root = false
 
-  attributes :id, :created_at, :updated_at, :member_id, :resource_id, :resource_type, :phone_call, :actor_id, :data
+  attributes :id, :created_at, :updated_at, :member_id, :resource_id,
+             :resource_type, :phone_call, :actor_id, :data, :image_url
 
   def attributes
     super.tap do |attributes|
@@ -14,5 +15,9 @@ class EntrySerializer < ViewSerializer
 
   def phone_call
     object.resource.try(:serializer, options.merge(shallow: true)) if object.resource_type == "PhoneCall"
+  end
+
+  def image_url
+    object.resource.try(:image_url) if object.resource.try(:respond_to?, :image_url)
   end
 end
