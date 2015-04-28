ad894ba78a5fe89e6393f5b6781cf5ab03c7679b
Neel Anand <neel@getbetter.com>
2015-04-27 19:23:47 -0700

    Add validations in the bmi_data_level model.

--- app/models/bmi_data_level.rb
+++ app/models/bmi_data_level.rb
@@ -1,3 +1,4 @@
 class BmiDataLevel < ActiveRecord::Base
-  attr_accessible :age, :gender, :l, :m, :s
+  attr_accessible :age_in_months, :gender, :power_in_transformation, :median, :coefficient_of_variation
+  validates :age_in_months, :gender, :power_in_transformation, :median, :coefficient_of_variation, presence: true
 end
