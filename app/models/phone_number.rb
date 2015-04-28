400bc5478acaa017140e7510c60d811d077df289
Peter Christensen <peter@pchristensen.com>
2015-04-27 20:25:04 -0700

    PhoneNumber validations and formatters [#93118526]

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -5,5 +5,26 @@ class PhoneNumber < ActiveRecord::Base
   belongs_to :address, inverse_of: :phone_numbers
 
   validates :type, inclusion: { in: %w(NPI Fax Home OFfice Alternate), message: "%{value} is not a valid phone number type" }
+  validate :must_belong_to_user_or_address
 
+  def must_belong_to_user_or_address
+    unless user || address
+      unless user
+        errors.add(:user, "or address must be assigned")
+      end
+      unless address
+        errors.add(:address, "or user must be assigned")
+      end
+    end
+  end
+
+  def display
+    phone.format("(%a) %f-%l")
+  end
+
+  private
+
+  def phone
+    @phone_number ||= Phoner::Phone.parse(number)
+  end
 end
