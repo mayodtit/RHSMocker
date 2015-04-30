751e27d4def25db138d75bbeffadf3699413ad8b
Neel Anand <neel@getbetter.com>
2015-04-29 18:15:46 -0700

    Remove warning_color from the db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150428214402) do
+ActiveRecord::Schema.define(:version => 20150428020845) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -1513,7 +1513,6 @@ ActiveRecord::Schema.define(:version => 20150428214402) do
     t.integer  "creator_id"
     t.string   "healthkit_source"
     t.string   "bmi_level"
-    t.string   "warning_color"
   end
 
   add_index "weights", ["creator_id"], :name => "index_weights_on_creator_id"
