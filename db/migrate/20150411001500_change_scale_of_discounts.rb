36e2ed47088e52a7ae3c9674757bf531d44eedd6
Wuang Qin <wuang@getbetter.com>
2015-05-06 14:55:22 -0700

    add down method to make the migration reversible

--- db/migrate/20150411001500_change_scale_of_discounts.rb
+++ db/migrate/20150411001500_change_scale_of_discounts.rb
@@ -2,4 +2,8 @@ class ChangeScaleOfDiscounts < ActiveRecord::Migration
   def up
     change_column :discounts, :discount_percent, :decimal, :precision => 2, :scale => 1
   end
+
+  def down
+    change_column :discounts, :discount_percent, :decimal
+  end
 end
