bbeffd58dbbef04fbb060792c3036d8df8d051e1
Emilio Zand <emilio@getbetter.com>
2015-05-19 16:52:05 -0700

    Add the ability to return only the user facing services for a specific member

--- app/controllers/api/V1/services_controller.rb
+++ app/controllers/api/V1/services_controller.rb
@@ -9,7 +9,11 @@ class Api::V1::ServicesController < Api::V1::ABaseController
 
   def index
     authorize! :read, Service
-    services = @member.services.where(params.permit(:subject_id, :state)).order("field(state, 'open', 'waiting', 'completed', 'abandoned'), due_at DESC, created_at DESC")
+    if params[:user_facing] = true
+      services = @member.services.where(user_facing: true).order('updated_at DESC, created_at DESC')
+    else
+      services = @member.services.where(params.permit(:subject_id, :state)).order("field(state, 'open', 'waiting', 'completed', 'abandoned'), due_at DESC, created_at DESC")
+    end
     index_resource services.serializer, name: :services
   end
 
@@ -112,5 +116,3 @@ class Api::V1::ServicesController < Api::V1::ABaseController
       create_params
   end
 end
-
-
