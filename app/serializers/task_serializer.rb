c946dc8a085250e498c8289a68485197a621c770
Neel Anand <neel@getbetter.com>
2015-05-20 10:42:42 -0700

    Serialize modal_template with task.

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -18,7 +18,8 @@ class TaskSerializer < ActiveModel::Serializer
         urgent: object.urgent,
         unread: object.unread,
         follow_up: object.follow_up,
-        day_priority: object.day_priority
+        day_priority: object.day_priority,
+        modal_template: modal_template
       }
       attributes[:member] = object.member.try(:serializer, options) if object.respond_to? :member
       attributes
@@ -50,7 +51,7 @@ class TaskSerializer < ActiveModel::Serializer
         urgent: object.urgent,
         unread: object.unread,
         follow_up: object.follow_up,
-        modal_template: object.modal_template,
+        modal_template: modal_template,
         member: object.member.try(:serializer, options.merge(shallow: true))
       }
       attributes
@@ -73,5 +74,11 @@ class TaskSerializer < ActiveModel::Serializer
 
   def triage_state
     '-'
+  end
+
+  def modal_template
+    if object.task_template.modal_template
+      object.task_template.modal_template
+    end
   end
 end
