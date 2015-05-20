1617ed3a10a516362a7f08c057b0e4ec25da6d5c
Kyle Chilcutt <kyle@getbetter.com>
2015-05-20 16:56:27 -0700

    Seed a SuggestedServiceTemplate
    Create a seeds file for SuggestedServiceTemplates and seed the first
    one.

--- app/models/suggested_service_template.rb
+++ app/models/suggested_service_template.rb
@@ -4,7 +4,8 @@ class SuggestedServiceTemplate < ActiveRecord::Base
   has_many :suggested_services
   belongs_to :service_template
 
-  attr_accessible :title, :description, :message
+  attr_accessible :title, :description, :message, :service_template,
+                  :service_template_id
 
   validates :title, :description, :message, presence: true
   validates :service_template, presence: true, if: ->(s){s.service_template_id}
