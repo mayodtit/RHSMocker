2250d91e3177e309944cbd51f69090f8e94567d2
Peter Christensen <peter@pchristensen.com>
2015-05-20 21:07:13 -0700

    PhoneNumberSerializer, including in has_many relations [#93578312]
     - Add has_many :phone_numbers to UserSerializer
     - Add has_many :phone_numbers to MemberSerializer
     - Add has_many :phone_numbers to AddressSerializer
     - preserve phone/work/text phone numbers in serializers
     - Cleanup some code in PhoneNumber

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -3,4 +3,6 @@ class AddressSerializer < ActiveModel::Serializer
 
   attributes :id, :user_id, :address, :address2, :line1, :line2, :city, :state,
              :postal_code, :name, :type
+
+  has_many :phone_numbers
 end
