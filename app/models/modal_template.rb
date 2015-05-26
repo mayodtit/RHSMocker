d4f58a20f3cee64344dee8f26450912fb652fa0d
Neel Anand <neel@getbetter.com>
2015-05-26 16:59:19 -0700

    Validate all params of modal_templates.

--- app/models/modal_template.rb
+++ app/models/modal_template.rb
@@ -2,5 +2,5 @@ class ModalTemplate < ActiveRecord::Base
   has_many :task_templates
 
   attr_accessible :title, :description, :accept, :reject
-  validates :title, presence: true
+  validates :title, :description, :accept, :reject, presence: true
 end
