8f3f4e9d43eb0b36bcc8309c4d6ff4e0edb5598b
Neel Anand <neel@getbetter.com>
2015-05-26 14:04:23 -0700

    Update the task descriptions and button texts.

--- db/seeds/modal_templates.rb
+++ db/seeds/modal_templates.rb
@@ -1,4 +1,4 @@
 ModalTemplate.upsert_attributes!({title: "Did you add doctor?"},
-                                 {description: "Did you remember to add the doctor and doctor photo to the member's profile? ",
-                                  accept: "Yes!",
-                                  reject: "No!"})
+                                 {description: "Did you remember to add doctor and doctor's photo to the member's profile? ",
+                                  accept: "Yes! Complete Task",
+                                  reject: "No! Go Back"})
