f4912676ac14b8428f67c36a4b94f89d4560531b
Neel Anand <neel@getbetter.com>
2015-05-26 16:23:50 -0700

    Add modal_template specs.

--- spec/models/modal_template_spec.rb
+++ spec/models/modal_template_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe ModalTemplate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_has_a 'valid factory'
+
+  describe 'validations' do
+    it_validates 'presence of', :title
+  end
+
+  describe "create_modal_template" do
+    let(:modal_template) { build_stubbed :modal_template }
+  end
 end
