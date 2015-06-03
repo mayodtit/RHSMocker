3689bac772e1e0d4ccb765a547b777075de999ce
Neel Anand <neel@getbetter.com>
2015-06-03 11:49:47 -0700

    Add method to avoid any updation of TaskTemplate if it's ServiceTemplate is published or retired. Also add dependent destroy to ServiceTemplate for TaskTemplate. [#95743268]

--- app/controllers/api/V1/task_templates_controller.rb
+++ app/controllers/api/V1/task_templates_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::TaskTemplatesController < Api::V1::ABaseController
   before_filter :load_user!
   before_filter :load_task_template!
+  before_filter :prevent_update_task_template, only: :update
 
   def show
     show_resource @task_template.serializer
+  end
+
+  def update
+    if @task_template.update_attributes(task_template_attributes)
+      render_success(task_template: @task_template.serializer)
+    else
+      render_failure({reason: @task_template.errors.full_messages.to_sentence}, 422)
+    end
   end
 
   private
@@ -13,4 +22,13 @@ class Api::V1::TaskTemplatesController < Api::V1::ABaseController
     authorize! :manage, @task_template
   end
 
+  def prevent_update_task_template
+    if @task_template.service_template.published? || @task_template.service_template.retired?
+      render_failure({reason: 'TaskTemplate\'s associated ServiceTemplate is published or retired. You cannot update it!'}, 422)
+    end
+  end
+
+  def task_template_attributes
+    params.require(:task_template).permit(:name, :title, :service_template, :description, :time_estimate, :service_ordinal, :modal_template_id)
+  end
 end
