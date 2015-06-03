8766b7a0ed5b8613e71aac046fff0a6b2a9ef097
Neel Anand <neel@getbetter.com>
2015-06-03 10:09:57 -0700

    Serialize version, unique_id and state for ServiceTemplate.

--- app/serializers/service_template_serializer.rb
+++ app/serializers/service_template_serializer.rb
@@ -1,7 +1,7 @@
 class ServiceTemplateSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes  :id, :name, :title, :description, :service_type_id, :time_estimate, :due_at, :user_facing, :service_request
+  attributes  :id, :name, :title, :description, :service_type_id, :time_estimate, :due_at, :user_facing, :service_request, :version, :unique_id, :state
 
   def attributes
     if options[:shallow]
