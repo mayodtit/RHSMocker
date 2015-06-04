cc9a3fba6d91eb67a2b5a24bbfeb96c5cf34c9f6
Neel Anand <neel@getbetter.com>
2015-06-04 15:54:09 -0700

    Clean up and refactor TaskTemplate controller.

--- app/controllers/api/V1/task_templates_controller.rb
+++ app/controllers/api/V1/task_templates_controller.rb
@@ -8,11 +8,7 @@ class Api::V1::TaskTemplatesController < Api::V1::ABaseController
   end
 
   def update
-    if @task_template.update_attributes(task_template_attributes)
-      render_success(task_template: @task_template.serializer)
-    else
-      render_failure({reason: @task_template.errors.full_messages.to_sentence}, 422)
-    end
+    update_resource(@task_template, permitted_params.task_template)
   end
 
   private
@@ -23,12 +19,8 @@ class Api::V1::TaskTemplatesController < Api::V1::ABaseController
   end
 
   def prevent_update_task_template
-    if @task_template.service_template.published? || @task_template.service_template.retired?
+    if @task_template.service_template.try(:published?) || @task_template.service_template.try(:retired?)
       render_failure({reason: 'TaskTemplate\'s associated ServiceTemplate is published or retired. You cannot update it!'}, 422)
     end
-  end
-
-  def task_template_attributes
-    params.require(:task_template).permit(:name, :title, :service_template, :description, :time_estimate, :service_ordinal, :modal_template_id)
   end
 end
