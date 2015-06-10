27542e8fc51b93763f6e7a55e97df0779ac185d0
Emilio Zand <emilio@getbetter.com>
2015-06-10 11:07:34 -0700

    Add rspec for FeatureFlag #track_update method

--- app/models/feature_flag.rb
+++ app/models/feature_flag.rb
@@ -7,8 +7,16 @@ class FeatureFlag < Metadata
 
   after_save :track_update, on: :update
 
+  def feature_enabled?
+    if mvalue == 'true'
+      true
+    else
+      false
+    end
+  end
+
   def track_update
-    changes = self.changes.except(:created_at, :updated_at, :avatar)
+    changes = self.changes.except(:created_at, :updated_at)
     return if changes.empty?
     @actor_id ||= Member.robot.id
     FeatureFlagChange.create! feature_flag: self, actor_id: actor_id, action: 'update', data: changes
