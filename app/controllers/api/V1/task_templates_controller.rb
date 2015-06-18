bd7aba0ee3479b9819791a5d9ba3f66e16c1c9e1
Neel Anand <neel@getbetter.com>
2015-06-18 13:12:41 -0700

    Refactor 'authorize' for TaskTemplates.

--- app/controllers/api/V1/task_templates_controller.rb
+++ app/controllers/api/V1/task_templates_controller.rb
@@ -5,32 +5,22 @@ class Api::V1::TaskTemplatesController < Api::V1::ABaseController
   before_filter :load_user!
 
   def index
-    authorize! :read, TaskTemplate
-
     index_resource @task_templates.serializer
   end
 
   def show
-    authorize! :read, @task_template
-
     show_resource @task_template.serializer
   end
 
   def update
-    authorize! :update, @task_template
-
     update_resource(@task_template, permitted_params.task_template)
   end
 
   def create
-    authorize! :create, TaskTemplate
-
     create_resource(@task_templates, permitted_params.task_template)
   end
 
   def destroy
-    authorize! :destroy, @task_template
-
     destroy_resource @task_template
   end
 
