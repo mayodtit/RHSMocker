2c01a4d3bd36aff500d04bedffb4bac090d6b7a4
Kyle Chilcutt <kyle@getbetter.com>
2015-06-22 15:55:42 -0700

    Remove options from TaskSerializer
    Show all the things all the time. Simplify first, optimize later.

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -2,79 +2,24 @@ class TaskSerializer < ActiveModel::Serializer
   self.root = false
 
   attributes :id, :title, :state, :description, :due_at, :type, :created_at,
-             :owner_id, :service_type_id, :triage_state, :member_id, :day_priority, :task_template_id, :urgent, :unread, :follow_up, :modal_template
+             :owner_id, :service_type_id, :triage_state, :member_id,
+             :day_priority, :task_template_id, :urgent, :unread, :follow_up,
+             :modal_template
 
-  delegate :task_steps, :input_data_fields, :output_data_fields, to: :object
+  delegate :member, :owner, :service_type, :task_changes, :task_steps,
+           :task_template, :input_data_fields, :output_data_fields, to: :object
 
   def attributes
-    if options[:workflow]
-      super.tap do |attrs|
-        attrs[:task_steps] = task_steps.serializer.as_json
-        attrs[:input_data_fields] = input_data_fields.serializer.as_json
-        attrs[:output_data_fields] = output_data_fields.serializer.as_json
-      end
-    elsif options[:shallow]
-      attributes = {
-        id: object.id,
-        title: object.title,
-        state: object.state,
-        due_at: object.due_at,
-        created_at: object.created_at,
-        type: type,
-        triage_state: triage_state,
-        member_id: member_id,
-        urgent: object.urgent,
-        unread: object.unread,
-        follow_up: object.follow_up,
-        day_priority: object.day_priority,
-        modal_template: modal_template
-      }
-      attributes[:member] = object.member.try(:serializer, options) if object.respond_to? :member
-      attributes
-    elsif options[:for_subject]
-      attributes = {
-        id: object.id,
-        title: object.title,
-        state: object.state,
-        due_at: object.due_at,
-        created_at: object.created_at,
-        member_id: member_id,
-        type: type,
-        service_type: object.service_type,
-        urgent: object.urgent,
-        owner: object.owner.try(:serializer, options.merge(shallow: true))
-      }
-      attributes
-    elsif options[:for_task]
-      attributes = {
-        id: object.id,
-        title: object.title,
-        state: object.state,
-        due_at: object.due_at,
-        created_at: object.created_at,
-        type: type,
-        service_type: object.service_type,
-        description: object.description,
-        day_priority: object.day_priority,
-        urgent: object.urgent,
-        unread: object.unread,
-        follow_up: object.follow_up,
-        modal_template: modal_template,
-        member: object.member.try(:serializer, options.merge(shallow: true))
-      }
-      attributes
-    else
-      super.tap do |attributes|
-        attributes.merge!(
-          owner: object.owner.try(:serializer, options),
-          service_type: object.service_type
-        )
-        attributes[:task_changes] = object.task_changes.try(:serializer, options.merge(shallow: true)) if object.respond_to? :task_changes
-        attributes[:member] = object.member.try(:serializer, options.merge(shallow: true)) if object.respond_to? :member
-      end
+    super.tap do |attrs|
+      attrs[:member] = member.try(:serializer, shallow: true)
+      attrs[:owner] = owner.try(:serializer, shallow: true)
+      attrs[:service_type] = service_type
+      attrs[:task_steps] = task_steps.serializer.as_json
+      attrs[:input_data_fields] = input_data_fields.serializer.as_json
+      attrs[:output_data_fields] = output_data_fields.serializer.as_json
+      attrs[:task_changes] = task_changes.try(:serializer, shallow: true)
     end
   end
-
 
   def type
     'task'
@@ -85,6 +30,6 @@ class TaskSerializer < ActiveModel::Serializer
   end
 
   def modal_template
-    object.task_template.try(:modal_template)
+    task_template.try(:modal_template)
   end
 end
