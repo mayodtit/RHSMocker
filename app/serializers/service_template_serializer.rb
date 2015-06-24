04e0fc08f89f30ffde0491bbec105023f056d9e4
Neel Anand <neel@getbetter.com>
2015-06-24 11:54:16 -0700

    Refactor unpublished version in serializer.

--- app/serializers/service_template_serializer.rb
+++ app/serializers/service_template_serializer.rb
@@ -1,7 +1,9 @@
 class ServiceTemplateSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes  :id, :name, :title, :description, :service_type_id, :time_estimate, :due_at, :user_facing, :service_request, :service_update, :version, :unique_id, :state, :state_events, :created_at, :check_unpublished_version
+  attributes  :id, :name, :title, :description, :service_type_id, :time_estimate, :due_at, :user_facing, :service_request, :service_update, :version, :unique_id, :state, :state_events, :created_at, :unpublished_version_id
+
+  delegate :unique_id, :published?, to: :object
 
   def attributes
     if options[:shallow]
@@ -27,15 +29,12 @@ class ServiceTemplateSerializer < ActiveModel::Serializer
     Time.now.business_minutes_from(object.time_estimate)
   end
 
-  def check_unpublished_version
-    @check_unpublished_version = if object.unique_id && object.published?
-                                   if @view_unpublished_version = object.class.unpublished.where(unique_id: object.try(:unique_id)).first
-                                     {unpublished_version: true, unpublished_version_id: @view_unpublished_version.id}
-                                   else
-                                     false
-                                   end
-                                 else
-                                   false
-                                 end
+  def unpublished_version_id
+    return nil unless unique_id && published?
+    if unpublished_template = ServiceTemplate.unpublished.find_by_unique_id(unique_id)
+      unpublished_template.id
+    else
+      nil
+    end
   end
 end
