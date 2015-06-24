5603ebabbfc226396a4a24769ada6bc86caa275b
Neel Anand <neel@getbetter.com>
2015-06-24 11:27:22 -0700

    Serialize unpublished template

--- app/serializers/service_template_serializer.rb
+++ app/serializers/service_template_serializer.rb
@@ -1,7 +1,7 @@
 class ServiceTemplateSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes  :id, :name, :title, :description, :service_type_id, :time_estimate, :due_at, :user_facing, :service_request, :service_update, :version, :unique_id, :state, :state_events, :created_at
+  attributes  :id, :name, :title, :description, :service_type_id, :time_estimate, :due_at, :user_facing, :service_request, :service_update, :version, :unique_id, :state, :state_events, :created_at, :check_unpublished_version
 
   def attributes
     if options[:shallow]
@@ -25,5 +25,17 @@ class ServiceTemplateSerializer < ActiveModel::Serializer
 
   def due_at
     Time.now.business_minutes_from(object.time_estimate)
+  end
+
+  def check_unpublished_version
+    @check_unpublished_version = if object.unique_id && object.published?
+                                   if @view_unpublished_version = object.class.unpublished.where(unique_id: object.try(:unique_id)).first
+                                     {unpublished_version: true, unpublished_version_id: @view_unpublished_version.id}
+                                   else
+                                     false
+                                   end
+                                 else
+                                   false
+                                 end
   end
 end
