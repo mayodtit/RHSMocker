309069f87df7dde71327726febe26fd5159eb0a8
Kyle Chilcutt <kyle@getbetter.com>
2015-06-23 19:25:10 -0700

    Serialize data_fields as part of task_steps
    Make the data available to the front end.

--- app/serializers/task_step_serializer.rb
+++ app/serializers/task_step_serializer.rb
@@ -2,4 +2,12 @@ class TaskStepSerializer < ActiveModel::Serializer
   self.root = false
 
   attributes :id, :description, :ordinal, :completed
+
+  delegate :data_fields, to: :object
+
+  def attributes
+    super.tap do |attrs|
+      attrs[:data_fields] = data_fields.serializer.as_json
+    end
+  end
 end
