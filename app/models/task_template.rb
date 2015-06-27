9caeaa91c7abe1d44cb066cfdf8fc88cbf65993c
Neel Anand <neel@getbetter.com>
2015-06-26 18:15:21 -0700

    Reposition copy title to name method.

--- app/models/task_template.rb
+++ app/models/task_template.rb
@@ -32,15 +32,15 @@ class TaskTemplate < ActiveRecord::Base
     )
   end
 
-  def create_deep_copy!(override_service_template=nil)
-    new_modal_template = modal_template.try(:create_copy!)
-    self.class.create!(attributes.except('id', 'service_template_id', 'created_at', 'updated_at', 'modal_template_id').merge(service_template: override_service_template || service_template, modal_template: new_modal_template || modal_template))
-  end
-
   def copy_title_to_name
     if !self.name
       self.name = self.title
     end
+  end
+
+  def create_deep_copy!(override_service_template=nil)
+    new_modal_template = modal_template.try(:create_copy!)
+    self.class.create!(attributes.except('id', 'service_template_id', 'created_at', 'updated_at', 'modal_template_id').merge(service_template: override_service_template || service_template, modal_template: new_modal_template || modal_template))
   end
 
   def no_placeholders_in_user_facing_attributes
