d25efba248cdb109e81aebb9ae995f60a95c480f
Kyle Chilcutt <kyle@getbetter.com>
2015-07-05 19:17:00 -0700

    Fix typo in TaskStep factory
    Ensure we create instances of the right model. We don't need to specify
    the class, FactoryGirl will match based on convention.

--- spec/factories/task_step_changes.rb
+++ spec/factories/task_step_changes.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :task_step_change, :class => 'TaskStepChanges' do
+  factory :task_step_change do
     task_step
     association :actor, factory: :member
     data { {hello: :world} }
