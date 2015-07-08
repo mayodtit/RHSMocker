ef85a0a1e02408009c0c9ad0ebca11c789d14f06
Neel Anand <neel@getbetter.com>
2015-07-07 18:43:37 -0700

    Associate 'affirmative_child' and 'negative_child' of the TaskTemplateSet on update call. Goal is to associate TaskTemplateSets when they are connected by a connector on UI.

--- app/models/task_template_set.rb
+++ app/models/task_template_set.rb
@@ -7,4 +7,17 @@ class TaskTemplateSet < ActiveRecord::Base
   # negative_child_id - for the child if the result is negative
   attr_accessible :result, :service_template_id, :parent_id, :affirmative_child_id, :negative_child_id
 
+  def create_parent_association!(parent_id)
+    if self.parent_id.nil?
+      self.parent_id = parent_id
+    end
+  end
+
+  def create_child_association!(affirmative_child_id, negative_child_id)
+    if self.affirmative_child_id.nil?
+      self.affirmative_child_id = affirmative_child_id
+    elsif self.negative_child_id.nil?
+      self.negative_child_id = negative_child_id
+    end
+  end
 end
