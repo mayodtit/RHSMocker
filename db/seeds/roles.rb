39e0a71ab7cfff98869c1715e487320a990e620c
Emilio Zand <emilio@getbetter.com>
2015-07-09 13:17:42 -0700

    Add Engineer role to roles seed for future use

--- db/seeds/roles.rb
+++ db/seeds/roles.rb
@@ -1,3 +1,3 @@
-%w(nurse admin super_admin pha pha_lead pha_bot hcc specialist service_admin).each do |role|
+%w(nurse admin super_admin pha pha_lead pha_bot hcc specialist service_admin engineer).each do |role|
   Role.find_or_create_by_name!(name: role)
 end
