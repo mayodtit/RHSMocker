1283b0296a9c5a5df94d751abe3848107a10fe4a
Emilio Zand <emilio@getbetter.com>
2015-07-10 11:45:35 -0700

    Remove duplicate #publish specs in MemberTask rspecs

--- spec/models/member_task_spec.rb
+++ spec/models/member_task_spec.rb
@@ -8,65 +8,6 @@ describe MemberTask do
     it_validates 'foreign key of', :subject
     it_validates 'foreign key of', :service_type
   end
-  describe '#publish' do
-    let(:task) { build(:member_task) }
-
-    context 'new record' do
-      before do
-        task.stub(:id) { 2 }
-        task.stub(:id_changed?) { true }
-        task.stub(:member_id) { 1 }
-        task.stub(:subject_id) { 5 }
-      end
-
-      it 'publishes that a new task was created' do
-        PubSub.should_receive(:publish).with(
-          "/tasks/new",
-          {id: task.id},
-          nil
-        )
-        PubSub.should_receive(:publish).with(
-          "/members/1/subjects/5/tasks/new",
-          {id: task.id},
-          nil
-        )
-        task.publish
-      end
-
-      let!(:member_task) { create(:member_task) }
-      it 'has default priority of 3' do
-        member_task[:priority].should == 3
-      end
-    end
-
-    context 'old record' do
-      before do
-        task.stub(:id) { 2 }
-        task.stub(:id_changed?) { false }
-        task.stub(:member_id) { 1 }
-        task.stub(:subject_id) { 5 }
-      end
-
-      it 'publishes that a task was updated' do
-        PubSub.should_receive(:publish).with(
-          "/tasks/update",
-          {id: task.id},
-          nil
-        )
-        PubSub.should_receive(:publish).with(
-          "/tasks/2/update",
-          {id: task.id},
-          nil
-        )
-        PubSub.should_receive(:publish).with(
-          "/members/1/subjects/5/tasks/update",
-          {id: task.id},
-          nil
-        )
-        task.publish
-      end
-    end
-  end
 
   describe '#publish' do
     let(:task) { build(:member_task) }
