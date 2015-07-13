b796c37cff194bd57d3a1f09a4254b30b8dd6d99
Neel Anand <neel@getbetter.com>
2015-07-13 11:55:24 -0700

    Simplify TaskTemplateSet model which creates association.

--- app/models/task_template_set.rb
+++ app/models/task_template_set.rb
@@ -7,17 +7,8 @@ class TaskTemplateSet < ActiveRecord::Base
   # negative_child_id - for the child if the result is negative
   attr_accessible :result, :service_template_id, :parent_id, :affirmative_child_id, :negative_child_id, :task_templates
 
-  def create_parent_association!(parent_id)
-    if self.parent_id.nil?
-      self.parent_id = parent_id
-    end
-  end
-
-  def create_child_association!(affirmative_child_id, negative_child_id)
-    if self.affirmative_child_id.nil?
-      self.affirmative_child_id = affirmative_child_id
-    elsif self.negative_child_id.nil?
-      self.negative_child_id = negative_child_id
-    end
+  def create_association!(affirmative_child_id, negative_child_id)
+    self.affirmative_child_id = affirmative_child_id
+    self.negative_child_id = negative_child_id
   end
 end
