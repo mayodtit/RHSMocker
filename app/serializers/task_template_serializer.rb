59067763351037155d601ff602e3762bb6834fd0
Kyle Chilcutt <kyle@getbetter.com>
2015-07-19 17:00:21 -0700

    Expand TaskTemplateSerializer
    Include nested models and changes to support task steps and worksheets.

--- app/serializers/task_template_serializer.rb
+++ app/serializers/task_template_serializer.rb
@@ -1,6 +1,18 @@
 class TaskTemplateSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes  :id, :service_template_id,:name, :title, :description, :time_estimate, :service_ordinal
+  attributes :id, :service_template_id, :name, :title, :description,
+             :time_estimate, :service_ordinal
 
+  delegate :service_template, :modal_template, :task_step_templates,
+           :task_data_field_templates, to: :object
+
+  def attributes
+    super.tap do |attrs|
+      attrs[:service_template] = service_template
+      attrs[:modal_template] = modal_template
+      attrs[:task_step_templates] = task_step_templates
+      attrs[:task_data_field_templates] = task_data_field_templates
+    end
+  end
 end
