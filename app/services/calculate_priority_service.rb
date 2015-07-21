3ebc1289e46c7156741333e00843f039a939b671
Emilio Zand <emilio@getbetter.com>
2015-07-21 12:27:57 -0700

    Fix a typo in CalculatePriorityService

--- app/services/calculate_priority_service.rb
+++ app/services/calculate_priority_service.rb
@@ -2,6 +2,7 @@ class CalculatePriorityService
   PST_HOUR_OFFSET = -8
   def initialize(options)
     @task = options[:task]
+    @service = options[:service]
   end
 
   def call
@@ -18,12 +19,11 @@ class CalculatePriorityService
     @priority = time_zone_score
   end
 
-
   def time_zone_score
-    @time_zone_score ||=  if @task.time_zone_score
-                            @task.time_zone_offset/3600
-                          else
-                            PST_HOUR_OFFSET
-                          end
+    @time_zone_score ||= if @task.try(:time_zone_offset)
+                           @task.time_zone_offset/3600
+                         else
+                           PST_HOUR_OFFSET
+                         end
   end
 end
