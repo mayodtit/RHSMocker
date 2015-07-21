2559d4f54f69ec545193768d677c0d91ca8af1e2
Kyle Chilcutt <kyle@getbetter.com>
2015-07-21 11:22:32 -0700

    Add candidate emails to attr_accessible
    Can't mass assign now. #paytheplant

--- app/models/onboarding_group_candidate.rb
+++ app/models/onboarding_group_candidate.rb
@@ -1,7 +1,7 @@
 class OnboardingGroupCandidate < ActiveRecord::Base
   belongs_to :onboarding_group, inverse_of: :onboarding_group_candidates
 
-  attr_accessible :onboarding_group, :onboarding_group_id
+  attr_accessible :onboarding_group, :onboarding_group_id, :email
 
   validates :onboarding_group, :email, presence: true
   validates :email, uniqueness: true
