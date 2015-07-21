6208cbeec0e49563bc75797845ceccdaba9c3be2
Neel Anand <neel@getbetter.com>
2015-07-20 23:36:19 -0700

    Refactor deep_copy method. Link all the TaskTemplateSets within. Some crazy naive stuff. (WIP - Refactor to more clear code.)

--- app/models/task_template_set.rb
+++ app/models/task_template_set.rb
@@ -11,9 +11,20 @@ class TaskTemplateSet < ActiveRecord::Base
     self.negative_child_id = negative_child_id
   end
 
-  def create_deep_copy!(override_service_template=nil)
-    new_task_template_set = self.class.create!(attributes.except('id', 'service_template_id', 'created_at', 'updated_at', 'affirmative_child_id', 'negative_child_id').merge(service_template: override_service_template || service_template))
-    task_templates.each do |tt|
+  def create_deep_copy!(override_service_template=nil, task_template_set=nil)
+    new_task_template_set = task_template_set.class.create!(attributes.except('id', 'service_template_id', 'created_at', 'updated_at', 'affirmative_child_id', 'negative_child_id').merge(service_template: override_service_template || service_template))
+    create_task_templates(task_template_set, new_task_template_set)
+    if affirmative_child_id = task_template_set.affirmative_child_id
+      child_task_template_set = TaskTemplateSet.find(affirmative_child_id)
+      next_task_template_set = child_task_template_set.class.create!(attributes.except('id', 'service_template_id', 'created_at', 'updated_at', 'affirmative_child_id', 'negative_child_id').merge(service_template: override_service_template || service_template))
+      create_task_templates(child_task_template_set, next_task_template_set)
+      new_task_template_set.update_attribute(:affirmative_child_id, next_task_template_set.id) if affirmative_child_id
+      create_deep_copy!(nil, child_task_template_set)
+    end
+  end
+
+  def create_task_templates(task_template_set, new_task_template_set)
+    task_template_set.task_templates.each do |tt|
       tt.create_deep_copy!(new_task_template_set)
     end
   end
