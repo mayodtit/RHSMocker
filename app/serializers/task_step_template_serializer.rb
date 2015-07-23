45c63962b57f4da20066361a6e452643a9aca46a
Kyle Chilcutt <kyle@getbetter.com>
2015-07-22 23:58:49 -0700

    Remove serialization of unused attributes
    Lighten the template serializers by removing join information that is
    unused.

--- app/serializers/task_step_template_serializer.rb
+++ app/serializers/task_step_template_serializer.rb
@@ -4,11 +4,10 @@ class TaskStepTemplateSerializer < ActiveModel::Serializer
   attributes :id, :task_template_id, :description, :ordinal, :details,
              :template
 
-  delegate :task_step_data_field_templates, :data_field_templates, to: :object
+  delegate :data_field_templates, to: :object
 
   def attributes
     super.tap do |attrs|
-      attrs[:task_step_data_field_templates] = task_step_data_field_templates
       attrs[:data_field_templates] = data_field_templates
     end
   end
--- app/serializers/task_template_serializer.rb
+++ app/serializers/task_template_serializer.rb
@@ -4,15 +4,13 @@ class TaskTemplateSerializer < ActiveModel::Serializer
   attributes :id, :service_template_id, :name, :title, :description,
              :time_estimate, :service_ordinal
 
-  delegate :service_template, :modal_template, :task_step_templates,
-           :task_data_field_templates, to: :object
+  delegate :service_template, :modal_template, :task_step_templates, to: :object
 
   def attributes
     super.tap do |attrs|
       attrs[:service_template] = service_template
       attrs[:modal_template] = modal_template
       attrs[:task_step_templates] = task_step_templates.serializer(options)
-      attrs[:task_data_field_templates] = task_data_field_templates
     end
   end
 end
