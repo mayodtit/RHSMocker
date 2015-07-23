8e0e3733eb99c28037351417676c425e6d9a7863
Emilio Zand <emilio@getbetter.com>
2015-07-23 10:27:21 -0700

    Add queue to the permitted params for task templates Add queue to the task templates serializer

--- app/models/permitted_params.rb
+++ app/models/permitted_params.rb
@@ -126,7 +126,7 @@ class PermittedParams < Struct.new(:params, :current_user, :subject)
   end
 
   def task_template
-    params.require(:task_template).permit(:name, :title, :service_template, :service_template_id, :description, :time_estimate, :service_ordinal, :modal_template_id)
+    params.require(:task_template).permit(:name, :title, :service_template, :service_template_id, :description, :time_estimate, :service_ordinal, :modal_template_id, :queue)
   end
 
   def feature_flag
