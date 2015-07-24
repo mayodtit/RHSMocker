cc4470b9992944843c82837a4a589245174b1662
Neel Anand <neel@getbetter.com>
2015-07-23 17:07:29 -0700

    Update TaskTemplateSet specs.

--- spec/models/task_template_set_spec.rb
+++ spec/models/task_template_set_spec.rb
@@ -18,4 +18,28 @@ describe TaskTemplateSet do
       end
     end
   end
+
+  describe '#create_task_template_set!' do
+    context 'it creates a deep copy of task_template_set' do
+      let(:task_template_set) { build_stubbed(:task_template_set) }
+
+      it 'creates a deep copy of task_template_set' do
+        task_template_set.should_receive(:create_deep_copy!) { task_template_set }
+
+        task_template_set.create_deep_copy!.should == task_template_set
+      end
+    end
+  end
+
+  describe '#create_task_templates!' do
+    context 'it creates task_templates\' deep copy' do
+      let(:task_template) { build_stubbed(:task_template) }
+
+      it 'creates a deep copy of task_template_set' do
+        task_template.should_receive(:create_deep_copy!) { task_template }
+
+        task_template.create_deep_copy!.should == task_template
+      end
+    end
+  end
 end
