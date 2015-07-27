f7e26591f6664798397a9a142d4d3cf2b29f002a
Kyle Chilcutt <kyle@getbetter.com>
2015-07-27 09:36:17 -0700

    Serialize DataFields required flag
    Let the front end know which data fields are required to start a
    service.

--- app/serializers/data_field_serializer.rb
+++ app/serializers/data_field_serializer.rb
@@ -1,5 +1,5 @@
 class DataFieldSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :id, :name, :type, :data
+  attributes :id, :name, :type, :data, :required_for_service_start
 end
