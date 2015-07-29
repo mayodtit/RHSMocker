fba47964bfe720f5f6ea561831ac6b9c7a65d65a
Emilio Zand <emilio@getbetter.com>
2015-07-29 13:05:54 -0700

    Update TaskTemplate requests spec to use the queue variable Update the task/task template factories to use an unsymbolized queue variable

--- spec/factories/task_templates.rb
+++ spec/factories/task_templates.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     title "Title"
     description "Description"
     time_estimate 6 * 60
-    queue :pha
+    queue 'pha'
 
     trait :with_service_template do
       service_template
