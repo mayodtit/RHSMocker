9e2db3487590acb5565c70bf3336b701c05aee4e
Neel Anand <neel@getbetter.com>
2015-07-29 13:42:38 -0700

    Remove 'created_at' from attr_accessible.

--- app/models/appointment_change.rb
+++ app/models/appointment_change.rb
@@ -3,7 +3,7 @@ class AppointmentChange < ActiveRecord::Base
   belongs_to :actor, class_name: 'Member'
   serialize :data, Hash
 
-  attr_accessible :appointment, :appointment_id, :created_at, :event, :from, :to,
+  attr_accessible :appointment, :appointment_id, :event, :from, :to,
                   :actor, :actor_id, :data, :reason
 
   validates :appointment, :actor, presence: true
