c21acbfe18fb5a07e9a5c8693a4b4619c901b172
Neel Anand <neel@getbetter.com>
2015-07-29 17:49:37 -0700

    Refactor the way Appointments are created and updated.

--- app/controllers/api/V1/appointments_controller.rb
+++ app/controllers/api/V1/appointments_controller.rb
@@ -12,11 +12,27 @@ class Api::V1::AppointmentsController < Api::V1::ABaseController
   end
 
   def create
-    create_resource @appointments, appointment_attributes
+    attributes = appointment_attributes
+    attributes[:creator_id] = current_user.id
+
+    @appointment = Appointment.create(attributes)
+
+    if @appointment.errors.empty?
+      render_success(appointment: @appointment.serializer)
+    else
+      render_failure({reason: @appointment.errors.full_messages.to_sentence}, 422)
+    end
   end
 
   def update
-    update_resource @appointment, appointment_attributes
+    update_params = appointment_attributes
+    update_params[:actor_id] = current_user.id
+
+    if @appointment.update_attributes(update_params)
+      render_success(appointment: @appointment.serializer)
+    else
+      render_failure({reason: @appointment.errors.full_messages.to_sentence}, 422)
+    end
   end
 
   def destroy
@@ -34,6 +50,6 @@ class Api::V1::AppointmentsController < Api::V1::ABaseController
   end
 
   def appointment_attributes
-    params.require(:appointment).permit(:user_id, :provider_id, :scheduled_at, :actor_id, :creator_id)
+    params.require(:appointment).permit(:user_id, :provider_id, :scheduled_at)
   end
 end
