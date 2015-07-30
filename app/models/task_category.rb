cc13e3ed9363e4f799d6553a1787e5bed4b93654
Emilio Zand <emilio@getbetter.com>
2015-07-30 13:01:36 -0700

    Make changes as per PR comments Remove unused variables from task_category Add Task Categories to task template deep copies Update should to expect in task acceptance  specs

--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -2,7 +2,7 @@ class TaskCategory < ActiveRecord::Base
   has_many :tasks
   has_many :task_templates
 
-  attr_accessible :task_template_id, :task_id, :title, :description, :priority_weight
+  attr_accessible :title, :description, :priority_weight
 
-  validates :title, :priority_weight, presence: true
+  validates :title, :description, :priority_weight, presence: true
 end
