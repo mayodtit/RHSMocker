62194f448e1e345bccc11248d71ca68f928ddef3
Peter Christensen <peter@pchristensen.com>
2015-07-29 22:52:51 -0700

    Add factory and spec file for SystemRelativeEventTemplate [#99476326]

--- spec/factories/system_event_templates.rb
+++ spec/factories/system_event_templates.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     name "Template"
     title "Title"
     description "Description"
+
+    factory :system_relative_event_template, class: SystemRelativeEventTemplate do
+      association :root_event_template, factory: :system_event_template
+      association :time_offset
+    end
   end
 end
---
+++ spec/models/system_relative_event_template_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe SystemRelativeEventTemplate do
+  it_has_a 'valid factory'
+
+end
