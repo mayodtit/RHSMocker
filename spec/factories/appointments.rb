98db8d454ea37ad7efa1563f1a1b8318a9da4e53
Neel Anand <neel@getbetter.com>
2015-08-05 16:10:48 -0700

    Update factory to fix specs.

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     association :creator, factory: :member
     appointment_template
     scheduled_at Time.now + 1.day
-    arrival_time Time.now + 2.day
-    departure_time Time.now + 2.day + 30.minutes
+    arrived_at Time.now + 2.day
+    departed_at Time.now + 2.day + 30.minutes
   end
 end
