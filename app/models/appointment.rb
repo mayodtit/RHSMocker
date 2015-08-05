e219f5b690e292a7b7fb15e4992c15f5d8dfc6ff
Neel Anand <neel@getbetter.com>
2015-08-05 11:39:15 -0700

    Associate Appointment with AppointmentTemplate.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,10 +2,11 @@ class Appointment < ActiveRecord::Base
   belongs_to :user
   belongs_to :provider, class_name: 'User'
   belongs_to :creator, class_name: 'Member'
+  belongs_to :appointment_template
   has_many :appointment_changes
 
   attr_accessor :actor_id
-  attr_accessible :user, :user_id, :provider, :provider_id, :scheduled_at, :actor_id, :creator, :creator_id
+  attr_accessible :user, :user_id, :provider, :provider_id, :scheduled_at, :actor_id, :creator, :creator_id, :arrival_time, :departure_time, :appointment_template_id, :appointment_template
 
   validates :user, :provider, :scheduled_at, :creator, presence: true
   after_commit :track_update, on: :update
