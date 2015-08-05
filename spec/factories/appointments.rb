d3bd35f03c1e5ad1bd7dd874d01899c9ec1d79e0
Neel Anand <neel@getbetter.com>
2015-08-05 11:40:06 -0700

    Update Appointment factory.

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -3,6 +3,9 @@ FactoryGirl.define do
     user
     association :provider, factory: :user
     association :creator, factory: :member
+    appointment_template
     scheduled_at Time.now + 1.day
+    arrival_time Time.now + 2.day
+    departure_time Time.now + 2.day + 30.minutes
   end
 end
