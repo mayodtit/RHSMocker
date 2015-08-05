ed48e62b9af709afe35b4a6cf565ba51030848aa
Kyle Chilcutt <kyle@getbetter.com>
2015-08-05 14:21:29 -0700

    Update resource for Contacts spec
    Contacts not Onboarding.

--- spec/acceptance/api/v1/contacts_spec.rb
+++ spec/acceptance/api/v1/contacts_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
-resource 'Onboarding' do
+resource 'Contacts' do
   header 'Accept', 'application/json'
   header 'Content-Type', 'application/json'
 
