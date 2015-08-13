b05501bd392244524e6a1a02b6b2cf6eae2a4827
Emilio Zand <emilio@getbetter.com>
2015-08-13 13:45:33 -0700

    Add TaskCategoryExpertises to Task Categories

--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -1,6 +1,8 @@
 class TaskCategory < ActiveRecord::Base
   has_many :tasks
   has_many :task_templates
+  has_many :task_category_expertises
+  has_many :expertises, through: :task_category_expertises
 
   attr_accessible :title, :description, :priority_weight
 
