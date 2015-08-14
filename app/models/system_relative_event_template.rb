4090e0c192b652e8091121041d226519dd5b9464
Kyle Chilcutt <kyle@getbetter.com>
2015-08-14 11:39:15 -0700

    Ensure a relative template has an offset
    Validate and auto-create. Add all the association options.

--- app/models/system_relative_event_template.rb
+++ app/models/system_relative_event_template.rb
@@ -1,9 +1,21 @@
 class SystemRelativeEventTemplate < SystemEventTemplate
-  attr_accessible :root_event_template_id
-
   belongs_to :root_event_template, class_name: "SystemEventTemplate",
                                    inverse_of: :system_relative_event_templates
-  has_one :time_offset, inverse_of: :system_relative_event_template
+  has_one :time_offset, inverse_of: :system_relative_event_template,
+                        validate: true,
+                        autosave: true,
+                        dependent: :destroy
 
-  validates :root_event_template, presence: true
+  attr_accessible :root_event_template, :root_event_template_id, :time_offset
+
+  validates :root_event_template, :time_offset, presence: true
+
+  before_validation :set_defaults, on: :create
+
+  private
+
+  def set_defaults
+    time_offset || create_time_offset(offset_type: :fixed, direction: :before, fixed_time: Time.at(0))
+    true
+  end
 end
