7795371f9c5def5add93ff585f628b4a1a3ec21f
Kyle Chilcutt <kyle@getbetter.com>
2015-08-17 14:48:40 -0700

    Serialize new values with TimeOffset
    Update to match new model attributes.

--- app/serializers/time_offset_serializer.rb
+++ app/serializers/time_offset_serializer.rb
@@ -1,15 +1,5 @@
 class TimeOffsetSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :direction, :offset_type, :fixed_time, :num_days, :relative_time, :fixed_time_as_integer, :relative_time_as_integer
-
-  delegate :fixed_time, :relative_time, to: :object
-
-  def fixed_time_as_integer
-    fixed_time.to_i
-  end
-
-  def relative_time_as_integer
-    relative_time.to_i
-  end
+  attributes :direction, :offset_type, :absolute_minutes, :relative_days, :relative_minutes_after_midnight
 end
