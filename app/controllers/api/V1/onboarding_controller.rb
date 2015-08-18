df4caf449a3b5c54fca05439d25bb162ef701d18
Peter Christensen <peter@pchristensen.com>
2015-08-17 17:49:24 -0700

    referral_code_validation - add failure code, instance var [#99809552]

--- app/controllers/api/V1/onboarding_controller.rb
+++ app/controllers/api/V1/onboarding_controller.rb
@@ -20,14 +20,15 @@ class Api::V1::OnboardingController < Api::V1::ABaseController
   end
 
   def referral_code_validation
-    if ReferralCode.find_by_code(params[:code])
+    if @referral_code = ReferralCode.find_by_code(params[:code])
       render_success(skip_credit_card: onboarding_skip_credit_card?,
                      onboarding_custom_welcome: onboarding_custom_welcome)
     else
-      render_failure(reason: 'invalid referral code',
-                     user_message:'Referral code is invalid',
-                     skip_credit_card: onboarding_skip_credit_card?,
-                     onboarding_custom_welcome: onboarding_custom_welcome)
+      render_failure({ reason: 'invalid referral code',
+                       user_message:'Referral code is invalid',
+                       skip_credit_card: onboarding_skip_credit_card?,
+                       onboarding_custom_welcome: onboarding_custom_welcome},
+                     404)
     end
   end
 
