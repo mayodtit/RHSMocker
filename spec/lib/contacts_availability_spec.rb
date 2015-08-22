86e710f2c57bc2ec21d5a7244ed73cc9f9b8182e
Peter Christensen <peter@pchristensen.com>
2015-08-21 23:49:44 -0700

    Add ContactsAvailability class, tests [#101008848]

---
+++ spec/lib/contacts_availability_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ContactsAvailability do
+  describe ".has_mayo_access?" do
+    context "true" do
+      it { expect(ContactsAvailability.has_mayo_access?(nil)).to be_true }
+      it "for groups that are mayo_pilot" do
+        mayo_pilot_group = double("onboarding group", :mayo_pilot? => true)
+        expect(ContactsAvailability.has_mayo_access?(mayo_pilot_group)).to be_true
+      end
+    end
+
+    context "false" do
+      it "for groups that aren't mayo_pilot" do
+        non_mayo_pilot_group = double("onboarding group", :mayo_pilot? => false)
+        expect(ContactsAvailability.has_mayo_access?(non_mayo_pilot_group)).to be_false
+      end
+    end
+  end
+end
