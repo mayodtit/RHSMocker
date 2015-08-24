b31526b45d35abe53d142b146bb349b18682a28b
Peter Christensen <peter@pchristensen.com>
2015-08-24 15:48:40 -0700

    Fix tests with new mayo_nurse_line_access field [#101008848]

--- spec/lib/contacts_availability_spec.rb
+++ spec/lib/contacts_availability_spec.rb
@@ -5,14 +5,14 @@ describe ContactsAvailability do
     context "true" do
       it { expect(ContactsAvailability.has_mayo_access?(nil)).to be_true }
       it "for groups that are mayo_pilot" do
-        mayo_pilot_group = double("onboarding group", :mayo_pilot? => true)
+        mayo_pilot_group = double("onboarding group", :mayo_nurse_line_access? => true)
         expect(ContactsAvailability.has_mayo_access?(mayo_pilot_group)).to be_true
       end
     end
 
     context "false" do
       it "for groups that aren't mayo_pilot" do
-        non_mayo_pilot_group = double("onboarding group", :mayo_pilot? => false)
+        non_mayo_pilot_group = double("onboarding group", :mayo_nurse_line_access? => false)
         expect(ContactsAvailability.has_mayo_access?(non_mayo_pilot_group)).to be_false
       end
     end
