9b51275ab08cb8c4d4e33fef30103a5f25983238
Kyle Chilcutt <kyle@getbetter.com>
2015-08-24 09:53:41 -0700

    Add service handling to system events
    Add mechanism to create a Service when a SystemEvent has a :service type
    and attached ServiceTemplate.

--- spec/services/trigger_system_event_service_spec.rb
+++ spec/services/trigger_system_event_service_spec.rb
@@ -48,5 +48,26 @@ describe TriggerSystemEventService do
         expect(action.result).to eq(Message.last)
       end
     end
+
+    context 'for a service SystemEvent' do
+      let!(:system_action_template) { create(:system_action_template, :service) }
+      let!(:system_event_template) { system_action_template.system_event_template }
+      let!(:system_event) { create(:system_event, user: user, system_event_template: system_event_template) }
+
+      it 'creates a service for the user' do
+        expect{ described_class.new(system_event: system_event).call }.to change(Service, :count).by(1)
+        service = user.reload.services.last
+        expect(service.service_template).to eq(system_action_template.published_versioned_resource)
+        expect(service.creator).to eq(Member.robot)
+      end
+
+      it 'creates a system action' do
+        expect{ described_class.new(system_event: system_event).call }.to change(SystemAction, :count).by(1)
+        action = SystemAction.last
+        expect(action.system_event).to eq(system_event)
+        expect(action.system_action_template).to eq(system_action_template)
+        expect(action.result).to eq(Service.last)
+      end
+    end
   end
 end
