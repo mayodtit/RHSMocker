496dd94c217ca2adb352fd5dadc075b4e86916f6
Kyle Chilcutt <kyle@getbetter.com>
2015-08-24 09:24:12 -0700

    Add resource to system action template
    Create reference to published versioned resource by unique identifier to
    system action templates. Use the unique id as the primary key to always
    select the current published version of the resource.

--- app/models/system_action_template.rb
+++ app/models/system_action_template.rb
@@ -4,14 +4,21 @@ class SystemActionTemplate < ActiveRecord::Base
 
   belongs_to :system_event_template, inverse_of: :system_action_template
   belongs_to :content
+  belongs_to :published_versioned_resource, polymorphic: true,
+                                            foreign_key: :versioned_resource_unique_id,
+                                            foreign_type: :versioned_resource_type,
+                                            primary_key: :unique_id,
+                                            conditions: {state: :published}
   has_many :system_actions, inverse_of: :system_action_template
   symbolize :type, in: TYPES
 
-  attr_accessible :system_event_template, :system_event_template_id, :type, :message_text, :content, :content_id
+  attr_accessible :system_event_template, :system_event_template_id, :type, :message_text,
+                  :content, :content_id, :published_versioned_resource
 
   validates :system_event_template, :type, presence: true
   validates :message_text, presence: true, if: ->(s) { s.system_message? || s.pha_message? }
   validates :content, presence: true, if: :content_id
+  validate :published_versioned_resource_is_published, if: :published_versioned_resource
 
   before_validation :set_defaults, on: :create
 
@@ -26,5 +33,11 @@ class SystemActionTemplate < ActiveRecord::Base
   def set_defaults
     self.type ||= :system_message
     self.message_text ||= 'Placeholder text'
+  end
+
+  def published_versioned_resource_is_published
+    unless published_versioned_resource.published?
+      errors.add(:published_versioned_resource, 'must be published')
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150819224810) do
+ActiveRecord::Schema.define(:version => 20150821235004) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -1248,8 +1248,10 @@ ActiveRecord::Schema.define(:version => 20150819224810) do
     t.string   "type"
     t.text     "message_text"
     t.integer  "content_id"
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.string   "versioned_resource_unique_id"
+    t.string   "versioned_resource_type"
   end
 
   create_table "system_actions", :force => true do |t|
