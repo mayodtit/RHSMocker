92201f40fa2cf6f385a3bd0647bc27407c77e43b
Neel Anand <neel@getbetter.com>
2015-08-24 17:55:55 -0700

    Update TaskTemplateSet factory and avoid creating objects of fixed values.

--- spec/factories/task_template_sets.rb
+++ spec/factories/task_template_sets.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :task_template_set do
-    service_template_id 2
-    affirmative_child_id 3
-    negative_child_id 4
+    service_template
+
+    trait :has_affirmative_child do
+      association :affirmative_child, factory: :task_template_set
+    end
+
+    trait :has_negative_child do
+      association :negative_child, factory: :task_template_set
+    end
   end
 end
