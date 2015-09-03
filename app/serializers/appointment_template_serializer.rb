d5d9b53a2ffbce40049925c8c1442f84a8a993ac
Neel Anand <neel@getbetter.com>
2015-09-03 13:27:53 -0700

    Serialize unpublished_version_id to make an unpublished template accessible.

--- app/serializers/appointment_template_serializer.rb
+++ app/serializers/appointment_template_serializer.rb
@@ -1,9 +1,11 @@
 class AppointmentTemplateSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :id, :title, :description, :version, :unique_id, :state, :state_events, :special_instructions, :reason_for_visit, :scheduled_at_system_event_template_id, :discharged_at_system_event_template_id
+  attributes :id, :title, :description, :version, :unique_id, :state, :state_events, :special_instructions,
+             :reason_for_visit, :scheduled_at_system_event_template_id, :discharged_at_system_event_template_id,
+             :unpublished_version_id
 
-  delegate :scheduled_at_system_event_template, :discharged_at_system_event_template, to: :object
+  delegate :scheduled_at_system_event_template, :discharged_at_system_event_template, :unique_id, :published?, to: :object
 
   def scheduled_at_system_event_template_id
     scheduled_at_system_event_template.try(:id)
@@ -11,5 +13,16 @@ class AppointmentTemplateSerializer < ActiveModel::Serializer
 
   def discharged_at_system_event_template_id
     discharged_at_system_event_template.try(:id)
+  end
+
+  private
+
+  def unpublished_version_id
+    return nil unless unique_id && published?
+    if unpublished_template = AppointmentTemplate.unpublished.find_by_unique_id(unique_id)
+      unpublished_template.id
+    else
+      nil
+    end
   end
 end
